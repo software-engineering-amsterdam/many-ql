85c62b1ebc1e97722741494ebf3bb6f9cc4999d0
Carlos C <uldericofilho@gmail.com>
2015-02-02 15:37:34 +0100

    Improve test coverage

--- carlos.cirello/questionaire/questionaire_test.go
+++ carlos.cirello/questionaire/questionaire_test.go
@@ -22,3 +22,25 @@ func TestQuestionaire(t *testing.T) {
 
 	aQuestionaire.PrettyPrintJson()
 }
+
+func TestMultipleQuestions(t *testing.T) {
+	aQuestionaire := &Questionaire{
+		Label: "University of Amsterdam Revenue Service",
+		Questions: []*question.Question{
+			&question.Question{
+				Label:   "What is the answer to life the universe and everything?",
+				Content: new(question.IntQuestion),
+			},
+			&question.Question{
+				Label:   "Who said the logic is the cement of our civilization with which we ascended from Chaos using reason as our guide?",
+				Content: new(question.StringQuestion),
+			},
+			&question.Question{
+				Label:   "Hungry-p",
+				Content: new(question.BoolQuestion),
+			},
+		},
+	}
+
+	aQuestionaire.PrettyPrintJson()
+}
