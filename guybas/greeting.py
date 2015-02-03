079c650ed813d1869eeab129f4ac8605b73f7064
basmeesters <meesters.bas@gmail.com>
2015-02-03 10:17:31 +0100

    Check push

---
+++ guybas/greeting.py
@@ -0,0 +1,4 @@
+from pyparsing import Word, alphas
+greet = Word( alphas ) + "," + Word( alphas ) + "!" # <-- grammar defined here
+hello = "Hello, World!"
+print (hello, "->", greet.parseString( hello ))
