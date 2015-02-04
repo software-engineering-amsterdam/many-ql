f17d2f754d10800be204d935226a2e6e547706e2
Carlos C <uldericofilho@gmail.com>
2015-02-04 21:56:02 +0100

    Replace GUI library with a simpler one - go-qml

--- carlos.cirello/frontend/graphic/graphic.go
+++ carlos.cirello/frontend/graphic/graphic.go
@@ -1,35 +1,127 @@
-// Package graphic is the GUI interface for Frontend. It does not interact directly with VM. It is the vendored package of https://github.com/andlabs/ui (3a742b136a2c5d468e9f75ac4d192fc2e81eae62). All compilations constraints apply.
+// Package graphic is the GUI interface for Frontend. It does not interact directly with VM. It is the vendored package of gopkg.in/qml.v1. All compilations constraints apply.
 package graphic
 
 import (
-	//Vendored from http://github.com/andlabs/ui/
-	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/frontend/graphic/ui"
+	"fmt"
+	"os"
+
+	"gopkg.in/qml.v1"
 )
 
-var window ui.Window
-
-func Draw() {
-	go ui.Do(func() {
-		name := ui.NewTextField()
-		button := ui.NewButton("Greet")
-		greeting := ui.NewLabel("")
-		stack := ui.NewVerticalStack(
-			ui.NewLabel("Enter your name:"),
-			name,
-			button,
-			greeting)
-		window = ui.NewWindow("Hello", 200, 100, stack)
-		button.OnClicked(func() {
-			greeting.SetText("Hello, " + name.Text() + "!")
-		})
-		window.OnClosing(func() bool {
-			ui.Stop()
-			return true
-		})
-		window.Show()
-	})
-	err := ui.Go()
-	if err != nil {
-		panic(err)
+func main() {
+	if err := qml.Run(run); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
+}
+
+const BASE = `
+import QtQuick 2.2
+import QtQuick.Controls 1.1
+import QtQuick.Layouts 1.0
+
+ApplicationWindow {
+    id: app
+    visible: true
+    title: "QL"
+    property int margin: 11
+    width: mainLayout.implicitWidth + 2 * margin
+    height: mainLayout.implicitHeight + 2 * margin
+    minimumWidth: mainLayout.Layout.minimumWidth + 2 * margin
+    minimumHeight: mainLayout.Layout.minimumHeight + 2 * margin
+
+    ColumnLayout {
+        id: mainLayout
+        objectName: "rows"
+        anchors.fill: parent
+        anchors.margins: margin
+        GroupBox {
+            objectName: "rowBox"
+            id: rowBox
+            title: "Row layout"
+            Layout.fillWidth: true
+
+            RowLayout {
+                id: rowLayout
+                anchors.fill: parent
+                TextField {
+                    placeholderText: "This wants to grow horizontally"
+                    Layout.fillWidth: true
+                }
+                Button {
+                    text: "Button"
+                }
+            }
+        }
+        GroupBox {
+            id: rowBoxa
+            title: "Row layouta"
+            Layout.fillWidth: true
+
+            RowLayout {
+                id: rowLayouta
+                anchors.fill: parent
+                TextField {
+                    placeholderText: "This wants to grow horizontally"
+                    Layout.fillWidth: true
+                }
+                Button {
+                    text: "Button"
+                }
+            }
+        }
+    }
+}
+`
+
+const NEW_LINE = `
+import QtQuick 2.2
+import QtQuick.Controls 1.1
+import QtQuick.Layouts 1.0
+
+GroupBox {
+objectName: "rowsa"
+id: rowBoxb
+            title: "Row layout ADDED DYNAMICALLY"
+            Layout.fillWidth: true
+
+            RowLayout {
+                id: rowLayoutb
+                anchors.fill: parent
+                TextField {
+                    placeholderText: "This wants to grow horizontally"
+                    Layout.fillWidth: true
+                }
+                Button {
+                    text: "Button"
+                }
+            }
+}
+`
+
+func run() error {
+	engine := qml.NewEngine()
+
+	base, err := engine.LoadString("base.qml", BASE)
+	if err != nil {
+		return err
+	}
+
+	newLine, err := engine.LoadString("newLine.qml", NEW_LINE)
+	if err != nil {
+		return err
+	}
+
+	nl := newLine.Create(nil)
+
+	win := base.CreateWindow(nil)
+	rows := win.Root().ObjectByName("rows")
+
+	nl.Set("parent", rows)
+	// nl.Destroy();
+
+	win.Show()
+	win.Wait()
+
+	return nil
 }
