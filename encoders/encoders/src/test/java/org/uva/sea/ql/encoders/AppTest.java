84b8f4ff3ae3f1f6494e80244b226f4b3109cd18
Pim Tegelaar <pimtegelaar@gmail.com>
2015-02-04 23:46:14 +0100

    Added initial maven project

---
+++ encoders/encoders/src/test/java/org/uva/sea/ql/encoders/AppTest.java
@@ -0,0 +1,14 @@
+package org.uva.sea.ql.encoders;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		App.main(new String[]{});
+	}
+
+}
