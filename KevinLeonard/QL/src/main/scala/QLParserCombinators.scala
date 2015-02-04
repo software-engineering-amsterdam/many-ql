4cfb7be53d3f78bd68679225347714479ff6e3e6
leonardpunt <leonard@q42.nl>
2015-02-04 14:34:34 +0100

    Remove example, since examples can be found in the resources folder.

--- KevinLeonard/QL/src/main/scala/QLParserCombinators.scala
+++ KevinLeonard/QL/src/main/scala/QLParserCombinators.scala
@@ -5,42 +5,6 @@ import java.io.Serializable
 import scala.util.parsing.combinator.JavaTokenParsers
 import scala.io.Source
 
-
-/*
-QL Language description example:
-
-form TaxForm {
-
-    question hasSoldHouse2 "How many houses did you sell in 2014?"
-    answer integer
-
-    question hasSoldHouse3 "How many houses did you sell in 2014?"
-    answer string
-
-    question hasSoldHouse "How many houses did you sell in 2014?"
-    answer boolean
-
-    if hasSoldHouse {
-        question totalHousesSold "How many houses did you sell in 2014?"
-        answer integer
-    } else {
-        question sellingPrice "What was the selling price?"
-        answer integer
-
-        question houseBuyer "To whom did you sell the house?"
-        answer string
-    }
-
-    if hasSoldHouse {
-        question totalHousesSold "How many houses did you sell in 2014?"
-        answer integer
-    }
-
-    question hasSoldHouse2 "How many houses did you sell in 2014?"
-    answer integer
-}
-
- */
 class QLParserCombinators extends JavaTokenParsers {
 
   def form : Parser[String] = "form" ~> formName ~ formBlock ^^ { _.toString }
