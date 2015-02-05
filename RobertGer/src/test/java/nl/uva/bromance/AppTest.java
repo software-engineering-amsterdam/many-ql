d589372ee16632ecf759272e694c4b35834338c0
Robert Diebels <robertdiebels@gmail.com>
2015-02-05 19:46:28 +0100

    - package fix to get the test resource from the package immediatel.

--- RobertGer/src/test/java/nl/uva/bromance/AppTest.java
+++ RobertGer/src/test/java/nl/uva/bromance/AppTest.java
@@ -3,16 +3,18 @@ package nl.uva.bromance;
 import org.junit.Test;
 import org.antlr.v4.runtime.*;
 
+import java.io.IOException;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
     @Test
-    public void testApp()
+    public void testApp() throws IOException
     {
-        try {
-            ANTLRInputStream ais = new ANTLRInputStream(getClass().getResourceAsStream("/test.ql"));
+
+            ANTLRInputStream ais = new ANTLRInputStream(getClass().getResourceAsStream("test.ql"));
             nl.uva.bromance.parsers.ExpLexer l = new nl.uva.bromance.parsers.ExpLexer(ais);
             nl.uva.bromance.parsers.ExpParser p = new nl.uva.bromance.parsers.ExpParser(new CommonTokenStream(l));
             p.addErrorListener(new BaseErrorListener() {
@@ -22,8 +24,6 @@ public class AppTest
                 }
             });
             p.field();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+
     }
 }
