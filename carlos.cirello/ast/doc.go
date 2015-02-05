4d70130cf0cb4cd1405ef1ead230b5e401f8635b
Carlos C <uldericofilho@gmail.com>
2015-02-05 18:48:39 +0100

    Fix documentation mistake

--- carlos.cirello/ast/doc.go
+++ carlos.cirello/ast/doc.go
@@ -1,2 +1,2 @@
-// Package ast describe the parse which the VM uses to execute. It is the result of the compilation process from Package compiler
+// Package ast describe the parse tree which the VM uses to execute. It is the result of the compilation process from Package compiler
 package ast
