f719885799c52973586bdb37e52aee7979eac10a
Geert Kapteijns <geertkapteijns@wcw-stud-145-109-27-20.wireless.uva.nl>
2015-02-07 14:02:43 +0100

    test toegevoegd aan preprocessor, hij klopt nog voor geen moer :D

--- geert.kai/ql/initialize.rb
+++ geert.kai/ql/initialize.rb
@@ -2,5 +2,5 @@
 #Citrus.load("query_language")
 
 require_relative "lib/query_language/preprocessor.rb"
-@string = Preprocessor.process("spec/source_files/one_query.ql")
+@string = Preprocessor.process("spec/source_files/if_statement.ql")
 
