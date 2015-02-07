226d3817864cc117190b70a7e1b388ab1b609b06
Gurrt <gerritkrijnen@hotmail.com>
2015-02-07 12:09:37 +0100

    - Multiple ways of parsing added, included error checking.

--- RobertGer/src/main/java/nl/uva/bromance/App.java
+++ RobertGer/src/main/java/nl/uva/bromance/App.java
@@ -1,13 +1,10 @@
 package nl.uva.bromance;
-import nl.uva.bromance.ql.Converter;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Converter c = new Converter();
-        c.getAST();
     }
 
 }
--- RobertGer/src/main/java/nl/uva/bromance/listeners/ExpParseTreeListener.java
+++ RobertGer/src/main/java/nl/uva/bromance/listeners/ExpParseTreeListener.java
@@ -12,19 +12,37 @@ import java.util.List;
 public class ExpParseTreeListener extends ExpBaseListener {
 
     public static ExpParser.FieldContext fieldCtx;
-    public static ExpParser.LocationContext locationCtx;
 
+    private String currentBurial;
     @Override
     public void enterField(ExpParser.FieldContext ctx) {
         super.enterField(ctx);
         fieldCtx = ctx;
+        List<ParseTree> kids = ctx.children;
+
         System.out.println("entering field :" + fieldCtx);
     }
 
     public void enterLocation(ExpParser.LocationContext lct){
         super.enterLocation(lct);
-        locationCtx = lct;
-        System.out.println("entering location : "+lct.x.getText()+","+lct.y.getText());
+        System.out.println("[Hard Parse] Treasure :"+currentBurial+" is buried at : "+lct.x.getText()+","+lct.y.getText());
+    }
+    public void exitLocation(ExpParser.LocationContext lct){
+       super.exitLocation(lct);
+        System.out.println("Exiting location"+lct);
+    }
+    public void enterBurial(ExpParser.BurialContext btx){
+        super.enterBurial(btx);
+        currentBurial = btx.treasure.getText();
+        if (btx.at == null){
+            System.err.println("Error@Line:"+btx.start.getLine()+"] Burial "+currentBurial+" has no valid location!");
+        } else {
+            System.out.println("[Easy Parse] Treasure :"+btx.treasure.getText()+" is buried at : "+btx.at.x.getText()+","+btx.at.y.getText());
+        }
+    }
+    public void exitBurial(ExpParser.BurialContext btx){
+        super.exitBurial(btx);
+        System.out.println("Exited burial");
     }
 
     public int getBurialCount() {
