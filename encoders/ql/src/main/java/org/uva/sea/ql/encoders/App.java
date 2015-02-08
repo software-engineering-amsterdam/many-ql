1bd9f09ecb22c2f5ef8a25e881e9e87505ba68fc
pimtegelaar <pimtegelaar@gmail.com>
2015-02-08 18:07:41 +0100

    Test with ANTLR

--- encoders/ql/src/main/java/org/uva/sea/ql/encoders/App.java
+++ encoders/ql/src/main/java/org/uva/sea/ql/encoders/App.java
@@ -1,13 +1,10 @@
 package org.uva.sea.ql.encoders;
+public class App {
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws Exception {
+
+        Game game = new GameFactory().createGame(App.class.getResourceAsStream("/example.field"));
+
+        game.play();
     }
 }
