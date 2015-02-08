f7c4453dda6cfcf522d79ff5934f7e757f261a97
verhoofstad <verhoofstad@hotmail.com>
2015-02-08 13:24:11 +0100

    Added unit test and winforms project

---
+++ CarlaErik/UvA.SoftCon.Questionnaire/Parsing.UnitTest/QLParserTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UvA.SoftCon.Questionnaire.Parsing.UnitTest
+{
+    [TestClass]
+    public class QLParserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
