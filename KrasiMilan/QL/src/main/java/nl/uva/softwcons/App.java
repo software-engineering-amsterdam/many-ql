136793799887265fec45a78d4f258d06974812b5
Milan Milanov <whizzer.me@gmail.com>
2015-02-09 11:40:22 +0100

    Add antlr maven plugin and a dummy grammar

--- KrasiMilan/QL/src/main/java/nl/uva/softwcons/App.java
+++ KrasiMilan/QL/src/main/java/nl/uva/softwcons/App.java
@@ -1,13 +1,12 @@
 package nl.uva.softwcons;
 
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+	}
 }
