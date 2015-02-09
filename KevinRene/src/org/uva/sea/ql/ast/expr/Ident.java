3ae181f0721f682bf27b3e8a586eea3c85db07bc
Rene Bulsing <rene.bulsing@outlook.com>
2015-02-09 11:58:08 +0100

    Fixed compilation problem.

--- KevinRene/src/org/uva/sea/ql/ast/expr/Ident.java
+++ KevinRene/src/org/uva/sea/ql/ast/expr/Ident.java
@@ -2,7 +2,7 @@ package org.uva.sea.ql.ast.expr;
 
 import org.uva.sea.ql.ast.ASTNode;
 
-public class Ident extends ASTNode {
+public class Ident implements ASTNode {
 	private String value;
 	
 	public Ident(String value) {
--- KevinRene/src/org/uva/sea/ql/ast/expr/Int.java
+++ KevinRene/src/org/uva/sea/ql/ast/expr/Int.java
@@ -2,7 +2,7 @@ package org.uva.sea.ql.ast.expr;
 
 import org.uva.sea.ql.ast.ASTNode;
 
-public class Int extends ASTNode {
+public class Int implements ASTNode {
 	
 	private int value;
 		
