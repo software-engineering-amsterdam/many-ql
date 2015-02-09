51287ae4a03fe3c24a1f708924c0dc24424db31f
leonardpunt <leonard@q42.nl>
2015-02-09 14:59:23 +0100

    Adds some tests.

--- KevinLeonard/QL/src/main/scala/parser/QLParser.scala
+++ KevinLeonard/QL/src/main/scala/parser/QLParser.scala
@@ -9,11 +9,13 @@ class QLParser extends JavaTokenParsers with QLAST {
 
   def expression: Parser[Expr] = "{" ~> rep(questionExpression | ifExpression) <~ "}" ^^ Sequence
 
-  def questionExpression: Parser[QuestionExpr] = "question" ~> variable ~ stringLiteral ~ answer ^^ { case v ~ label ~ _type => QuestionExpr(v, label, _type)}
+  def questionExpression: Parser[QuestionExpr] = "question" ~> variable ~ label ~ answer ^^ { case v ~ label ~ _type => QuestionExpr(v, label, _type)}
 
   def answer: Parser[String] = "answer" ~> ("boolean" | "integer" | "string")
 
   def ifExpression: Parser[IfExpr] = ("if" ~> variable) ~ expression ~ ("else" ~> expression ?) ^^ { case v ~ expr1 ~ expr2 => IfExpr(v, expr1, expr2)}
 
   def variable: Parser[Variable] = ident ^^ Variable
+
+  def label: Parser[String] = stringLiteral
 }
--- KevinLeonard/QL/src/test/scala/QLParserSpec.scala
+++ KevinLeonard/QL/src/test/scala/QLParserSpec.scala
@@ -9,11 +9,24 @@ class QLParserSpec extends Specification with ParserMatchers {
 
   import parsers._
 
-  "form" should {
-    "succeed to recognize forms with a question" in {
-      form must succeedOn("form TaxForm {\n\n    question hasSoldHouse2 \"How many houses did you sell in 2014?\"\n    answer integer\n}")
-        .withResult(equalTo("(TaxForm~List(((hasSoldHouse2~\"How many houses did you sell in 2014?\")~integer)))"))
+  "question expressions" should {
+    "have a variable, label and type" in {
+      questionExpression must succeedOn("question var \"label\"\nanswer string")
+        .withResult(new QuestionExpr(new Variable("var"), "\"label\"", "string"))
     }
   }
 
+  "if expressions" should {
+    "be valid without an else clause" in {
+      ifExpression must succeedOn("if var {}")
+        .withResult(new IfExpr(new Variable("var"), new Sequence(List()), None))
+    }
+  }
+
+  "if expressions" should {
+    "be valid with an else clause" in {
+      ifExpression must succeedOn("if var {} else {}")
+        .withResult(new IfExpr(new Variable("var"), new Sequence(List()), Some(new Sequence(List()))))
+    }
+  }
 }
