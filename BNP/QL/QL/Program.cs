f998060296506fe4f5f5a9d23c945d91b68e10de
Pavol Kincel <secult@gmail.com>
2015-02-09 18:27:19 +0100

    got running the basics

--- BNP/QL/QL/Program.cs
+++ BNP/QL/QL/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+
 
 namespace QL
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+
+            Stream inputStream = Console.OpenStandardInput();
+            AntlrInputStream input = new AntlrInputStream(inputStream);
+            QLLexer lexer = new QLLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            QLParser parser = new QLParser(tokens);
+            
+
+
+            Console.ReadLine();
         }
     }
 }
