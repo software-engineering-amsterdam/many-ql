86102a27eb01d27cde92d0b46306e5803aced533
Carlos C <uldericofilho@gmail.com>
2015-02-09 12:30:51 +0100

    Reorganize ql.go extracting stream instantion from consumption.

--- carlos.cirello/ql.go
+++ carlos.cirello/ql.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/cli"
+	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/cli/stream"
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/compiler"
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/frontend"
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/frontend/graphic"
@@ -16,28 +14,18 @@ import (
 func main() {
 	inFn, outFn := cli.Args()
 
-	// todo(carlos) extract CSV output into a proper module.
-	outWriter := os.Stdout
-	if outFn != "-" {
-		file, err := os.Create(outFn)
-		if err != nil {
-			log.Fatalln("Error creating output file: ", err)
-		}
-		outWriter = file
-	}
+	inReader, outWriter := stream.New(inFn, outFn)
+
 	csvWriter := output.New(outWriter)
+	codeBuf := reader.New(inReader)
 
-	codeBuf, err := reader.New(inFn)
-	if err != nil {
-		log.Fatal(err)
-	}
 	code := codeBuf.Read()
-
 	aQuestionaire := compiler.CompileQL(code)
 	fromVM, toVM := vm.New(aQuestionaire)
 
 	driver := graphic.GUI(aQuestionaire.Label)
 	frontend.New(fromVM, toVM, driver)
 	driver.Loop()
+
 	csvWriter.Write(aQuestionaire)
 }
