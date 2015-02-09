0c0f5342095286a079de1ccc16e706c6f2ce4239
leonardpunt <leonard@q42.nl>
2015-02-09 17:25:16 +0100

    Adds boolean expressions.

--- KevinLeonard/QL/src/main/scala/parser/QLParser.scala
+++ KevinLeonard/QL/src/main/scala/parser/QLParser.scala
@@ -12,22 +12,47 @@ class QLParser extends JavaTokenParsers with QLAST {
     case name ~ expr => Form(name, expr)
   }
 
-  def expression: Parser[Expr] = "{" ~> rep(questionExpression | ifExpression) <~ "}" ^^ Sequence
-
-  def questionExpression: Parser[QuestionExpr] = "question" ~> variable ~ label ~ answer ^^ {
-    case v ~ label ~ "boolean" => BooleanQuestionExpr(v, label)
-    case v ~ label ~ "integer" => IntegerQuestionExpr(v, label)
-    case v ~ label ~ "string" => StringQuestionExpr(v, label)
-  }
-
-  def answer: Parser[String] = "answer" ~> ("boolean" | "integer" | "string")
-
-  def ifExpression: Parser[IfExpr] = ("if" ~> variable) ~ expression ~ ("else" ~> expression ?) ^^ {
-    case v ~ expr1 ~ expr2 => IfExpr(v, expr1, expr2)
-  }
+  def const: Parser[Const] = ("true" | "false") ^^ Const
 
   def variable: Parser[Variable] = ident ^^ Variable
 
   def label: Parser[String] = stringLiteral
+
+  def expression: Parser[Expr] = "{" ~> rep(questionExpression | ifExpression) <~ "}" ^^ Sequence
+
+  // parse questions
+  def questionExpression: Parser[QuestionExpr] = "question" ~> variable ~ label ~ answer ^^ {
+    case v ~ label ~ "boolean" => BooleanQuestion(v, label)
+    case v ~ label ~ "integer" => IntegerQuestion(v, label)
+    case v ~ label ~ "string" => StringQuestion(v, label)
+  }
+
+  def answer: Parser[String] = "answer" ~> ("boolean" | "integer" | "string")
+
+  // parse if statements
+  def ifExpression: Parser[IfExpr] = ("if" ~> variable) ~ expression ~ ("else" ~> expression ?) ^^ {
+    case v ~ expr1 ~ expr2 => IfExpr(v, expr1, expr2)
+  }
+
+  // parse boolean expression
+  def booleanExpression: Parser[Expr] = orExpression
+
+  def orExpression: Parser[Expr] = rep1sep(andExpression, "or") ^^ {
+    _.reduceLeft(Or)
+  }
+
+  def andExpression: Parser[Expr] = rep1sep(notExpression, "and") ^^ {
+    _.reduceLeft(And)
+  }
+
+  def notExpression: Parser[Expr] = opt("not") ~ atom ^^ {
+    case Some(_) ~ x => Not(x)
+    case _ ~ x => x
+  }
+
+  def atom: Parser[Expr] = (const
+    | variable
+    | "(" ~> booleanExpression <~ ")"
+    )
 
 }
