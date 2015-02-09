fb83d8cd082248d4a335cd7d4675eaa4db7ff940
Kevin A <kevin_son@live.nl>
2015-02-09 17:54:55 +0100

    Added a way to print AST trees

--- KevinRene/src/cons/ql/parser/Test.java
+++ KevinRene/src/cons/ql/parser/Test.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
+
 import cons.ql.ast.ASTNode;
+import cons.ql.ast.Binary;
 
 public class Test {
 	
@@ -16,20 +18,37 @@ public class Test {
 	public static void main(String [] args) throws IOException {
 //		Reader reader = readFile("input.txt");
 		
-		System.out.println("Starting test");
+//		System.out.println("Starting test");
 		
-		testString("5 + 5 - 10 * 193");
+		printASTTree("5 + 5 - 10 * 193");
 	}
 	
-	private static ASTNode testString (String input) {
+	public static ASTNode testString (String input) {
 		Reader reader = new StringReader(input);
 		QLLexer lexer = new QLLexer(reader);
 		QLParser parser = new QLParser(lexer);
 
 		lexer.nextToken();
-		System.out.println("Parser success: " + parser.parse());
-		System.out.println(parser.getResult());
+		parser.parse();
+//		System.out.println("Parser success: " + parser.parse());
+//		System.out.println(parser.getResult());
 		
 		return parser.getResult();
+	}
+	
+	public static void printASTTree (String input) {
+		ASTNode root = testString(input);
+		
+		printSubTree(root, "", true);
+	}
+	
+	private static void printSubTree(ASTNode root, String prefix, boolean tail) {
+
+		System.out.println(prefix + (tail ? "└── " : "├── ") + root);
+		
+		if (root instanceof Binary) {
+			printSubTree(((Binary)root).getLeft(), prefix + (tail ? "  " : "│  "), false);
+			printSubTree(((Binary)root).getRight(), prefix + (tail ? "  " : "│  "), true);
+		}
 	}
 }
