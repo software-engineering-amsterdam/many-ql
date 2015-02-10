8b5c06f85158c457a98d871813f2cea5bb30beb4
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-02-10 17:46:30 +0100

    Literals and Visitor on them

---
+++ Fugazi/src/org/fugazi/ast/Literals/Literal.java
@@ -0,0 +1,20 @@
+package org.fugazi.ast.Literals;
+
+import org.fugazi.ast.ASTNode.ASTNode;
+import org.fugazi.ast.Type.TypeVisitor;
+
+/**
+ * The Literal class. An abstract class the express a Literal.
+ * It is a Node of the AST.
+ */
+public abstract class Literal extends ASTNode {
+
+    /**
+     * Convert to string
+     * @return string
+     */
+    public abstract String toString();
+    
+    // Accept Visitor
+    public abstract <T> T accept(LiteralVisitor<T> visitor);
+}
---
+++ Fugazi/src/org/fugazi/ast/Literals/STRING.java
@@ -0,0 +1,28 @@
+package org.fugazi.ast.Literals;
+
+/**
+ * The String.
+ */
+public class STRING extends Literal {
+
+    private String name;
+
+    public STRING(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public <T> T accept(LiteralVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
+
