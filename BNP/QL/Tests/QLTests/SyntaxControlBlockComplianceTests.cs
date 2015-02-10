3ff23e72912740e131deb1482bf1f68566f1ea35
BAPostma <barry.postma@student.uva.nl>
2015-02-10 17:29:56 +0100

    Added basic syntax unit tests (though not all succeed)

---
+++ BNP/QL/Tests/QLTests/SyntaxControlBlockComplianceTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.QLTests
+{
+    [TestClass]
+    public class SyntaxControlBlockComplianceTests : SyntaxComplianceTestBase
+    {
+        [TestMethod]
+        public void IfBlock()
+        {
+            string input = @"if (7 == 7) {
+                                
+                             }
+                            ";
+            Build(input);
+            var unit = Parser.unit();
+
+            Assert.IsNull(unit.exception);
+        }
+        
+        [TestMethod]
+        public void IfElseBlock()
+        {
+            string input = @"if (7 == 7) {
+                                
+                             } else {
+
+                             }
+                            ";
+            Build(input);
+            var unit = Parser.unit();
+
+            Assert.IsNull(unit.exception);
+        }
+
+        [TestMethod]
+        public void IfElseIfElseBlock()
+        {
+            string input = @"if (7 == 7) {
+                                
+                             } else if (yes == no) {
+
+                             } else {
+
+                             }
+                            ";
+            Build(input);
+            var unit = Parser.unit();
+
+            Assert.IsNull(unit.exception);
+        }
+
+
+    }
+}
