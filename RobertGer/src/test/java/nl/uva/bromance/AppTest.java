07bfa1fc5ad62ce7b5aa07f5e0c0cb12d7423c34
Robert Diebels <me@robertdiebels.com>
2015-02-10 11:15:06 +0100

    - Updated test to match removal of old grammars

--- RobertGer/src/test/java/nl/uva/bromance/AppTest.java
+++ RobertGer/src/test/java/nl/uva/bromance/AppTest.java
@@ -1,8 +1,4 @@
 package nl.uva.bromance;
-
-import nl.uva.bromance.listeners.BurialListener;
-import nl.uva.bromance.listeners.DrinkListener;
-import nl.uva.bromance.listeners.ExpParseTreeListener;
 import nl.uva.bromance.listeners.QLParseTreeListener;
 import nl.uva.bromance.parsers.*;
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -13,49 +9,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
-    /*
-    @Test
-    public void expTest() throws IOException {
 
-        ExpLexer lexer = new ExpLexer(new ANTLRInputStream(this.getClass().getResourceAsStream("ExpTest.ql")));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ExpParser parser = new ExpParser(tokens);
-        ParserRuleContext burialContext = parser.burial();
-        BurialListener listener = new BurialListener();
-        ParseTreeWalker walker = new ParseTreeWalker();
-
-        walker.walk(listener, burialContext);
-
-        assertThat(listener.getCountBurial()).isEqualTo(1);
-    }
-
-    @Test
-    public void drinkTest() throws IOException {
-        DrinkLexer lexer = new DrinkLexer(new ANTLRInputStream(this.getClass().getResourceAsStream("DrinkTest.ql")));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        DrinkParser parser = new DrinkParser(tokens);
-        DrinkParser.DrinkSentenceContext drinkSentenceContext = parser.drinkSentence();
-        DrinkListener listener = new DrinkListener();
-        ParseTreeWalker walker = new ParseTreeWalker();
-
-        walker.walk(listener, drinkSentenceContext);
-    }
-
-    @Test
-    public void treeTest() throws IOException {
-        ExpLexer lexer = new ExpLexer(new ANTLRInputStream(this.getClass().getResourceAsStream("ExpTest.ql")));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ExpParser parser = new ExpParser(tokens);
-        ParseTree tree = parser.burial();
-        ExpParseTreeListener listener = new ExpParseTreeListener();
-        ParseTreeWalker walker = new ParseTreeWalker();
-
-        walker.walk(listener, tree);
-    }
-*/
     @Test
     public void grammarTest() throws IOException {
         QLLexer lexer = new QLLexer(new ANTLRInputStream(this.getClass().getResourceAsStream("GrammarTest.ql")));
@@ -67,32 +23,4 @@ public class AppTest {
         walker.walk(listener, tree);
         System.out.println("Tree Structure : "+tree.toStringTree(parser));
     }
-/*
-    @Test
-    public void showTree() throws IOException {
-        ExpLexer lexer = new ExpLexer(new ANTLRInputStream(this.getClass().getResourceAsStream("ExpTest.ql")));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ExpParser parser = new ExpParser(tokens);
-        ParseTree tree = parser.field();
-        ExpParseTreeListener listener = new ExpParseTreeListener();
-        ParseTreeWalker walker = new ParseTreeWalker();
-
-        walker.walk(listener, tree);
-
-        //show AST in console
-        System.out.println(tree.toStringTree(parser));
-
-        //show AST in GUI
-        JFrame frame = new JFrame("Antlr AST");
-        JPanel panel = new JPanel();
-        TreeViewer viewer = new TreeViewer(Arrays.asList(
-                parser.getRuleNames()), tree);
-        viewer.setScale(1.5);//scale a little
-        panel.add(viewer);
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200, 200);
-        frame.setVisible(true);
-    }
-    */
 }
