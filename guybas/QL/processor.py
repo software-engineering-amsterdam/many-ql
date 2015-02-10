acab54cb1c316d857b906a1c467f63b18c7da50a
Guy Rombaut <admin@gsrweb.net>
2015-02-10 21:27:52 +0100

    Guy's changes

--- guybas/QL/processor.py
+++ guybas/QL/processor.py
@@ -1,9 +1,22 @@
 from pyparsing import *
+import collections
+
+
 class Processor:
     def conditions_proc(self, condition):
         # TODO: process the expression, and return true/false
         c_list = ParseResults(condition).asList()
-        print(str(c_list))
-        # c_str  = str.join(condition)
-        # print(eval(c_str))
-        return True
+        c_str  = self.rlist2string(c_list)
+        result = eval(c_str)
+        if result:
+            return True
+        return False
+
+    def rlist2string(self, a):
+        res = ''
+        if isinstance(a, list):
+            for item in a:
+                res += str(self.rlist2string(item)) + ' '
+        else:
+            res = str(a)
+        return res
