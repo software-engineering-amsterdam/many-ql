ad219d074e8931f6c6fb73193ffd8653f0c77236
Kevin van Cleef <kevin@developmentstudio.nl>
2015-02-10 12:35:50 +0100

    Adds a begin for computed questions.

--- KevinLeonard/QL/src/main/scala/ast/QLAST.scala
+++ KevinLeonard/QL/src/main/scala/ast/QLAST.scala
@@ -10,9 +10,18 @@ trait QLAST {
   case class Variable(name: String) extends Expr
 
   sealed abstract class QuestionExpr extends Expr
+  sealed abstract class ComputedQuestionExpr extends QuestionExpr
   case class BooleanQuestion(v: Variable, label: String) extends QuestionExpr
   case class IntegerQuestion(v: Variable, label: String) extends QuestionExpr
   case class StringQuestion(v: Variable, label: String) extends QuestionExpr
+  case class ComputedBooleanQuestion(v: Variable, label: String, value: Object) extends ComputedQuestionExpr
+  case class ComputedIntegerQuestion(v: Variable, label: String) extends ComputedQuestionExpr
+  case class ComputedStringQuestion(v: Variable, label: String) extends ComputedQuestionExpr
+
+  sealed abstract class QuestionType
+  case class BooleanType() extends QuestionType
+  case class IntegerType() extends QuestionType
+  case class StringType() extends QuestionType
 
   case class Or(l: Expr, r: Expr) extends Expr
   case class And(l: Expr, r: Expr) extends Expr
