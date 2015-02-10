939c205522ac51e8b5f3aa5a5f03fdd25b5481cc
Gurrt <gerritkrijnen@hotmail.com>
2015-02-10 10:06:10 +0100

    - I accidentally a class ;)

---
+++ RobertGer/src/main/java/nl/uva/bromance/questionnaire/ranges/BiggerThan.java
@@ -0,0 +1,34 @@
+package nl.uva.bromance.questionnaire.ranges;
+
+/**
+ * Created by Gerrit Krijnen on 2/9/2015.
+ */
+public class BiggerThan implements Range {
+
+    double val;
+
+    public BiggerThan(double value){
+        this.val = value;
+    }
+    public BiggerThan(int value){
+        this.val = (double) value;
+    }
+
+    @Override
+    public boolean valueConformsToRange(double value) {
+        if (value > this.val)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public boolean valueConformsToRange(int value) {
+        return this.valueConformsToRange((double) value);
+    }
+
+    @Override
+    public String toString(){
+        return "> "+val;
+    }
+}
