380105a1a67379c78c964437c3021a4497e6ada6
basmeesters <meesters.bas@gmail.com>
2015-02-10 09:09:37 +0100

    Gestructure directory + added abstraction layer

--- guybas/QL/grammar.py
+++ guybas/QL/grammar.py
@@ -1,7 +1,8 @@
-# Grammar 2.0
+# Grammar 
 from pyparsing import *
 from exceptions import *
-from ast2 import *
+from abstract import *
+from ast import *
 
 # Normal sentence grammar
 endSignEsc      = Word('?', exact = 3) | Word ('.', exact = 3) | Word('!', exact = 3)
@@ -9,6 +10,7 @@ word            = Word("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234
 hexaColor       = Suppress("#") + Word(hexnums, exact=6)
 endSign         = oneOf(". ? !")
 comment         = Literal("//") + restOfLine | cStyleComment
+variable        = Word(alphanums)
 
 # Brackets / Brackets / Colon
 obrac           = Literal("{")
@@ -18,32 +20,32 @@ cpar            = Literal(")")
 col             = Literal(":")
 
 # Answer types
-bool            = Word("True") | Word("False") 
-sentence        = (OneOrMore(word) + endSign).setResultsName("TEXT").setParseAction(makeSentence)
+bool            = Literal("True") | Literal("False") 
+sentence        = (OneOrMore(word) + endSign).setParseAction(make_sentence)
 sentences       = OneOrMore(sentence)
-integer         = Word(nums).setName("INTEGER")
+integer         = Word(nums)
 
 # Constraints
 exp             = bool | integer | sentence
 compare         = oneOf("> >= < <= ==")
-condition       = Group(Suppress("Question") + integer + compare + exp)
-pIf             = Suppress("if" + opar) + condition + Suppress(cpar)
-pElse           = Word("else")
+condition       = (Suppress("Question") + integer + compare + exp)
 
 # Form
-answerType      = Word("bool") | Word("integer") | sentence  
-answer          = Group(Suppress("Answer-type:") + answerType)
+answerType      = Literal("bool") | Literal("integer") | Literal("text")  
+answer          = Suppress("Answer-type:") + answerType
 question        = (Suppress("Question") + integer + Suppress(col) + sentence +\
-                  answer).setResultsName("QUESTION").setParseAction(Question)
+                  answer).setResultsName("QUESTION").setParseAction(ASTReady.make_question)
 questions       = OneOrMore(question)
-questions2       = Group(pIf + Suppress(obrac) + questions + Suppress(cbrac)).setResultsName("CONDITIONAL").setParseAction(Conditional_Questions) | questions
-form            = (word.setResultsName("NAME") + OneOrMore(questions2)).setResultsName("FORM")
+pIf             = (Suppress("if" + opar) + condition + Suppress(cpar) + Suppress(obrac) + questions + Suppress(cbrac))
+pElse           = pIf+ Literal("else") + Suppress(obrac) +  questions + Suppress(cbrac)
+questions2      = pElse.setParseAction(ASTReady.make_else) | pIf.setParseAction(ASTReady.make_if) | questions
+form            = (word + OneOrMore(questions2)).setParseAction(ASTReady.make_form)
 
 # Test
 try:
     myfile = open('ql_example.ql', 'r').read()
     l = form.ignore(comment).parseString(myfile)
-    print(l)
-    
+    for i in l:
+        print(i)    
 except Exception as e:
     exceptionsHandling(e)
