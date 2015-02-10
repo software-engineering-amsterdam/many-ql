6746d42f7189d49de87ce01ab9950bcfd65b01d1
Guido Loupias <guidouva@outlook.com>
2015-02-10 12:08:23 +0100

    test script

--- TeamAwesome/ql/build.py
+++ TeamAwesome/ql/build.py
@@ -1,8 +1,5 @@
 import os
 import test
-import test_expr
 
 os.system('java org.antlr.v4.Tool -Dlanguage=Python3 QL.g4');
 test.main()
-print('-' * 50)
-test_expr.main()
--- TeamAwesome/ql/test.py
+++ TeamAwesome/ql/test.py
@@ -1,19 +1,30 @@
-import sys
+import glob
 
 from antlr4 import *
 from QLLexer import QLLexer
 from QLParser import QLParser
-from QLListener import QLListener
 
-input = FileStream("t.QL")
+def run_test(test_filename):
+    input = FileStream(test_filename)
+    lexer = QLLexer(input)
+    stream = CommonTokenStream(lexer)
+    parser = QLParser(stream)
 
-lexer = QLLexer(input)
+    method_name = test_filename.split('.')[0].split('_')[1]
 
-stream = CommonTokenStream(lexer)
+    tree_test = getattr(parser, method_name)()
+    if parser._syntaxErrors > 0:
+        print_tree(tree_test, 0)
+        print( str(parser._syntaxErrors)\
+             + ' error(s) while testing `'\
+             + method_name\
+             + '` on input file '\
+             + test_filename
+             ) 
+        print('-'*60)
 
-parser = QLParser(stream)
+    return parser._syntaxErrors
 
-tree_test = parser.form()
 
 def print_tree(tree_test, lev):
     if not isinstance(tree_test, tree.Tree.TerminalNodeImpl):
@@ -27,7 +38,16 @@ def print_tree(tree_test, lev):
 
 
 def main():
-    print_tree(tree_test, 0)    
+    errors = 0
+
+    test_filenames = glob.glob('test_*.ql')
+    for test_filename in test_filenames:
+        errors += run_test(test_filename)
+
+    print(str(errors) + ' error(s).\n'\
+         + 'Tested '+str(len(test_filenames))+' file(s): '\
+         + str(test_filenames)
+         )
 
 if __name__ == '__main__':
     main()
