6536fa29bec3eab2597eb20c25c3dc441c7a8929
Carlos C <uldericofilho@gmail.com>
2015-02-10 16:46:49 +0100

    Add first set of visitor calls to traverse the AST

--- carlos.cirello/vm/vm.go
+++ carlos.cirello/vm/vm.go
@@ -4,6 +4,8 @@ Package vm is the runtime which executes the AST created from the compiler.
 package vm
 
 import (
+	"log"
+
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/ast"
 	fe "github.com/software-engineering-amsterdam/many-ql/carlos.cirello/frontend"
 )
@@ -37,13 +39,8 @@ func (v *vm) loop() {
 		select {
 		case r := <-v.receive:
 			if r.Type == fe.ReadyT {
-				for _, action := range v.questionaire.Stack {
-					questionCopy := action.QuestionNode.Clone()
-					v.send <- &fe.Event{
-						Type:     fe.Render,
-						Question: questionCopy,
-					}
-				}
+				// visit everything to setup interface
+				visitQuestionaire(v.questionaire, v.send)
 				v.send <- &fe.Event{
 					Type: fe.Flush,
 				}
@@ -56,6 +53,8 @@ func (v *vm) loop() {
 							v.questionaire.Stack[k].QuestionNode.From(answer)
 						}
 					}
+					// visit everything again
+					visitQuestionaire(v.questionaire, v.send)
 				}
 			}
 		default:
@@ -65,4 +64,30 @@ func (v *vm) loop() {
 		}
 	}
 
+}
+
+func visitQuestionaire(q *ast.QuestionaireNode, toFrontend chan *fe.Event) {
+	for _, actionNode := range q.Stack {
+		visitActionNode(actionNode, toFrontend)
+	}
+}
+
+func visitActionNode(a *ast.ActionNode, toFrontend chan *fe.Event) {
+	if nil != a.QuestionNode {
+		visitQuestionNode(a.QuestionNode, toFrontend)
+	} else if nil != a.IfNode {
+		visitIfNode(a.IfNode, toFrontend)
+	}
+}
+
+func visitQuestionNode(q *ast.QuestionNode, toFrontend chan *fe.Event) {
+	questionCopy := q.Clone()
+	toFrontend <- &fe.Event{
+		Type:     fe.Render,
+		Question: questionCopy,
+	}
+}
+
+func visitIfNode(i *ast.IfNode, toFrontend chan *fe.Event) {
+	log.Println("Ignoring IfNodes for now")
 }
