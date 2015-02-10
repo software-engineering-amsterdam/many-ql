345df6dbeb4755bdf16a5d72f7c68c83bfd474f0
Calin Borlovan <calin.borlovan@gmail.com>
2015-02-10 15:34:27 +0100

    AST in progress

--- CalinWouter/src/org/uva/student/calinwouter/ql/ast/ParenExp.java
+++ CalinWouter/src/org/uva/student/calinwouter/ql/ast/ParenExp.java
@@ -1,4 +1,17 @@
 package org.uva.student.calinwouter.ql.ast;
 
-public class ParenExp {
+import org.uva.student.calinwouter.ql.interpreter.components.InterpretationException;
+import org.uva.student.calinwouter.ql.interpreter.model.Environment;
+
+public class ParenExp extends Exp{
+    private Exp e;
+
+    public Object interprete(Environment environment) throws InterpretationException {
+        return e.interprete(environment);
+    }
+
+    public ParenExp(Exp e){
+        this.e = e;
+    }
+
 }
