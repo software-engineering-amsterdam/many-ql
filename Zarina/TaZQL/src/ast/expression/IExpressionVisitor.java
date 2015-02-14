35605e09d45d94512b93800d2de454529ae0bb5d
Zarinka <efendijeva@gmail.com>
2015-02-14 18:28:03 +0100

    Refactoring visitor class of expressions & removing old stuff

--- Zarina/TaZQL/src/ast/expression/IExpressionVisitor.java
+++ Zarina/TaZQL/src/ast/expression/IExpressionVisitor.java
@@ -1,6 +1,17 @@
 package ast.expression;
 
+import ast.expression.variables.BooleanVariable;
+import ast.expression.variables.Id;
+import ast.expression.variables.IntegerVariable;
+import ast.expression.variables.StringVariable;
+
+
 
 public interface IExpressionVisitor<T> {
-	 T visit(ArithmeticExpression expr);
+	public T visit(ArithmeticExpression expr);
+	
+	public T visit(StringVariable string);
+	public T visit(IntegerVariable integer);
+	public T visit(BooleanVariable bool);
+	public T visit(Id identifier);
 }
