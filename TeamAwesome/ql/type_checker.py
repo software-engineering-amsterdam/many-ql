33718d59fec8913e6dce3e06a096ab65a97fb0a0
Guido Loupias <guidouva@outlook.com>
2015-02-15 14:37:27 +0100

    Description of what the type-checker must do

--- TeamAwesome/ql/type_checker.py
+++ TeamAwesome/ql/type_checker.py
@@ -1,5 +1,33 @@
-class TypeChecker(object):
-	def __init__(self, ast):
-		self.ast = ast
+def check(ast):
+    """
+    Typechecks the AST.
 
-	
+    Language features:
+    - No nested form statements
+    - No else statements
+
+    - Unary and binary arithmetic can only be performed on numeric
+      type
+    - Logical negation can only be performed on boolean type
+    - Less-than and greater-than comparison can only be performed on
+      numeric type
+    - Equality and inequality comparison can be performed on
+      boolean, numeric and string type
+    - Logical AND and OR can only be performed on boolean type
+
+    Numeric types: integer, money
+
+    Detailed features implemented:
+
+    *Statements*
+    - root can only contain form statements
+      (should be caught by parser)
+    - form statements can only contain if and question statements
+    - if statements can only contain if and question statements
+    - assigned default values of question_type statements must match
+      the type
+
+    *Expressions*
+    - (See Language features)
+    """
+    pass
