fbbdc813a066e58ab7a8ea9a5aadd9d40c73be86
Jonatan <felipez@live.nl>
2015-02-15 13:55:49 +0100

    linked generated files
    that way we won't have to re add them after grammar rebuilds.

---
+++ FelipezConde/QuestionnaireLanguage/QuestionnaireLanguage/Implementation/QLMainVisitor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrammarProject;
+using QuestionnaireLanguage.Implementation.Visitors;
+
+namespace QuestionnaireLanguage.Implementation
+{
+
+    /// <summary>
+    /// This Partial class contains the visitors that handle the form structure productions
+    /// </summary>
+    public partial class QLVisitor: GrammarProject.IQLMainVisitor<RoseTree>
+    {
+        Dictionary<string, string> symbolTable = new Dictionary<string, string>();
+        RoseTree AST;
+
+        ArithmeticVisitor arithmeticVis = new ArithmeticVisitor();
+        ExpressionVisitor expressionVis = new ExpressionVisitor();
+        KeyValPairVisitor keyValPairVis = new KeyValPairVisitor();
+        FormVisitor formVis = new FormVisitor();
+        TypeVisitor typeVis = new TypeVisitor();
+
+    }
+
+    /// <summary>
+    /// This partial class contains the visitors that handle the type related productions 
+    /// </summary>
+    public partial class QLVisitor : GrammarProject.IQLMainVisitor<RoseTree>
+    {
+        ArithmeticVisitor arVis = new ArithmeticVisitor();
+
+
+        RoseTree Antlr4.Runtime.Tree.IParseTreeVisitor<RoseTree>.Visit(Antlr4.Runtime.Tree.IParseTree tree)
+        {
+            throw new NotImplementedException();
+        }
+
+        RoseTree Antlr4.Runtime.Tree.IParseTreeVisitor<RoseTree>.VisitChildren(Antlr4.Runtime.Tree.IRuleNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        RoseTree Antlr4.Runtime.Tree.IParseTreeVisitor<RoseTree>.VisitErrorNode(Antlr4.Runtime.Tree.IErrorNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        RoseTree Antlr4.Runtime.Tree.IParseTreeVisitor<RoseTree>.VisitTerminal(Antlr4.Runtime.Tree.ITerminalNode node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// This partial class contains the visitors that handle the KeyValPairs productions
+    /// </summary>
+    public partial class QLVisitor : GrammarProject.QLMainBaseVisitor<RoseTree>
+    {
+        RoseTree IQLMainVisitor<RoseTree>.VisitKeyValPair(QLMainParser.KeyValPairContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// This partial class contains the visitors that handle the expression productions
+    /// </summary>
+    public partial class QLVisitor : GrammarProject.QLMainBaseVisitor<RoseTree>
+    {
+
+    }
+
+    /// <summary>
+    /// This partial class contains the visitors that handle the arithmetic productions
+    /// </summary>
+    public partial class QLVisitor : GrammarProject.QLMainBaseVisitor<RoseTree>
+    {
+
+    }
+}
