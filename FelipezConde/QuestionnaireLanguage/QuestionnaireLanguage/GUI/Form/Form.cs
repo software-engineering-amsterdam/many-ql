cf6a573505aa3aaba76222ffedb4dc52b4ed9b66
Daniel <danijose24@gmail.com>
2015-02-15 13:28:45 +0100

    First implementation - structure GUI elements.
    first version of the structure for the creation of GUI elements. Not
    complete yet.

---
+++ FelipezConde/QuestionnaireLanguage/QuestionnaireLanguage/GUI/Form/Form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuestionnaireLanguage.GUI.Form
+{
+    public class FormSection
+    {
+        public StackPanel Create()
+        {
+            StackPanel _stack = new StackPanel();
+
+            return _stack;
+        }
+
+        public StackPanel AddChildren(StackPanel panel, Control control)
+        {
+            panel.Children.Add(control);
+
+            return panel;
+        }
+    }
+}
