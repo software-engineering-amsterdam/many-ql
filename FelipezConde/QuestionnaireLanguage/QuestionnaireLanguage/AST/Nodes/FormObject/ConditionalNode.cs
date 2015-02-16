fa578c2d388091dbd020f6e7b65732fdf288a888
Daniel <danijose24@gmail.com>
2015-02-16 15:37:45 +0100

    Visitor class
    first visitors created

--- FelipezConde/QuestionnaireLanguage/QuestionnaireLanguage/AST/Nodes/FormObject/ConditionalNode.cs
+++ FelipezConde/QuestionnaireLanguage/QuestionnaireLanguage/AST/Nodes/FormObject/ConditionalNode.cs
@@ -8,5 +8,21 @@ namespace QuestionnaireLanguage.AST.Nodes.FormObject
 {
     public class ConditionalNode : iFormObjectNode
     {
+        private IList<iASTNode> children;
+
+        public ConditionalNode()
+        {
+            children = new List<iASTNode>();
+        }
+
+        public void AddChild( iASTNode conditionalNode)
+        {
+            children.Add(conditionalNode);
+        }
+
+        public IList<iASTNode> GetChildren()
+        {
+            return children;
+        }
     }
 }
