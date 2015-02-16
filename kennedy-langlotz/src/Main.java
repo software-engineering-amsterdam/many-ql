e8410f5f4311233f7efd0dc86330d7e638769863
Juriaan <juriaankennedy@gmail.com>
2015-02-16 17:21:05 +0100

    end of day commit

--- kennedy-langlotz/src/Main.java
+++ kennedy-langlotz/src/Main.java
@@ -1,10 +1,36 @@
 import AST.Node;
+import AST.ParseTreeConverter;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.*;
+import parser.*;
+import AST.KLQNodes.*;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
  * Created by Timon on 09.02.2015.
  */
 public class Main {
-    public static void main(String[] args){}
+    public static void main(String[] args) throws Exception {
+        System.out.println("start running");
+        String inputFile = null;
+        if ( args.length>0 ) inputFile = args[0];
+        InputStream is = System.in;
+        if ( inputFile!=null ) is = new FileInputStream(inputFile);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+
+        KLQLexer lexer = new KLQLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        KLQParser parser = new KLQParser(tokens);
+        ParseTree tree = parser.questionaire(); // parse
+
+        ParseTreeConverter eval = new ParseTreeConverter();
+        eval.visit(tree);
+        for (Node child : eval.getAst().getChildren()){
+            child.printSelf();
+        }
+        System.out.println("done running");
+    }
 }
