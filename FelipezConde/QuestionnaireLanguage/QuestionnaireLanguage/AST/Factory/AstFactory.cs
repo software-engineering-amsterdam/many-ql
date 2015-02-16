faa48f78d5892036d88732d18015af27827e375f
Daniel <danijose24@gmail.com>
2015-02-16 22:52:56 +0100

    ast factory

---
+++ FelipezConde/QuestionnaireLanguage/QuestionnaireLanguage/AST/Factory/AstFactory.cs
@@ -0,0 +1,22 @@
+﻿using QuestionnaireLanguage.AST.Nodes;
+using QuestionnaireLanguage.AST.Nodes.FormObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireLanguage.AST.Factory
+{
+    public abstract class AstFactory
+    {
+        public static AstFactory GetNodeFactory()
+        {
+            AstFactory factory = NodeFactory.GetNodeFactory();
+            return factory;
+        }
+
+        public abstract iFormObjectNode GetFormObjectNode (string formObjectName);
+
+    }
+}
---
+++ FelipezConde/QuestionnaireLanguage/QuestionnaireLanguage/AST/Nodes/NodeFactory.cs
@@ -0,0 +1,30 @@
+﻿using QuestionnaireLanguage.AST.Factory;
+using QuestionnaireLanguage.AST.Nodes.FormObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireLanguage.AST.Nodes
+{
+    internal class NodeFactory : AstFactory
+    {
+
+        public static NodeFactory GetNodeFactory()
+        {
+            return new NodeFactory();
+        }
+
+        public override iFormObjectNode GetFormObjectNode(string formObjectName)
+        {
+            iFormObjectNode formObjectNode;
+            if (formObjectName.Equals("question"))
+                formObjectNode = new QuestionNode();
+            else// if (context.GetText().Equals("question"))
+                formObjectNode = new FieldNode();
+
+            return formObjectNode;
+        }
+    }
+}
