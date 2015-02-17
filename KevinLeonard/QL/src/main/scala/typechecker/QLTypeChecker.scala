aeac0e2303d55807c985b05910d0f3336a2770e2
leonardpunt <leonard@q42.nl>
2015-02-17 17:51:26 +0100

    Fix indentation and add comment.

--- KevinLeonard/QL/src/main/scala/typechecker/QLTypeChecker.scala
+++ KevinLeonard/QL/src/main/scala/typechecker/QLTypeChecker.scala
@@ -3,109 +3,111 @@ package typechecker
 import ast.QLAST
 
 class QLTypeChecker extends {
-    import QLAST._
+  import QLAST._
 
-    type Environment = Map[String, Literal]
-    val emptyEnvironment = Map[String, Literal]()
+  // Holds the defined variables and their type.
+  // Note that we require variables to be declared before being used.
+  type Environment = Map[String, Literal]
+  val emptyEnvironment = Map[String, Literal]()
 
-    val defaultBooleanValue = BooleanLiteral(false)
-    val defaultNumberValue = NumberLiteral(0)
-    val defaultStringValue = StringLiteral("")
+  val defaultBooleanValue = BooleanLiteral(false)
+  val defaultNumberValue = NumberLiteral(0)
+  val defaultStringValue = StringLiteral("")
 
-    def check(form: Form, env: Environment = emptyEnvironment): Environment = check(form.e, env)
-  
-    def check(statement: Statement, env: Environment): Environment = statement match {
-      case Sequence(statements: List[Statement]) => statements.foldLeft(env) { (env, statement) => check(statement, env) }
-      case s @ IfStatement(e: Expression, s1: Statement, None) => check(e, env) match {
-        case BooleanLiteral(_) =>
-          check(s1, env)
-          env // Return environment without the questions in s1.
-        case _ => sys.error(s"Invalid boolean condition for if statement at line ${s.pos}")
-      }
-      case s @ IfStatement(e: Expression, s1: Statement, Some(s2: Statement)) => check(e, env) match {
-        case BooleanLiteral(_) =>
-          check(s1, env)
-          check(s2, env)
-          env // Return environment without the questions in s1 and s2.
-        case _ => sys.error(s"Invalid boolean condition for if statement at line ${s.pos}")
-      }
-      case BooleanQuestion(v: Variable, label: String) => env + (v.name -> defaultBooleanValue)
-      case NumberQuestion(v: Variable, label: String) => env + (v.name -> defaultNumberValue)
-      case StringQuestion(v: Variable, label: String) => env + (v.name -> defaultStringValue)
-      case s @ ComputedBooleanQuestion(v: Variable, label: String, e: Expression) => check(e, env) match {
-        case BooleanLiteral(_) => env + (v.name -> defaultBooleanValue)
-        case _ => sys.error(s"Invalid expression for value of computed boolean question at line ${s.pos}")
-      }
-      case s @ ComputedNumberQuestion(v: Variable, label: String, e: Expression) => check(e, env) match {
-        case NumberLiteral(_) => env + (v.name -> defaultNumberValue)
-        case _ => sys.error(s"Invalid expression for value of computed number expression at line ${s.pos}")
-      }
-      case s @ ComputedStringQuestion(v: Variable, label: String, e: Expression) => check(e, env) match {
-        case StringLiteral(_) => env + (v.name -> defaultStringValue)
-        case _ => sys.error(s"Invalid expression for value of computed string expression at line ${s.pos}")
-      }
+  def check(form: Form, env: Environment = emptyEnvironment): Environment = check(form.e, env)
+
+  def check(statement: Statement, env: Environment): Environment = statement match {
+    case Sequence(statements: List[Statement]) => statements.foldLeft(env) { (env, statement) => check(statement, env) }
+    case s @ IfStatement(e: Expression, s1: Statement, None) => check(e, env) match {
+      case BooleanLiteral(_) =>
+        check(s1, env)
+        env // Return environment without the questions in s1.
+      case _ => sys.error(s"Invalid boolean condition for if statement at line ${s.pos}")
     }
-  
-    def check(expression: Expression, env: Environment): Literal = expression match {
-      case e @ Or(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid or expression at line ${e.pos}")
-      }
-      case e @ And(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid and expression at line ${e.pos}")
-      }
-      case e @ Not(expr: Expression) => check(expr, env) match {
-        case BooleanLiteral(_) => defaultBooleanValue
-        case _ => sys.error(s"Invalid not expression at line ${e.pos}")
-      }
-      case e @ Equal(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
-        case (StringLiteral(_), StringLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid == expression at line ${e.pos}")
-      }
-      case e @ NotEqual(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
-        case (StringLiteral(_), StringLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid != expression at line ${e.pos}")
-      }
-      case e @ LessThan(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid < expression at line ${e.pos}")
-      }
-      case e @ LessThanEqual(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid <= expression at line ${e.pos}")
-      }
-      case e @ GreaterThan(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid > than expression at line ${e.pos}")
-      }
-      case e @ GreaterThanEqual(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
-        case _ => sys.error(s"Invalid >= expression at line ${e.pos}")
-      }
-      case e @ Add(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
-        case _ => sys.error(s"Invalid addition expression at line ${e.pos}")
-      }
-      case e @ Sub(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
-        case _ => sys.error(s"Invalid subtraction expression at line ${e.pos}")
-      }
-      case e @ Mul(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
-        case _ => sys.error(s"Invalid multiplication expression at line ${e.pos}")
-      }
-      case e @ Div(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
-        case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
-        case _ => sys.error(s"Invalid division expression at line ${e.pos}")
-      }
-      case e @ Variable(name: String) => env getOrElse(name, sys.error(s"Undefined variable $name at line ${e.pos}"))
+    case s @ IfStatement(e: Expression, s1: Statement, Some(s2: Statement)) => check(e, env) match {
+      case BooleanLiteral(_) =>
+        check(s1, env)
+        check(s2, env)
+        env // Return environment without the questions in s1 and s2.
+      case _ => sys.error(s"Invalid boolean condition for if statement at line ${s.pos}")
+    }
+    case BooleanQuestion(v: Variable, label: String) => env + (v.name -> defaultBooleanValue)
+    case NumberQuestion(v: Variable, label: String) => env + (v.name -> defaultNumberValue)
+    case StringQuestion(v: Variable, label: String) => env + (v.name -> defaultStringValue)
+    case s @ ComputedBooleanQuestion(v: Variable, label: String, e: Expression) => check(e, env) match {
+      case BooleanLiteral(_) => env + (v.name -> defaultBooleanValue)
+      case _ => sys.error(s"Invalid expression for value of computed boolean question at line ${s.pos}")
+    }
+    case s @ ComputedNumberQuestion(v: Variable, label: String, e: Expression) => check(e, env) match {
+      case NumberLiteral(_) => env + (v.name -> defaultNumberValue)
+      case _ => sys.error(s"Invalid expression for value of computed number expression at line ${s.pos}")
+    }
+    case s @ ComputedStringQuestion(v: Variable, label: String, e: Expression) => check(e, env) match {
+      case StringLiteral(_) => env + (v.name -> defaultStringValue)
+      case _ => sys.error(s"Invalid expression for value of computed string expression at line ${s.pos}")
+    }
+  }
+
+  def check(expression: Expression, env: Environment): Literal = expression match {
+    case e @ Or(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid or expression at line ${e.pos}")
+    }
+    case e @ And(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid and expression at line ${e.pos}")
+    }
+    case e @ Not(expr: Expression) => check(expr, env) match {
       case BooleanLiteral(_) => defaultBooleanValue
-      case NumberLiteral(_) => defaultNumberValue
-      case StringLiteral(_) => defaultStringValue
+      case _ => sys.error(s"Invalid not expression at line ${e.pos}")
     }
+    case e @ Equal(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
+      case (StringLiteral(_), StringLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid == expression at line ${e.pos}")
+    }
+    case e @ NotEqual(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (BooleanLiteral(_), BooleanLiteral(_)) => defaultBooleanValue
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
+      case (StringLiteral(_), StringLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid != expression at line ${e.pos}")
+    }
+    case e @ LessThan(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid < expression at line ${e.pos}")
+    }
+    case e @ LessThanEqual(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid <= expression at line ${e.pos}")
+    }
+    case e @ GreaterThan(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid > than expression at line ${e.pos}")
+    }
+    case e @ GreaterThanEqual(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultBooleanValue
+      case _ => sys.error(s"Invalid >= expression at line ${e.pos}")
+    }
+    case e @ Add(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
+      case _ => sys.error(s"Invalid addition expression at line ${e.pos}")
+    }
+    case e @ Sub(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
+      case _ => sys.error(s"Invalid subtraction expression at line ${e.pos}")
+    }
+    case e @ Mul(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
+      case _ => sys.error(s"Invalid multiplication expression at line ${e.pos}")
+    }
+    case e @ Div(l: Expression, r: Expression) => (check(l, env), check(r, env)) match {
+      case (NumberLiteral(_), NumberLiteral(_)) => defaultNumberValue
+      case _ => sys.error(s"Invalid division expression at line ${e.pos}")
+    }
+    case e @ Variable(name: String) => env getOrElse(name, sys.error(s"Undefined variable $name at line ${e.pos}"))
+    case BooleanLiteral(_) => defaultBooleanValue
+    case NumberLiteral(_) => defaultNumberValue
+    case StringLiteral(_) => defaultStringValue
+  }
 }
