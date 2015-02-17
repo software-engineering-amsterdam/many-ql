a498c4a9985479579b22f1fbaa40bb4868c034b6
haren <lukasz.harezlak@gmail.com>
2015-02-17 20:24:32 +0100

    ast tree visits started. if statements breaks visitor

--- Fugazi/src/org/fugazi/type_checker/TypeChecker.java
+++ Fugazi/src/org/fugazi/type_checker/TypeChecker.java
@@ -6,11 +6,11 @@ import org.fugazi.ast.form.Form;
 import java.util.ArrayList;
 
 /*
-// TODO type checker detect
+// TODO type checker must detect
+
 reference to undefined questions
 duplicate question declarations with different types
 conditions that are not of the type boolean
-operands of invalid type to operators
 cyclic dependencies between questions
 duplicate labels (warning)
  */
@@ -21,9 +21,11 @@ public class TypeChecker {
     // should it be final then?
     // or use clean method like now?
     private final ASTErrorHandler astErrorHandler;
+    private final TypeCheckerVisitor visitor;
 
     public TypeChecker() {
         this.astErrorHandler = new ASTErrorHandler();
+        this.visitor = new TypeCheckerVisitor();
     }
 
     /**
@@ -34,6 +36,7 @@ public class TypeChecker {
 
     // TODO should there be a separate isFormCorrect method?
     public boolean checkForm(Form form) {
+        form.accept(this.visitor);
         return !this.astErrorHandler.hasErrors();
     }
 
