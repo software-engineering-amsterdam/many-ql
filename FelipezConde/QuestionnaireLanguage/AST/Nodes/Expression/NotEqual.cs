b6d212373350c0b5ba9039ad1aff7a10fd54672a
Daniel <danijose24@gmail.com>
2015-02-17 13:00:04 +0100

    undelete project, thank you git

---
+++ FelipezConde/QuestionnaireLanguage/AST/Nodes/Expression/NotEqual.cs
@@ -0,0 +1,29 @@
+﻿using AST.Nodes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AST.Nodes.Expression
+{
+    public class NotEqual : IExpressionNode
+    {
+        private IList<IASTNode> children;
+
+        public NotEqual()
+        {
+            children = new List<IASTNode>();
+        }
+
+        public void AddChild(IASTNode node)
+        {
+            children.Add(node);
+        }
+
+        public IList<IASTNode> GetChildren()
+        {
+            return children;
+        }
+    }
+}
