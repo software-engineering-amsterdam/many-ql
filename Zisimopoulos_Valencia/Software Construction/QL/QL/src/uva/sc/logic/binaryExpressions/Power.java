8869c9cfd658b6164a5cce287c2c27eb0ce913e0
Pantelis Zisimopoulos <pantelis.zisimopoulos@student.uva.nl>
2015-02-17 19:50:01 +0100

    ast

---
+++ Zisimopoulos_Valencia/Software Construction/QL/QL/src/uva/sc/logic/binaryExpressions/Power.java
@@ -0,0 +1,10 @@
+package uva.sc.logic.binaryExpressions;
+
+import uva.sc.logic.Node;
+
+public class Power extends BinaryExpression{
+
+	public Power(Node firstOperand, Node secondOperand) {
+		super(firstOperand, secondOperand);
+	}
+}
---
+++ Zisimopoulos_Valencia/Software Construction/QL/QL/src/uva/sc/logic/unaryExpressions/Minus.java
@@ -0,0 +1,11 @@
+package uva.sc.logic.unaryExpressions;
+
+import uva.sc.logic.Node;
+
+public class Minus extends UnaryExpression{
+
+	public Minus(Node operand) {
+		super(operand);
+	}
+
+}
