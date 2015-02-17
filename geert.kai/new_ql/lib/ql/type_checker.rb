1dcaf646c03f1a18b3b1c1a3c34e7e328c754fcd
Kai Bakker <kaibakker@gmail.com>
2015-02-17 13:32:32 +0100

    beginnetje dup label checker

--- geert.kai/new_ql/lib/ql/type_checker.rb
+++ geert.kai/new_ql/lib/ql/type_checker.rb
@@ -1,3 +1,22 @@
-class TypeChecker < BaseVisitor
-   
+
+class DuplicateLabelChecker < BaseVisitor
+  visitor_for Form do |form|
+    form.accept(self)
+  end
+
+  visitor_for Conditional do |conditional|
+    conditional.accept(self)
+  end
+
+  visitor_for Question do |question|
+    question.description
+  end
+
+  def initialize(form)
+    @form = form
+  end
+  
+  def check
+    descriptions = visit(@form)  
+  end
 end
--- geert.kai/new_ql/spec/type_checker_spec.rb
+++ geert.kai/new_ql/spec/type_checker_spec.rb
@@ -6,7 +6,7 @@ describe "Type checker" do
   it "detects duplicate labels" do
     question = Question.new(description: "Wat is uw leeftijd?", variable_name: "leeftijd", type: "integer")
     form = Form.new(statements: [question, question], name: "dubbel_label")
-    expect(TypeChecker.new(form).check_for_duplicate_labels).to eq ( { :valid => false, :labels => ["Wat is uw leeftijd?"] } )
+    expect(DuplicateLabelChecker.new(form).check).to eq ( { :valid => false, :labels => ["Wat is uw leeftijd?"] } )
 
   end
 end
