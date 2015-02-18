18a107fecda5843c548d096f1623ed7314194e9e
Guido Loupias <guidouva@outlook.com>
2015-02-18 17:15:06 +0100

    Visitor for AST

--- TeamAwesome/ql/GenericVisitor.py
+++ TeamAwesome/ql/GenericVisitor.py
@@ -1,15 +1,16 @@
-class SomeVisitor:
+class GenericVisitor:
     def visit(self, obj):
         return self._dispatch(obj)
 
     # Dispatch works this way in Python:
     # suppose obj is an instance of class Foo.
     # The method dispatched to will be _visitFoo(obj).
+    #
+    # Python will raise an AttributeError if getattr() fails to find
+    # the method.
     def _dispatch(self, obj):
         method = getattr(
             self,
             '_visit' + obj.__class__.__name__
         )
-        if method is None:
-            raise AssertionError('method %s not found' % (method))
         return method(obj)
