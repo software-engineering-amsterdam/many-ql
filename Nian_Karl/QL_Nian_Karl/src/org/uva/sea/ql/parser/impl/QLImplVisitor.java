76197730698918e9956e772c6973b357f3be7bd6
Karl Chow <Karltjuh92@hotmail.com>
2015-02-18 09:57:41 +0100

    Added labels for the expressions, make sure to implement all the visit methods for each expression.

--- Nian_Karl/QL_Nian_Karl/src/org/uva/sea/ql/parser/impl/QLImplVisitor.java
+++ Nian_Karl/QL_Nian_Karl/src/org/uva/sea/ql/parser/impl/QLImplVisitor.java
@@ -11,57 +11,67 @@ import org.uva.sea.ql.model.statement.IfStatement;
 import org.uva.sea.ql.model.statement.QuestionStatement;
 import org.uva.sea.ql.model.statement.Statement;
 import org.uva.sea.ql.parser.antlr.QLBaseVisitor;
+import org.uva.sea.ql.parser.antlr.QLParser.AddExprContext;
+import org.uva.sea.ql.parser.antlr.QLParser.AndExprContext;
 import org.uva.sea.ql.parser.antlr.QLParser.StatementContext;
-
 
 public class QLImplVisitor extends QLBaseVisitor<List<Statement>> {
 
-	private List<Statement> statementList; 
+	private List<Statement> statementList;
 	private QLFactory factory;
 
-	
 	public QLImplVisitor() {
-		this.statementList  = new ArrayList<Statement>();
+		this.statementList = new ArrayList<Statement>();
 		this.factory = new QLFactory();
 	}
 	
 	
+	 @Override
+	public List<Statement> visitAndExpr(AndExprContext ctx) {
+		// example.
+		 return super.visitAndExpr(ctx);
+	}
+
 	@Override
 	public List<Statement> visitStatement(StatementContext ctx) {
-		if (ctx.question() != null) {			
+		if (ctx.question() != null) {
 			statementList.add(factory.getQuestion(ctx.question()));
 		}
 		if (ctx.ifStatement() != null) {
-			
-			// Nian: Try to parse these crazy expressions like: "if((100 > 5) && ((5+6) < 10))"
+			// Nian: Try to parse these crazy expressions like:
+			// "if((100 > 5) && ((5+6) < 10))"
 			// and get a boolean out of it.
-			String leftString = ctx.ifStatement().expression().expression(0).getText();
+			String leftString = ctx.ifStatement().expression().expression(0)
+					.getText();
 			System.out.println(leftString);
-			Expression leftExpression = new NumberLiteral(Integer.parseInt(leftString));
-			
-			
-			String rightString = ctx.ifStatement().expression().expression(1).getText();
+			Expression leftExpression = new NumberLiteral(
+					Integer.parseInt(leftString));
+
+			String rightString = ctx.ifStatement().expression().expression(1)
+					.getText();
 			System.out.println(rightString);
-			Expression rightExpression = new NumberLiteral(Integer.parseInt(rightString));
-			
-			
-			Expression expr = new AddExpression(leftExpression, rightExpression);			
+			Expression rightExpression = new NumberLiteral(
+					Integer.parseInt(rightString));
+
+			Expression expr = new AddExpression(leftExpression, rightExpression);
 			IfStatement statement = new IfStatement(expr);
 
 			for (int i = 0; i < ctx.ifStatement().block().size(); i++) {
-				List<Statement> statements2 = visitStatement(ctx.ifStatement().block().get(i).statement(i));
+				List<Statement> statements2 = visitStatement(ctx.ifStatement()
+						.block().get(i).statement(i));
 				for (int j = 0; j < statements2.size(); j++) {
 					if (statements2.get(i).getClass() == QuestionStatement.class) {
-						
+
 					}
-						
+
 				}
 			}
 			statementList.add(statement);
-			// Recursive method to get all the questions + if statements inside the ifstatement.
-			
+			// Recursive method to get all the questions + if statements inside
+			// the ifstatement.
+
 		}
 		return statementList;
 	}
-	
+
 }
