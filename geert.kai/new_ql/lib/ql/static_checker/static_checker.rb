d6c561d8c027f362cfe43505c33c36522cbf4e64
Geert Kapteijns <geertkapteijns@wcw-stud-145-109-26-103.wireless.uva.nl>
2015-02-19 16:09:31 +0100

    we gaan beginnen met de evaluator

--- geert.kai/new_ql/lib/ql/static_checker/static_checker.rb
+++ geert.kai/new_ql/lib/ql/static_checker/static_checker.rb
@@ -4,9 +4,14 @@ class StaticChecker < BaseVisitor
   def initialize(form)
     @form = form
     after_initialize(form)
+    self
   end
 
   def after_initialize(form)
+  end
+
+  visitor_for Form do |form|
+    form.accept(self)
   end
 
   private
--- geert.kai/new_ql/spec/type_checker_spec.rb
+++ geert.kai/new_ql/spec/type_checker_spec.rb
@@ -7,13 +7,24 @@ describe "Type checker" do
     question = Question.new(description: "Wat is uw leeftijd?", variable_name: "leeftijd", type: "integer")
     form = Form.new(statements: [question, question], name: "dubbel_label")
     expect(DuplicateLabelChecker.new(form).check).to eq ( { :valid => false, :labels => ["Wat is uw leeftijd?"] } )
-
   end
 
   it "detects if the type doesn't match up" do
     question = Question.new(description: "Wat is je naam?", variable_name: "naam", type: "string")
     conditional = If.new(expression: GreaterThan.new(Variable.new("naam"), IntegerLiteral.new(8)), statements: [])
     form = Form.new(name: "Test", statements: [question, conditional])
-    expect { TypeChecker.new(form).check }.to raise_error /Types don't match/
+    expect { TypeChecker.new(form).check }.to raise_error TypeError, /doesn't match/
+  end
+
+  it 'complains if expression of a conditional is not of type boolean' do
+    conditional = If.new(expression: IntegerLiteral.new(3), statements: [])
+    form = Form.new(name: "Test", statements: [conditional])
+    expect { TypeChecker.new(form).check }.to raise_error TypeError, /not a boolean/
+  end
+
+  it 'detects undefined variables' do
+    conditional = If.new(expression: Equal.new(Variable.new("naam"), StringLiteral.new("Geert")), statements: [])
+    form = Form.new(name: "Test", statements: [conditional])
+    expect { TypeChecker.new(form).check }.to raise_error NameError, /naam/ 
   end
 end
