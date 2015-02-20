43a8ca08cc93ba03a31b81f5c38915a28a56cd78
Geert Kapteijns <geertkapteijns@wcw-stud-145-109-26-103.wireless.uva.nl>
2015-02-20 12:13:07 +0100

    runner en evaluator. Ik ga nu beginnen aan de runner:

--- geert.kai/new_ql/lib/ql/evaluator/evaluator.rb
+++ geert.kai/new_ql/lib/ql/evaluator/evaluator.rb
@@ -1,3 +1,57 @@
-class Evaluator
+require_relative "../static_checker/visitor"
+
+class Runner < BaseVisitor
+  def initialize(form)
+    @form = form
+    @values = {}
+  end 
+
+  def next_question
+    "Pietje"
+  end
+
+  def update(variable_name:, value:)
+    @values[variable_name] = value
+  end
+
+  visitor_for Form do |form|
+    form.accept(self)
+  end
+
+  visitor_for Question do |question|
+    if looked_for_question.nil?
+      question 
+    elsif question == looked_for_question
+      looked_for_question = nil
+    else
+  end
+
+end
+
+class Evaluator < BaseVisitor
+  def self.evaluate(expression, values)
+    self.new(expression, values).evaluate
+  end
+
+  def initialize(expression, values)
+    @expression = expression
+    @values = values
+  end
+
+  def evaluate
+    visit(@expression)
+  end
   
+  visitor_for BinaryExpression do |expression|
+    expression.lhs.accept(self).send(expression.operator, expression.rhs.accept(self))
+  end
+
+  visitor_for Variable do |variable|
+    @values[variable.name]
+  end
+
+  visitor_for Literal do |literal|
+    literal.value
+  end
+
 end
