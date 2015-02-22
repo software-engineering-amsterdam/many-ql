45d71b5408a8fbc0a5a2c535d895bee026c26e4d
Santiago Valencia <santiagovalenciavargas@Santiagos-MacBook-Pro.local>
2015-02-22 21:07:19 +0100

    no message

--- Zisimopoulos_Valencia/Software Construction/QL/src/uva/sc/logic/binaryExpressions/Substraction.java
+++ Zisimopoulos_Valencia/Software Construction/QL/src/uva/sc/logic/binaryExpressions/Substraction.java
@@ -6,7 +6,6 @@ public class Substraction extends BinaryExpression{
 
 	public Substraction(Node firstOperand, Node secondOperand) {
 		super(firstOperand, secondOperand);
-		System.out.print("op 1: " + firstOperand + " op 2: " + secondOperand);
 	}
 	
 	public String toString() {
