d78710d733634e298060a1697221243ae62fd95d
Santiago Valencia <santiagovalenciavargas@Santiagos-MacBook-Pro.local>
2015-02-22 16:03:50 +0100

    ast

--- Zisimopoulos_Valencia/Software Construction/QL/src/uva/sc/test/EvalVisitor.java
+++ Zisimopoulos_Valencia/Software Construction/QL/src/uva/sc/test/EvalVisitor.java
@@ -1,25 +1,26 @@
 package uva.sc.test;
 
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.List;
-import java.util.HashMap;
 
 import org.antlr.v4.runtime.misc.NotNull;
 
 import uva.sc.logic.Node;
-import uva.sc.ast.NodeTree;
+import uva.sc.logic.Expression;
 import uva.sc.logic.Form;
+import uva.sc.logic.If_Statement;
 import uva.sc.logic.Literal;
 import uva.sc.logic.Question;
 import uva.sc.logic.Statement;
+import uva.sc.logic.Type;
 import uva.sc.logic.binaryExpressions.*;
 import uva.sc.logic.unaryExpressions.*;
 import uva.sc.parser.GrammarBaseVisitor;
 import uva.sc.parser.GrammarParser;
-import uva.sc.parser.GrammarParser.StatContext;
 
 public class EvalVisitor extends GrammarBaseVisitor<Node> {
+
+/*========================== Parsing blocks ============================*/
 	
 	@Override
 	public Form visitForm(GrammarParser.FormContext ctx) {
@@ -28,8 +29,25 @@ public class EvalVisitor extends GrammarBaseVisitor<Node> {
 			statementList.add((Statement)visitStat(ctx.sts.get(i)));
 		return new Form(ctx.ID().getText(), statementList);
 	}
-
 	
+	@Override
+	public Question visitQuestion(GrammarParser.QuestionContext ctx) {
+		String label = ctx.STRING().getText();
+		String id = ctx.ID().getText();
+		Type type = (Type)this.visit(ctx.type());
+		Expression expression = (Expression)this.visit(ctx.expr());
+		return new Question (label, id, type, expression);
+	}
+	
+	@Override
+	public If_Statement visitIf_stat(GrammarParser.If_statContext ctx) {
+		Expression expr = (Expression)this.visit(ctx.expr());
+		List<Question> questionList = new ArrayList<Question>();
+		for (int i = 0 ; i < ctx.qs.size() ; i++)
+			questionList.add((Question)visitQuestion(ctx.qs.get(i)));
+		return new If_Statement(expr, questionList);
+	}
+
 /*============================ Literals ================================*/
 
 	@Override
@@ -41,17 +59,18 @@ public class EvalVisitor extends GrammarBaseVisitor<Node> {
 	
 	@Override
 	public Node visitNumber(GrammarParser.NumberContext ctx) {
-		return new Literal(Double.valueOf(ctx.getText()));
+		Node result = null;
+		if (ctx.getText().contains("."))
+			result = new Literal(Double.valueOf(ctx.getText()));
+		else
+			result= new Literal(Integer.valueOf(ctx.getText()));
+		return result;
 	}
 	
 	@Override
 	public Node visitBoolean(GrammarParser.BooleanContext ctx) {
 		return new Literal(Boolean.valueOf(ctx.getText()));
 	}
-	
-/*======================================================================*/
-
-
 	
 /*======================== Expressions =================================*/
 	@Override
@@ -119,6 +138,20 @@ public class EvalVisitor extends GrammarBaseVisitor<Node> {
 	}
 	
 	@Override
+	public Node visitEquality(GrammarParser.EqualityContext ctx) {
+		Node result = null;
+		Node firstOperand = this.visit(ctx.expr(0));
+		Node secondOperand = this.visit(ctx.expr(1));
+		switch (ctx.op.getType()) {
+		case GrammarParser.EQ:
+			result = new Equals(firstOperand, secondOperand);
+		case GrammarParser.NEQ:
+			result = new NotEquals(firstOperand, secondOperand);
+		}
+		return result;
+	}
+	
+	@Override
 	public And visitAnd(GrammarParser.AndContext ctx) {
 		Node firstExpression = this.visit(ctx.expr(0));
 		Node secondExpression = this.visit(ctx.expr(1));
@@ -131,19 +164,18 @@ public class EvalVisitor extends GrammarBaseVisitor<Node> {
 		Node secondExpression = this.visit(ctx.expr(1));
 		return new Or(firstExpression, secondExpression);
 	}
+	
+	@Override 
+	public Node visitParenthesis(GrammarParser.ParenthesisContext ctx) { 
+		return this.visit(ctx.expr()); 
+	}
 
-	@Override
+	/*@Override
 	public Addition visitId(GrammarParser.IdContext ctx) {
 		Value returnValue = null;
 		String id = ctx.ID().getText();
 		if (memory.containsKey(id))
 			returnValue = memory.get(id); 
 		return returnValue;
-	}
-	
-	@Override 
-	public Node visitQuestion(GrammarParser.QuestionContext ctx) {
-		return new Question (ctx.STRING(), ctx.ID(), this.visit(ctx.type()), visitChildren(ctx));
-		//return visitChildren(ctx);
-	}
+	}*/
 }
