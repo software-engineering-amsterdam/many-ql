14696f83394062bced3631a5c8d1a381efa31681
Wouter Nederhof <wouter@nedtalk.com>
2015-02-23 14:58:14 +0100

    Added missing model stuff...

--- CalinWouter/src/org/uva/student/calinwouter/qlqls/qls/model/Radio.java
+++ CalinWouter/src/org/uva/student/calinwouter/qlqls/qls/model/Radio.java
@@ -2,12 +2,23 @@ package org.uva.student.calinwouter.qlqls.qls.model;
 
 import org.uva.student.calinwouter.qlqls.ql.interpreter.impl.headless.HeadlessFormInterpreter;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Radio extends AbstractWidget<Radio> {
     @Override
     public void apply(IModel iModel) {
         iModel.caseRadio(this);
+    }
+
+    @Override
+    public void caseHashMap(HashMap<Object, Object> hashMap) {
+        // TODO
+    }
+
+    @Override
+    public void caseString(String string) {
+        // TODO
     }
 
     @Override
