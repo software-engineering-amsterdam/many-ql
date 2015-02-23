5a6cf7968ca9bfb087bd1edb9b927106d6d9ab02
Carlos C <uldericofilho@gmail.com>
2015-02-23 16:47:55 +0100

    HoC Review: Squash all symboltable routines in one

--- carlos.cirello/interpreter/symboltable/table.go
+++ carlos.cirello/interpreter/symboltable/table.go
@@ -1,30 +1,65 @@
 package symboltable
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/ast"
 	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/interpreter/event"
 )
 
-// SymbolTable holds a map with pairs of question identifiers and a reference to
-// it. It is the core of SymbolTable process.
+// SymbolTable is the typechecker specific symbol table which detects duplicated
+// identifiers and labels
 type SymbolTable struct {
 	Events chan *event.Symbol
 
 	symbols map[string]*ast.QuestionNode
+	labels  map[string][]string
+
+	err  []error
+	warn []error
 }
 
-// New is the factory for SymbolTable process. It spawns a background process
-// which listen for queries to its internal map.
 func New(events chan *event.Symbol) *SymbolTable {
 	table := &SymbolTable{
-		Events:  events,
 		symbols: make(map[string]*ast.QuestionNode),
+		labels:  make(map[string][]string),
+		Events:  events,
 	}
 
 	go table.loop()
 	return table
+}
+
+// Err returns all found errors during symbolTable operation
+func (s SymbolTable) Err() []error {
+	return s.err
+}
+
+// Warn returns all found warnings during symbolTable operation
+func (s SymbolTable) Warn() []error {
+	return s.warn
+}
+
+// ShowWarn iterates through all warnings and prints them
+func (s *SymbolTable) ShowWarn() bool {
+	if warn := s.Warn(); warn != nil {
+		for _, e := range warn {
+			log.Printf("warning: %s", e)
+		}
+		return true
+	}
+	return false
+}
+
+// ShowWarn iterates through all errors, prints them and panic in the end
+func (s *SymbolTable) PanicErr() {
+	if err := s.Err(); err != nil {
+		for _, e := range err {
+			log.Println(e)
+		}
+		panic("typechecker errors found")
+	}
 }
 
 func (s *SymbolTable) loop() {
@@ -32,33 +67,57 @@ func (s *SymbolTable) loop() {
 		question, ok := s.symbolExistP(r.Identifier)
 		switch r.Command {
 		default:
-			log.Fatalf("Invalid operation at symbols table: %#v",
-				r.Command)
-
+			s.err = append(s.err, fmt.Errorf(
+				"Invalid operation at typechecker table: %#v",
+				r.Command))
 		case event.SymbolRead:
 			if !ok {
-				log.Fatalf("Identifier unknown at symbols table: %s", r.Identifier)
+				s.appendErrf(
+					"Identifier unknown at typechecker table: %s",
+					r.Identifier)
 			}
 			r.Ret <- question
 
 		case event.SymbolCreate:
 			if !ok {
-				s.upsertSymbol(r.Identifier, r.Content)
+				s.upsert(r.Identifier, r.Content)
+				s.detectRepeatedLabel(r.Content.Label(), r.Identifier)
+			} else {
+				s.appendErrf(
+					"Duplicated identifier found at typechecker: %s",
+					r.Identifier)
 			}
 
 		case event.SymbolUpdate:
 			if ok {
-				s.upsertSymbol(r.Identifier, r.Content)
+				s.upsert(r.Identifier, r.Content)
 			}
 		}
 	}
 }
 
-func (s *SymbolTable) upsertSymbol(identifier string, content *ast.QuestionNode) {
+func (s *SymbolTable) upsert(identifier string, content *ast.QuestionNode) {
 	s.symbols[identifier] = content
 }
 
 func (s *SymbolTable) symbolExistP(identifier string) (question *ast.QuestionNode, ok bool) {
 	question, ok = s.symbols[identifier]
 	return question, ok
+}
+
+func (s *SymbolTable) detectRepeatedLabel(label, identifier string) {
+	if _, ok := s.labels[label]; ok {
+		s.appendWarnf(
+			"Repeated question label at typechecker: %s for %s",
+			label, identifier)
+	}
+	s.labels[label] = append(s.labels[label], identifier)
+}
+
+func (s *SymbolTable) appendErrf(err string, vars ...interface{}) {
+	s.err = append(s.err, fmt.Errorf(err, vars...))
+}
+
+func (s *SymbolTable) appendWarnf(warn string, vars ...interface{}) {
+	s.warn = append(s.warn, fmt.Errorf(warn, vars...))
 }
