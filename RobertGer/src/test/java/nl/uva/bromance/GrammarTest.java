6dfe9657cf2eb496077e456edb52c024b831f0d4
Robert Diebels <robertdiebels@gmail.com>
2015-02-23 20:42:41 +0100

    - GrammarErrorListener message is now clearer. - GrammarTest extended. - Updated grammar to force a question to have a text and an answertype

--- RobertGer/src/test/java/nl/uva/bromance/GrammarTest.java
+++ RobertGer/src/test/java/nl/uva/bromance/GrammarTest.java
@@ -4,9 +4,7 @@ import nl.uva.bromance.listeners.GrammarErrorListener;
 import nl.uva.bromance.listeners.QLParseTreeListener;
 import nl.uva.bromance.parsers.QLLexer;
 import nl.uva.bromance.parsers.QLParser;
-import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Before;
@@ -16,7 +14,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.contentOf;
 
 /**
  * Created by Robert on 10-2-2015.
@@ -27,56 +24,64 @@ public class GrammarTest {
     private ParseTreeWalker walker;
 
     @Before
-    public void setup()
-    {
+    public void setup() {
         listener = new FakeGrammarListener();
         walker = new ParseTreeWalker();
     }
 
-
-    private QLParser.QuestionnaireContext createTree(String content) throws IOException{
-        QLLexer lexer = new QLLexer(new ANTLRInputStream(new ByteArrayInputStream(content.getBytes())));
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(new GrammarErrorListener());
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        QLParser parser = new QLParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(new GrammarErrorListener());
-        return parser.questionnaire();
-    }
-
     @Test(expected = GrammarErrorListener.SyntaxError.class)
-    public void contentContainsQuestionnaireWithoutForms() throws IOException{
+    public void contentContainsQuestionnaireWithoutForms() throws IOException {
         String content = "Name: \"Tax\" {}";
 
         walker.walk(listener, createTree(content));
-
-        assertThat(listener.questionnaireCount).isEqualTo(1);
-        assertThat(listener.formCount).isEqualTo(0);
     }
 
 
     @Test
-    public void questionnaireHasOneFormWithoutBodyContent() throws IOException{
+    public void questionnaireHasOneFormWithoutBodyContent() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n" +
-                "    }}";
+                "    }" +
+                "}";
 
         walker.walk(listener, createTree(content));
 
         assertThat(listener.formCount).isEqualTo(1);
     }
 
-    @Test
-    public void containsMultipleFormsWithoutBodyContent() throws IOException{
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void formHasMalformedFormBody() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n" +
-                "    }"+
+                "    " +
+                "}";
+
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void formHasNoIdentifier() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form:  {\n" +
+                "\n" +
+                "    }" +
+                "}";
+
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test
+    public void containsMultipleFormsWithoutBodyContent() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n" +
-                "    }}";
+                "    }" +
+                "    Form: \"default\" {\n" +
+                "\n" +
+                "    }" +
+                "}";
         walker.walk(listener, createTree(content));
 
         assertThat(listener.formCount).isEqualTo(2);
@@ -84,8 +89,7 @@ public class GrammarTest {
     }
 
     @Test
-    public void formContainsIfStatementWithoutBodyContents() throws IOException
-    {
+    public void formContainsIfStatementWithoutBodyContents() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n     If: something{}" +
@@ -95,9 +99,26 @@ public class GrammarTest {
         assertThat(listener.ifStatementCount).isEqualTo(1);
     }
 
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void malformedIfStatementBody() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     If: something{" +
+                "    }}";
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void ifStatementHasNoExpression() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     If: {}" +
+                "    }}";
+        walker.walk(listener, createTree(content));
+    }
+
     @Test
-    public void formContainsMulitpleIfStatementWithoutBodyContents() throws IOException
-    {
+    public void formContainsMultipleIfStatementWithoutBodyContents() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n     If: something{}" +
@@ -107,9 +128,18 @@ public class GrammarTest {
 
         assertThat(listener.ifStatementCount).isEqualTo(2);
     }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void elseIfStatementWithoutBodyContents() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Else If: something{}" +
+                "    }}";
+        walker.walk(listener, createTree(content));
+    }
+
     @Test
-    public void formContainsIfStatementAndElseIfStatementWithoutBodyContents() throws IOException
-    {
+    public void ifStatementAndElseIfStatementWithoutBodyContents() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n     If: something{}" +
@@ -120,9 +150,9 @@ public class GrammarTest {
         assertThat(listener.ifStatementCount).isEqualTo(1);
         assertThat(listener.elseIfStatementCount).isEqualTo(1);
     }
+
     @Test
-    public void formContainsIfStatementAndMultipleElseIfStatementWithoutBodyContents() throws IOException
-    {
+    public void ifStatementAndMultipleElseIfStatementWithoutBodyContents() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n     If: something{}" +
@@ -148,21 +178,122 @@ public class GrammarTest {
         assertThat(listener.elseStatementCount).isEqualTo(1);
     }
 
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void formContainsElseStatementWithoutBodyContents() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Else:{}" +
+                "    }}";
+        walker.walk(listener, createTree(content));
+    }
+
     @Test
     public void formContainsIfStatementAndElseIfStatementAndElseStatementWithoutBodyContents() throws IOException {
         String content = "Name: \"Tax\" {\n" +
                 "    Form: \"default\" {\n" +
                 "\n     If: something{}" +
                 "\n     Else If: something{}" +
-                "\n     Else If: something{}" +
                 "\n     Else:{}" +
                 "    }}";
         walker.walk(listener, createTree(content));
 
         assertThat(listener.ifStatementCount).isEqualTo(1);
-        assertThat(listener.elseIfStatementCount).isGreaterThan(1);
+        assertThat(listener.elseIfStatementCount).isEqualTo(1);
         assertThat(listener.elseStatementCount).isEqualTo(1);
     }
+
+
+    @Test
+    public void question() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Question: \"question\"{" +
+                "           Text: \"How much money did you earn through employer paid wages during 2014?\"" +
+                "           Answer: integer" +
+                "    }}}";
+
+        walker.walk(listener, createTree(content));
+
+        assertThat(listener.questionCount).isEqualTo(1);
+    }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void questionWithoutText() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Question: \"question\"{" +
+                "           Answer: integer" +
+                "       }" +
+                "    }}";
+
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void questionWithoutAnswerType() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Question: \"question\"{" +
+                "           Text: \"How much money did you earn through employer paid wages during 2014?\"" +
+                "    }}}";
+
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void malformedQuestion() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Question: \"question\"{" +
+                "    }}";
+
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test(expected = GrammarErrorListener.SyntaxError.class)
+    public void questionWithoutIdentifier() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Question: {" +
+                "           Text: \"How much money did you earn through employer paid wages during 2014?\"" +
+                "           Answer: integer" +
+                "       }" +
+                "    }}";
+
+        walker.walk(listener, createTree(content));
+    }
+
+    @Test
+    public void formContainsMultipleQuestions() throws IOException {
+        String content = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "\n     Question: \"question\"{" +
+                "           Text: \"How much money did you earn through employer paid wages during 2014?\"" +
+                "           Answer: integer" +
+                "       }" +
+                "\n     Question: \"question\"{" +
+                "           Text: \"How much money did you earn through employer paid wages during 2014?\"" +
+                "           Answer: integer" +
+                "       }" +
+                "    }}";
+
+        walker.walk(listener, createTree(content));
+
+        assertThat(listener.questionCount).isEqualTo(2);
+    }
+
+    private QLParser.QuestionnaireContext createTree(String content) throws IOException {
+        QLLexer lexer = new QLLexer(new ANTLRInputStream(new ByteArrayInputStream(content.getBytes())));
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(new GrammarErrorListener());
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        QLParser parser = new QLParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(new GrammarErrorListener());
+        return parser.questionnaire();
+    }
+
+
     static class FakeGrammarListener extends QLParseTreeListener {
         public int formCount = 0;
         public int questionCount = 0;
@@ -170,6 +301,7 @@ public class GrammarTest {
         public int ifStatementCount = 0;
         public int elseIfStatementCount = 0;
         public int elseStatementCount = 0;
+        public int questionTextCount = 0;
 
         @Override
         public void exitForm(QLParser.FormContext ftx) {
@@ -186,25 +318,31 @@ public class GrammarTest {
         @Override
         public void exitQuestionnaire(QLParser.QuestionnaireContext ctx) {
             super.exitQuestionnaire(ctx);
-            questionnaireCount +=1;
+            questionnaireCount += 1;
         }
 
         @Override
         public void exitIfStatement(QLParser.IfStatementContext ctx) {
             super.exitIfStatement(ctx);
-            ifStatementCount +=1;
+            ifStatementCount += 1;
         }
 
         @Override
         public void exitElseIfStatement(QLParser.ElseIfStatementContext ctx) {
             super.exitElseIfStatement(ctx);
-            this.elseIfStatementCount +=1;
+            this.elseIfStatementCount += 1;
         }
 
         @Override
         public void exitElseStatement(QLParser.ElseStatementContext ctx) {
             super.exitElseStatement(ctx);
             this.elseStatementCount += 1;
+        }
+
+        @Override
+        public void enterQuestionText(QLParser.QuestionTextContext ctx) {
+            super.enterQuestionText(ctx);
+            this.questionTextCount += 1;
         }
     }
 
