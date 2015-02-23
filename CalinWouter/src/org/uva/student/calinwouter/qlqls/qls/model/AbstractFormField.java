653fe217732ccdd43374852450fb7c614f5ac07d
Calin Borlovan <calin.borlovan@gmail.com>
2015-02-23 12:04:42 +0100

    few more getters added

--- CalinWouter/src/org/uva/student/calinwouter/qlqls/qls/model/AbstractFormField.java
+++ CalinWouter/src/org/uva/student/calinwouter/qlqls/qls/model/AbstractFormField.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 // TODO check if invoking this model fails the interpreter.
 public abstract class AbstractFormField<T> extends AbstractModel<T> {
     private String ident;
-    private HashMap<Object, Object> arguments;
-    int arg;
+    protected HashMap<Object, Object> arguments;
+    protected int arg;
 
     @Override
     public void caseString(String string) {
