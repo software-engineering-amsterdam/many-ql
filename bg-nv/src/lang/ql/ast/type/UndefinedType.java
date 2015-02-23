6c483b6bc8d1b6d3bd42bce099a4deaab38339d9
Boryana <boryana.goncharenko@gmail.com>
2015-02-23 21:04:58 +0100

    added undefined type; refactorered type checker

---
+++ bg-nv/src/lang/ql/ast/type/UndefinedType.java
@@ -0,0 +1,8 @@
+package lang.ql.ast.type;
+
+/**
+ * Created by bore on 23/02/15.
+ */
+public class UndefinedType
+{
+}
---
+++ bg-nv/src/lang/ql/semantics/LabelMap.java
@@ -0,0 +1,8 @@
+package lang.ql.semantics;
+
+/**
+ * Created by bore on 23/02/15.
+ */
+public class LabelMap
+{
+}
