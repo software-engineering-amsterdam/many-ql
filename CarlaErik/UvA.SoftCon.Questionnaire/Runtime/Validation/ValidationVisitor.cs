5387ff111a131be93b697efa618360ac68edaf43
verhoofstad <verhoofstad@hotmail.com>
2015-02-23 16:17:42 +0100

    Added lab work

---
+++ CarlaErik/UvA.SoftCon.Questionnaire/Runtime/Validation/ValidationVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UvA.SoftCon.Questionnaire.AST;
+
+namespace UvA.SoftCon.Questionnaire.Runtime.Validation
+{
+    public abstract class ValidationVisitor : ASTVisitor
+    {
+
+
+
+    }
+}
