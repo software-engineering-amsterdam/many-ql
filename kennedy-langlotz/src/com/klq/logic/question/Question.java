d2c0fa39809ebc3a24e1feccc706dc69604d0c43
Timon Langlotz <timon.langlotz@gmail.com>
2015-02-24 10:24:21 +0100

    Added Dependency object to Question, some work on Store

--- kennedy-langlotz/src/com/klq/logic/question/Question.java
+++ kennedy-langlotz/src/com/klq/logic/question/Question.java
@@ -2,19 +2,23 @@ package com.klq.logic.question;
 
 import com.klq.logic.IKLQItem;
 
+import java.util.List;
+
 /**
  * Created by Timon on 10.02.2015.
  */
 public class Question implements IKLQItem {
-    private Type type;
-    private OptionSet options;
-    private Text text;
+    private final Type type;
+    private final OptionSet options;
+    private final Text text;
+    private final List<Dependency> dependencies;
     private Answer result;
 
-    public Question (Type type, OptionSet options, Text text){
+    public Question (Type type, OptionSet options, Text text, List<Dependency> dependencies){
         this.type = type;
         this.options = options;
         this.text = text;
+        this.dependencies = dependencies;
     }
 
     public Type getType() {
@@ -27,5 +31,17 @@ public class Question implements IKLQItem {
 
     public Text getText() {
         return text;
+    }
+
+    public List<Dependency> getDependencies() {
+        return dependencies;
+    }
+
+    public void setResult(Answer result) {
+        this.result = result;
+    }
+
+    public Answer getResult() {
+        return result;
     }
 }
