9eaddd8a0cd6f131a11a9a6c77f6409e4061fca6
Guido Loupias <guidouva@outlook.com>
2015-02-24 21:54:07 +0100

    renaming

--- TeamAwesome/ql/Visitor.py
+++ TeamAwesome/ql/Visitor.py
@@ -1,10 +1,10 @@
 from . import Dispatch
 
 class Visitor:
-    def visit(self, obj):
+    def visit(self, subject):
         return Dispatch.onClass(
             self,
-            obj,
+            subject,
             lambda className:
                 '_visit' + className[0].upper() + className[1:]
         )
