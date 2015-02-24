1e522c965ee2532168622ac8b73fb7bdd40d4257
Guido Loupias <guidouva@outlook.com>
2015-02-24 20:16:24 +0100

    expand visitor to visit into atomic nodes

--- TeamAwesome/ql/GenericVisitor.py
+++ TeamAwesome/ql/GenericVisitor.py
@@ -9,8 +9,10 @@ class GenericVisitor:
     # Python will raise an AttributeError if getattr() fails to find
     # the method.
     def _dispatch(self, obj):
+        className = obj.__class__.__name__
+        destinationName = className[0].upper() + className[1:]
         method = getattr(
             self,
-            '_visit' + obj.__class__.__name__
+            '_visit' + destinationName
         )
         return method(obj)
