b6ae9b0d6e1669db108c6e2c69bfd43fe48a12a9
Calin Borlovan <calin.borlovan@gmail.com>
2015-02-24 19:19:20 +0100

    Textbox widget implemented.

--- CalinWouter/src/org/uva/student/calinwouter/qlqls/application/gui/qls/widgets/QuestionWidgetFetcher.java
+++ CalinWouter/src/org/uva/student/calinwouter/qlqls/application/gui/qls/widgets/QuestionWidgetFetcher.java
@@ -1,12 +1,13 @@
 package org.uva.student.calinwouter.qlqls.application.gui.qls.widgets;
 
 import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.boolwidgets.CheckboxWidget;
+import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.boolwidgets.ComboboxWidget;
 import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.boolwidgets.RadioWidget;
-import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.intwidgets.IntboxWidget;
 import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.intwidgets.SliderWidget;
 import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.intwidgets.SpinboxWidget;
-import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.stringwidgets.TextboxWidget;
+import org.uva.student.calinwouter.qlqls.application.gui.qls.widgets.question.textwidgets.TextboxWidget;
 import org.uva.student.calinwouter.qlqls.ql.interpreter.impl.headless.HeadlessFormInterpreter;
+import org.uva.student.calinwouter.qlqls.ql.interpreter.impl.typechecker.FormTypeChecker;
 import org.uva.student.calinwouter.qlqls.qls.model.interfaces.IQuestionWidgetCallback;
 import org.uva.student.calinwouter.qlqls.qls.model.functions.Question;
 
@@ -15,11 +16,18 @@ import java.awt.*;
 public class QuestionWidgetFetcher implements IQuestionWidgetCallback, IWidget {
     private IWidget widget;
     private HeadlessFormInterpreter headlessFormInterpreter;
+    private FormTypeChecker formTypeChecker;
 
     @Override
     public void caseCheckboxWidget(Question question) {
         widget = new LabelWithWidgetWidget(question,
                 new CheckboxWidget(question, headlessFormInterpreter), headlessFormInterpreter);
+    }
+
+    @Override
+    public void caseComboboxWidget(Question question) {
+        widget = new LabelWithWidgetWidget(question,
+                new ComboboxWidget(question,headlessFormInterpreter), headlessFormInterpreter);
     }
 
     @Override
@@ -35,12 +43,6 @@ public class QuestionWidgetFetcher implements IQuestionWidgetCallback, IWidget {
     }
 
     @Override
-    public void caseIntboxWidget(Question question) {
-        widget = new LabelWithWidgetWidget(question,
-                new IntboxWidget(question, headlessFormInterpreter), headlessFormInterpreter);
-    }
-
-    @Override
     public void caseSpinboxWidget(Question question) {
         widget = new LabelWithWidgetWidget(question,
                 new SpinboxWidget(question, headlessFormInterpreter), headlessFormInterpreter);
@@ -49,7 +51,8 @@ public class QuestionWidgetFetcher implements IQuestionWidgetCallback, IWidget {
     @Override
     public void caseTextboxWidget(Question question) {
         widget = new LabelWithWidgetWidget(question,
-                new TextboxWidget(question, headlessFormInterpreter), headlessFormInterpreter);
+                new TextboxWidget(question, headlessFormInterpreter, formTypeChecker.getTypeDescriptor(question.getFieldName())),
+                headlessFormInterpreter);
     }
 
     @Override
@@ -57,7 +60,8 @@ public class QuestionWidgetFetcher implements IQuestionWidgetCallback, IWidget {
         return widget.getWidget();
     }
 
-    public QuestionWidgetFetcher(HeadlessFormInterpreter headlessFormInterpreter) {
+    public QuestionWidgetFetcher(HeadlessFormInterpreter headlessFormInterpreter, FormTypeChecker formTypeChecker) {
         this.headlessFormInterpreter = headlessFormInterpreter;
+        this.formTypeChecker = formTypeChecker;
     }
 }
