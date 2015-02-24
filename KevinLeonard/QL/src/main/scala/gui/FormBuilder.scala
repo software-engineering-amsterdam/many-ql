a15da74652b387173721c324703aff0d2e64c919
leonardpunt <leonard@q42.nl>
2015-02-24 19:39:13 +0100

    Add EvalEnvironment to FormBuilder.

--- KevinLeonard/QL/src/main/scala/gui/FormBuilder.scala
+++ KevinLeonard/QL/src/main/scala/gui/FormBuilder.scala
@@ -4,35 +4,53 @@ import javafx.beans.value.ObservableValue
 
 import ast._
 
+import scala.collection.immutable.Map
 import scalafx.Includes._
 import scalafx.scene.Node
-import scalafx.scene.control.{Label, TextField}
+import scalafx.scene.control.{CheckBox, Label, TextField}
 import scalafx.scene.layout.VBox
 
 class FormBuilder {
 
-  def build(form: Form): FormGUI = {
-    new FormGUI(form.label, build(form.s))
+  type VariableName = String
+  type EvalEnvironment = Map[VariableName, Value]
+
+  def build(form: Form, env: EvalEnvironment): FormGUI = {
+    new FormGUI(form.label, build(form.s, env))
   }
 
-  def build(s: Statement): List[Node] = s match {
-    case Sequence(statements: List[Statement]) => statements.flatMap(s => build(s))
+  def build(s: Statement, env: EvalEnvironment): List[Node] = s match {
+    case Sequence(statements: List[Statement]) => statements.flatMap(s => build(s, env))
     //case i: IfStatement => Evaluation stuff + build the rights sequence body.
     case q: Question => q._type match {
-      case StringType() => List(addVBox(q.label, new TextField()))
-      case BooleanType() => List(addVBox(q.label, new TextField()))
-      case NumberType() => List(addVBox(q.label, new TextField()))
+      case StringType() => List(addTextField(q.label, q.variable.name, env))
+      case BooleanType() => List(addCheckBox(q.label))
+      case NumberType() => List(addTextField(q.label, q.variable.name, env))
     }
     case _ => List()
   }
 
-  def addVBox(l: String, field: Node): VBox = {
+  def addTextField(l: String, name: VariableName, env: EvalEnvironment): VBox = {
     val box = new VBox()
     val label = new Label(l)
-    val field = new TextField()
+    val field = new TextField {
+      text = env get name match {
+        case Some(StringValue(v)) => v
+        case Some(NumberValue(v)) => v.toString
+        case None => throw new AssertionError("Error in evaluator. Variable not found.")
+      }
+    }
     field.text.addListener(
       (obs: ObservableValue[_ <: Object], oldV: Object, newV: Object) => println(newV)
     )
+    box.children.addAll(label, field)
+    box
+  }
+
+  def addCheckBox(l: String): VBox = {
+    val box = new VBox()
+    val label = new Label(l)
+    val field = new CheckBox()
     box.children.addAll(label, field)
     box
   }
