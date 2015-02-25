38805cfb91351a46de3768e927d4f08d44e9984b
Carlos C <uldericofilho@gmail.com>
2015-02-25 19:17:54 +0100

    Fork QLS from QL

---
+++ carlos.cirello/parser/qls/readqls.go
@@ -0,0 +1,22 @@
+//go:generate -command yacc go tool yacc
+//go:generate yacc -o parser.go -p "qls" parser.y
+
+/*
+Package parser uses native Go's yacc to parse the QLS source code.
+Internally, it uses a customized lexer to walk throughout the tokens
+of the source code.
+*/
+package parser
+
+import (
+	"io"
+
+	"github.com/software-engineering-amsterdam/many-ql/carlos.cirello/ast"
+)
+
+// ReadQLS generates an AST out of source code.
+func ReadQLS(stream io.Reader, fn string) *ast.QuestionaireNode {
+	finalQuestionaire = nil
+	qlsParse(newLexer(stream, fn))
+	return finalQuestionaire
+}
