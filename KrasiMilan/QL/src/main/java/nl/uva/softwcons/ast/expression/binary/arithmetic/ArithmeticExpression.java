475ed63ac7c2ede28a9896341cb28d5618942108
krasimira-bozhanova <krasimira_bozhanova@abv.bg>
2015-02-28 22:18:08 +0100

    Fix types in expressions

--- KrasiMilan/QL/src/main/java/nl/uva/softwcons/ast/expression/binary/arithmetic/ArithmeticExpression.java
+++ KrasiMilan/QL/src/main/java/nl/uva/softwcons/ast/expression/binary/arithmetic/ArithmeticExpression.java
@@ -3,7 +3,10 @@ package nl.uva.softwcons.ast.expression.binary.arithmetic;
 import nl.uva.softwcons.ast.LineInfo;
 import nl.uva.softwcons.ast.expression.Expression;
 import nl.uva.softwcons.ast.expression.binary.BinaryExpression;
+import nl.uva.softwcons.ast.type.DecimalType;
+import nl.uva.softwcons.ast.type.IntegerType;
 import nl.uva.softwcons.ast.type.Type;
+import nl.uva.softwcons.ast.type.UndefinedType;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
@@ -11,10 +14,10 @@ import com.google.common.collect.Table;
 public abstract class ArithmeticExpression extends BinaryExpression {
     private static final Table<Type, Type, Type> ARITHMENTIC_OPERATORS_TABLE = HashBasedTable.create();
     static {
-        ARITHMENTIC_OPERATORS_TABLE.put(Type.INTEGER, Type.INTEGER, Type.INTEGER);
-        ARITHMENTIC_OPERATORS_TABLE.put(Type.INTEGER, Type.DECIMAL, Type.DECIMAL);
-        ARITHMENTIC_OPERATORS_TABLE.put(Type.DECIMAL, Type.INTEGER, Type.DECIMAL);
-        ARITHMENTIC_OPERATORS_TABLE.put(Type.DECIMAL, Type.DECIMAL, Type.DECIMAL);
+        ARITHMENTIC_OPERATORS_TABLE.put(IntegerType.instance, IntegerType.instance, IntegerType.instance);
+        ARITHMENTIC_OPERATORS_TABLE.put(IntegerType.instance, DecimalType.instance, DecimalType.instance);
+        ARITHMENTIC_OPERATORS_TABLE.put(DecimalType.instance, IntegerType.instance, DecimalType.instance);
+        ARITHMENTIC_OPERATORS_TABLE.put(DecimalType.instance, DecimalType.instance, DecimalType.instance);
     }
     private final LineInfo lineInfo;
 
@@ -26,7 +29,7 @@ public abstract class ArithmeticExpression extends BinaryExpression {
 
     public static Type resolveType(final Type type, final Type otherType) {
         final Type resolvedType = ARITHMENTIC_OPERATORS_TABLE.get(type, otherType);
-        return resolvedType != null ? resolvedType : Type.UNDEFINED;
+        return resolvedType != null ? resolvedType : UndefinedType.instance;
     }
 
     @Override
