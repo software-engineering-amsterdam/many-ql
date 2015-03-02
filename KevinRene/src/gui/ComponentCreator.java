1fd24540d7c5eac16ecc9205f010354ba84d662c
Kevin A <kevin_son@live.nl>
2015-03-02 10:49:12 +0100

    Removed the instanceofs by creating a WidgetFactory, which implements the visitors

--- KevinRene/src/gui/ComponentCreator.java
+++ KevinRene/src/gui/ComponentCreator.java
@@ -1,8 +1,5 @@
 package gui;
 
-import gui.widgets.IntegerField;
-import gui.widgets.RadioButton;
-import gui.widgets.TextField;
 import gui.widgets.Widget;
 
 import java.awt.Container;
@@ -12,13 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
-import cons.ValueEnvironment;
 import cons.ql.ast.ASTNode;
-import cons.ql.ast.expression.Identifier;
-import cons.ql.ast.expression.QLType;
-import cons.ql.ast.expression.type.QLBoolean;
-import cons.ql.ast.expression.type.QLInteger;
-import cons.ql.ast.expression.type.QLString;
 import cons.ql.ast.statement.ComputedQuestion;
 import cons.ql.ast.statement.If;
 import cons.ql.ast.statement.IfElse;
@@ -52,9 +43,9 @@ public class ComponentCreator implements StatementVisitor<Void>, ExpressionVisit
 	@Override
 	public Void visit(ComputedQuestion compQuestionNode) {
     	addLabel(compQuestionNode.getText().toString(), pane);
-    	
-    	Widget comp = createComponent(compQuestionNode.getType(), compQuestionNode.getIdentifier(), 
-    			controller, false);
+
+    	Widget comp = compQuestionNode.getType().accept(
+				new WidgetFactory(compQuestionNode.getIdentifier(), controller, false));
     	pane.add(comp.getComponent(), "wrap");
     	
     	ComputedQuestionObserver observer = 
@@ -68,8 +59,9 @@ public class ComponentCreator implements StatementVisitor<Void>, ExpressionVisit
 	public Void visit(Question questionNode) {
 		addLabel(questionNode.getText().toString(), pane);
 		
-		Widget comp = createComponent(questionNode.getType(), questionNode.getIdentifier(), controller);
-			
+		Widget comp = questionNode.getType().accept(
+				new WidgetFactory(questionNode.getIdentifier(), controller));
+		
 		controller.putComponent(questionNode.getIdentifier(), comp);
 	    pane.add(comp.getComponent(), "wrap");
     	
@@ -105,22 +97,6 @@ public class ComponentCreator implements StatementVisitor<Void>, ExpressionVisit
 		pane.add(elsePanel, "span");
 		
 		return null;
-	}
-	
-	private Widget createComponent(QLType type, Identifier identifier, Controller controller, boolean enabled) {
-		if (type instanceof QLString) {
-			return new TextField(identifier, controller, enabled);		
-		}
-		else if (type instanceof QLInteger) {
-			return new IntegerField(identifier, controller, enabled);
-		}
-		else if (type instanceof QLBoolean) {
-			return new RadioButton(identifier, controller, enabled);
-		}
-		return null; 
-	}
-	private Widget createComponent(QLType type, Identifier identifier, Controller controller) {
-		return createComponent(type, identifier, controller, true);
 	}
 	
 	private void addLabel(String text, Container pane) {
