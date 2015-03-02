4e09013ea34ee15873f7790f8eb75316a67f768f
basmeesters <meesters.bas@gmail.com>
2015-03-02 15:04:52 +0100

    Added factories and started AST QLS

--- guybas/QLS/AST/sheet.py
+++ guybas/QLS/AST/sheet.py
@@ -2,12 +2,48 @@
 
 
 class Sheet:
-    pass
+    def __init__(self, name, pages):
+        self.name = name
+        self.pages = pages
+
+    def pretty_print(self, level=0):
+        s = "    " * level + "Sheet " + self.name + "\n"
+        for p in self.pages:
+            s += p.pretty_print(level + 1)
+        return s
 
 
 class Page:
-    pass
+    def __init__(self, name, sections):
+        self.name = name
+        self.sections = sections
+
+    def pretty_print(self, level=0):
+        s = "    " * level + "Page " + self.name + "\n"
+        for p in self.sections:
+            s += p.pretty_print(level+1)
+        return s
 
 
 class Section:
-    pass
+    def __init__(self, name, question_styles):
+        self.name = name
+        self.question_styles = question_styles
+
+    def pretty_print(self, level=0):
+        s = "    " * level + "Section " + self.name + "\n"
+        for p in self.question_styles:
+            s += p.pretty_print(level + 1)
+        return s
+
+
+class QuestionStyle:
+    def __init__(self, qid, widget):
+        self.id = qid
+        self.widget = widget
+
+    def pretty_print(self, level=0):
+        s = "    " * level + "Question " + self.id + "\n"
+        s += self.widget.pretty_print(level + 1)
+        s += "\n"
+        return s
