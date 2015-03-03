803e64f4802b6bb8f2194d604eb10464ac7c56d6
Geert Kapteijns <geertkapteijns@wcw-stud-145-109-27-66.wireless.uva.nl>
2015-03-03 12:56:30 +0100

    hoi

--- geert.kai/qls/lib/qls/ast/stylesheet.rb
+++ geert.kai/qls/lib/qls/ast/stylesheet.rb
@@ -1,28 +1,29 @@
 module AST
 	class Stylesheet
-		attr_reader :name, :children, :defaults
+		attr_reader :name, :defaults
 
 		def initialize(name, stylesheet_rules)
 			@name = name
       # Kan die AST:: weg, omdat we in de module AST zelf zitten?
       # @children moet niet @pages zijn?
+      # rule.instance_of? En moet dit niet 'netter'?
 			@defaults, @children = stylesheet_rules.partition { |rule| rule.class == AST::Default }
 		end
 
 		def pages
-			children
+			@children
 		end
 	end
 
 	class Page < Stylesheet
 		def sections
-			children
+			@children
 		end
 	end
 
 	class Section < Stylesheet
 		def questions
-			children
+			@children
 		end
 	end
 end
