628b4209d0a1417b805965fa82642c51dc719d5f
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-03-03 11:51:07 +0100

    Create empty Visitor for the QLS.

---
+++ Fugazi/src/org/fugazi/qls/ast/AbstractASTNode.java
@@ -0,0 +1,21 @@
+package org.fugazi.qls.ast;
+
+public abstract class AbstractASTNode {
+
+    protected final int lineNumber;
+
+    public static final int DUMMY_LINE_NUMBER = -1;
+
+    public AbstractASTNode() {
+        // this is used for temporary objects
+        this.lineNumber = DUMMY_LINE_NUMBER;
+    }
+
+    public AbstractASTNode(int _lineNum) {
+        this.lineNumber = _lineNum;
+    }
+
+    public int getLineNumber() {
+        return this.lineNumber;
+    }
+}
