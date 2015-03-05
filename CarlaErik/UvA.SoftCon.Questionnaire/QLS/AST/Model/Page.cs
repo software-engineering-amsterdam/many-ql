0b0bbdc320e897b6ce33f012e0ceab935fbb5dc7
verhoofstad <verhoofstad@hotmail.com>
2015-03-05 20:16:04 +0100

    Added AST classes for QLS.

---
+++ CarlaErik/UvA.SoftCon.Questionnaire/QLS/AST/Model/Page.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UvA.SoftCon.Questionnaire.Utilities.AST;
+
+namespace UvA.SoftCon.Questionnaire.QLS.AST.Model
+{
+    public class Page : QLSNode
+    {
+        public ICollection<Section> Sections
+        {
+            get;
+            private set;
+        }
+
+        public Page(ICollection<Section> sections, TextPosition position)
+            : base(position)
+        {
+            Sections = sections;
+        }
+    }
+}
