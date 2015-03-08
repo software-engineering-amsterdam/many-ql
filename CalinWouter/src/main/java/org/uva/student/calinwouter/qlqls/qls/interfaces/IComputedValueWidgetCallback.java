6f124a55ed4cf6d92d882fb3a0f577cdc0cdc10d
Wouter Nederhof <wouter@nedtalk.com>
2015-03-08 03:03:40 +0100

    Reworked QLS and removed files

--- CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/qls/interfaces/IComputedValueWidgetCallback.java
+++
@@ -1,9 +0,0 @@
-package org.uva.student.calinwouter.qlqls.qls.interfaces;
-
-import org.uva.student.calinwouter.qlqls.qls.model.components.ComputedValue;
-
-public interface IComputedValueWidgetCallback {
-
-    public void caseLabelWidget(ComputedValue computedValue);
-
-}
