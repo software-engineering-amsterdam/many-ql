711280f28d8e67321731ce5357759d2549e98b90
Milan Milanov <whizzer.me@gmail.com>
2015-03-08 17:41:47 +0100

    Implement update value propagation and listeners notification in Evaluator

--- KrasiMilan/QL/src/test/java/helper/TestHelper.java
+++ KrasiMilan/QL/src/test/java/helper/TestHelper.java
@@ -1,6 +1,7 @@
 package helper;
 
 import nl.uva.softwcons.ql.ast.LineInfo;
+import nl.uva.softwcons.ql.ast.expression.identifier.Identifier;
 
 public final class TestHelper {
 
@@ -8,6 +9,10 @@ public final class TestHelper {
     }
 
     public static final LineInfo DUMMY_LINE_INFO = new LineInfo(-1, -1);
+    public static final Identifier UNUSED = new Identifier("unused", DUMMY_LINE_INFO);
+    public static final Identifier QUESTION = new Identifier("question", DUMMY_LINE_INFO);
+    public static final Identifier QUESTION2 = new Identifier("question2", DUMMY_LINE_INFO);
+    public static final Identifier QUESTION3 = new Identifier("question3", DUMMY_LINE_INFO);
 
     public static String buildForm(final String formName, final String... statements) {
         return String.format("form %s {%n %s %n}", formName, String.join("\n", statements));
