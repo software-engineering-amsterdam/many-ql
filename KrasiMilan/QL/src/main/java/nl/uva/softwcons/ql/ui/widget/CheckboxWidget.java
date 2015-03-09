a620b860f3d8a3464e539375d6bae13f2f274e68
Milan Milanov <whizzer.me@gmail.com>
2015-03-10 00:51:10 +0100

    Start using converters in UI widgets

--- KrasiMilan/QL/src/main/java/nl/uva/softwcons/ql/ui/widget/CheckboxWidget.java
+++ KrasiMilan/QL/src/main/java/nl/uva/softwcons/ql/ui/widget/CheckboxWidget.java
@@ -2,16 +2,26 @@ package nl.uva.softwcons.ql.ui.widget;
 
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.CheckBox;
 import nl.uva.softwcons.ql.eval.value.Value;
+import nl.uva.softwcons.ql.ui.conveter.ValueConverter;
 
 public class CheckboxWidget extends Widget {
     private CheckBox checkBox;
     private Property<Value> valueProperty;
 
-    public CheckboxWidget(final String checkString) {
+    public CheckboxWidget(final String checkString, final ValueConverter<Boolean> converter) {
         this.checkBox = new CheckBox(checkString);
         this.valueProperty = new SimpleObjectProperty<Value>();
+
+        this.checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                valueProperty.setValue(converter.toValue(newValue));
+            }
+        });
     }
 
     @Override
