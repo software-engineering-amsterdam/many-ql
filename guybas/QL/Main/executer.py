374a7f3c72a94b56d24f797aef86937b216eeed1
basmeesters <meesters.bas@gmail.com>
2015-03-09 13:14:19 +0100

    QLS type checker + properties

--- guybas/QL/Main/executer.py
+++ guybas/QL/Main/executer.py
@@ -1,14 +1,11 @@
 import QL.Grammar.form as form
 import QL.Runtime.gui as g
 import QL.Validators.type_checker as type_checker
-import QL.Runtime.form as rform
 
 formAsParseResults = form.FormFormat.form.ignore(form.basic_types.BasicTypes.comment).parseFile("example.ql")
 form = form.forms.FormFactory.make_form(formAsParseResults)
 
 typeChecker = type_checker.TypeChecker(form)
-#print(type(form.get_statements()[0]))
-#run_time_form = rform.RForm(form.get_name(), form.get_introduction(), form.get_statements())
 
 gui = g.QuestionnaireGUI(form)
 gui.generate_gui()
