64036e67c37bf175a3498e8b9ac2085cc7a6e747
Zarinka <efendijeva@gmail.com>
2015-03-10 00:08:07 +0100

    Fixed display of errors in typechecker

--- Zarina/TaZQL/src/typechecker/errors/ErrorDialog.java
+++ Zarina/TaZQL/src/typechecker/errors/ErrorDialog.java
@@ -3,6 +3,7 @@ package typechecker.errors;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.List;
 
 import javax.swing.JDialog;
@@ -12,6 +13,8 @@ import javax.swing.JLabel;
 public class ErrorDialog {
 	private List<TaZQLError> errorCollection;
 	private List<TaZQLWarning> warningCollection;
+	private JLabel errorLabel, warningLabel;
+	
 	
 	public ErrorDialog(List<TaZQLError> errorCollection, List<TaZQLWarning> warningCollection) {
 		this.errorCollection = errorCollection;
@@ -22,30 +25,49 @@ public class ErrorDialog {
 	public void errorDialog() {	
 		JDialog dialog = new JDialog();
 		Container contentPane = dialog.getContentPane();
+		contentPane.setLayout(new GridLayout(0, 1));
+		errorLabel = new JLabel("Errors found", JLabel.CENTER);
+		errorLabel.setForeground(Color.red);
+		errorLabel.setVisible(false);
+		contentPane.add(errorLabel);
 		
 		for(TaZQLError errorMessage : this.errorCollection) {
+			if (errorMessage !=null) {
+				errorLabel.setVisible(true);
+			}
+			System.out.println("Errors: " + errorMessage);
 			contentPane.add(addErrorLabel(errorMessage));
+		
 		}
 		
+		warningLabel = new JLabel("Warnings found", JLabel.CENTER);
+		warningLabel.setForeground(Color.red);
+		warningLabel.setVisible(false);
+		contentPane.add(warningLabel);
+		
 		for(TaZQLWarning warningMessage : this.warningCollection) {
+			if (warningMessage !=null) {
+				warningLabel.setVisible(true);
+			}
 			contentPane.add(addWarningLabel(warningMessage));
 		}
 		
 		contentPane.setBackground(Color.white);
+		contentPane.setVisible(true);
 		dialog.setTitle("Typechecker: errors found");
 		dialog.pack();
 		dialog.setLocationRelativeTo(null);
 		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		dialog.setMinimumSize(new Dimension(300,100));
+		dialog.setMinimumSize(new Dimension(400,200));
 		dialog.setVisible(true);
 	}
 	
 	public JLabel addErrorLabel(TaZQLError error) {
-		return new JLabel(error.getErrorMessage());
+		return new JLabel("  " + error.getErrorMessage() + " ");
 	}
 	
 	public JLabel addWarningLabel(TaZQLWarning warn) {
-		return new JLabel(warn.getWarningMessage());
+		return new JLabel("  "+ warn.getWarningMessage() + " ");
 	}
 
 }
