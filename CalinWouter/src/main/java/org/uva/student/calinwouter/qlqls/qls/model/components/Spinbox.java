8aa1e8daa1209aaa64a86853fc7b24d1727aa6c3
Wouter Nederhof <wouter@nedtalk.com>
2015-03-09 15:09:21 +0100

    QLS fully working again\!

--- CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/qls/model/components/Spinbox.java
+++ CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/qls/model/components/Spinbox.java
@@ -1,5 +1,13 @@
 package org.uva.student.calinwouter.qlqls.qls.model.components;
 
-public class Spinbox {
+import org.uva.student.calinwouter.qlqls.qls.abstractions.AbstractWidget;
+import org.uva.student.calinwouter.qlqls.qls.interfaces.IQuestionWidgetCallback;
 
+public class Spinbox extends AbstractWidget {
+
+
+    @Override
+    public void applyWidget(final IQuestionWidgetCallback widgetCallback) {
+        widgetCallback.widgetIsSpinboxWidget(this);
+    }
 }
