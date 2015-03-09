fe575cd9b2b8e1a1bcf30a75664654a78b8749b7
haren <lukasz.harezlak@gmail.com>
2015-03-09 17:55:00 +0100

    Added supported question types to widgets.

--- Fugazi/src/org/fugazi/qls/ast/widget/Slider.java
+++ Fugazi/src/org/fugazi/qls/ast/widget/Slider.java
@@ -1,7 +1,14 @@
 package org.fugazi.qls.ast.widget;
 
+import org.fugazi.ql.ast.type.BoolType;
+import org.fugazi.ql.ast.type.IntType;
+import org.fugazi.ql.ast.type.StringType;
+import org.fugazi.ql.ast.type.Type;
 import org.fugazi.qls.ast.IQLSASTVisitor;
 import org.fugazi.qls.ast.style.Style;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Slider extends Widget {
 
@@ -15,6 +22,13 @@ public class Slider extends Widget {
     @Override
     public void applyStyle(Style _style) {
         // todo
+    }
+
+    public List<Type> getSupportedQuestionTypes() {
+        List<Type> supportedTypes = new ArrayList<>();
+        supportedTypes.add(new IntType());
+
+        return supportedTypes;
     }
 
     public <T> T accept(IQLSASTVisitor<T> _visitor) {
