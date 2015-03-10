3081a2dcef5836b9706c34535d9bb5445a1ed17b
Calin Borlovan <calin.borlovan@gmail.com>
2015-03-10 13:25:47 +0100

    QL refactorings and merged with QLS

--- CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/ql/model/ComputedValueField.java
+++ CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/ql/model/ComputedValueField.java
@@ -1,6 +1,5 @@
 package org.uva.student.calinwouter.qlqls.ql.model;
 
-import org.uva.student.calinwouter.qlqls.ql.interpreter.FormInterpreter;
 import org.uva.student.calinwouter.qlqls.ql.interpreter.TypeDescriptor;
 import org.uva.student.calinwouter.qlqls.ql.interpreter.impl.headless.HeadlessFormInterpreter;
 
--- CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/ql/model/FormField.java
+++ CalinWouter/src/main/java/org/uva/student/calinwouter/qlqls/ql/model/FormField.java
@@ -1,6 +1,5 @@
 package org.uva.student.calinwouter.qlqls.ql.model;
 
-import org.uva.student.calinwouter.qlqls.ql.interpreter.FormInterpreter;
 import org.uva.student.calinwouter.qlqls.ql.interpreter.impl.headless.HeadlessFormInterpreter;
 import org.uva.student.calinwouter.qlqls.ql.types.Value;
 
