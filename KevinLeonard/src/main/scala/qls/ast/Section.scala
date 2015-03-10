2dd371be43a4ff63e5c726f32aa7c47c6b9ede20
Kevin van Cleef <kevin@developmentstudio.nl>
2015-03-10 10:54:16 +0100

    Fixed some missing defaults in QLS. Adds parser + AST classes till section level.

--- KevinLeonard/src/main/scala/qls/ast/Section.scala
+++ KevinLeonard/src/main/scala/qls/ast/Section.scala
@@ -3,4 +3,4 @@ package qls.ast
 /**
  * Created by DevelopmentStudio on 09-03-15.
  */
-case class Section(title: String, widget: Widget)
+case class Section(title: String, questions: QuestionSequence)
