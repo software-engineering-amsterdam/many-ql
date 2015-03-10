424f9b6c4b3b715fce8a89d429498bd6915e09bc
Steven Kok <stevenen@Live.nl>
2015-03-10 10:41:26 +0100

    Question has list of styles

--- Steven/src/main/java/edu/parser/QL/nodes/question/Question.java
+++ Steven/src/main/java/edu/parser/QL/nodes/question/Question.java
@@ -1,13 +1,17 @@
 package edu.parser.QL.nodes.question;
 
+import edu.exceptions.ParseException;
 import edu.nodes.QuestionType;
+import edu.nodes.styles.Style;
 import edu.parser.QL.QLVisitor;
 import edu.parser.QL.nodes.AbstractNode;
 import edu.parser.QL.nodes.expression.Expression;
 import edu.parser.QL.nodes.expression.Identifier;
 import edu.parser.QL.nodes.statement.Statement;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by Steven Kok on 21/02/2015.
@@ -19,13 +23,15 @@ public class Question extends Statement implements Cloneable {
     private final Label label;
     private final Optional<Expression> expression;
     private final boolean isEnabled; //todo should receive enum State (enabled/disables/unselected/non-boolean)
+    private final List<Style> styles;
 
-    public Question(Identifier identifier, QuestionType questionType, Label label, boolean isEnabled, Optional<Expression> expression) {
+    public Question(Identifier identifier, QuestionType questionType, Label label, boolean isEnabled, Optional<Expression> expression, List<Style> styles) {
         this.expression = expression;
         this.identifier = identifier;
         this.questionType = questionType;
         this.label = label;
         this.isEnabled = isEnabled;
+        this.styles = styles;
     }
 
     public boolean isEnabled() { //todo: refactor to: getState
@@ -76,11 +82,35 @@ public class Question extends Statement implements Cloneable {
     }
 
     public Question clone(boolean isEnabled) throws CloneNotSupportedException {
+        Optional<Expression> clonedExpression = cloneExpression();
+        List<Style> clonedStyles = cloneStyles();
+        return new Question(identifier.clone(), questionType, label.clone(), isEnabled, clonedExpression, clonedStyles);
+    }
+
+    public Question clone(List<Style> styles) throws CloneNotSupportedException {
+        Optional<Expression> clonedExpression = cloneExpression();
+        return new Question(identifier.clone(), questionType, label.clone(), isEnabled, clonedExpression, styles);
+    }
+
+    private List<Style> cloneStyles() {
+        return styles.stream()
+                .map(this::cloneStyle)
+                .collect(Collectors.toList());
+    }
+
+    private Style cloneStyle(Style style) {
+        try {
+            return style.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new ParseException(e);
+        }
+    }
+
+    private Optional<Expression> cloneExpression() throws CloneNotSupportedException {
         Optional<Expression> expression = Optional.empty();
         if (this.expression.isPresent()) {
             expression.of(this.expression.get().clone());
         }
-
-        return new Question(identifier.clone(), questionType, label.clone(), isEnabled, expression);
+        return expression;
     }
 }
