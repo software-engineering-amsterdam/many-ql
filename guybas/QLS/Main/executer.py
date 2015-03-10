636eacd892735d16f4e1773f1e26b5cbbf9d19e7
Guy Rombaut <admin@gsrweb.net>
2015-03-10 22:43:21 +0100

    pages gui

--- guybas/QLS/Main/executer.py
+++ guybas/QLS/Main/executer.py
@@ -3,6 +3,8 @@ import QLS.Validators.type_checker as t
 import QL.Grammar.basic_types as b
 import QL.Grammar.form as f
 import QLS.Factory.qls as ql
+import QLS.GUI.gui as g
+
 qls = ql.QLSFactory.make_sheet(q.QLS.sheet.parseFile("example.qls"))
 #print(qls.pretty_print())
 
@@ -10,5 +12,9 @@ print(qls.get_property_dict())
 formAsParseResults = f.Form.form.ignore(b.BasicTypes.comment).parseFile("example.ql")
 f = f.forms.FormFactory.make_form(formAsParseResults)
 checker = t.TypeChecker(f, qls)
+
+pages = [1,2,3]
+gui = g.GUI.draw_pages(pages)
+
 
 # TODO: all imports in init
