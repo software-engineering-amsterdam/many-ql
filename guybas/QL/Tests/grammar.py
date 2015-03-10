8ffddf40422d2938216d72b10d9f9edb3de89378
basmeesters <meesters.bas@gmail.com>
2015-03-10 16:21:19 +0100

    Update unit tests

--- guybas/QL/Tests/grammar.py
+++ guybas/QL/Tests/grammar.py
@@ -1,92 +1,98 @@
 import unittest
-from QL.Grammar.form import *
-from QL.AST.Elements import *
+import QL.Grammar.basic_types as basic_types
+import QL.Grammar.form as forms
+import QL.Grammar.expressions as expressions
+import QL.AST.Expressions.simple_expression as simple_expression
+import QL.AST.Statements.AnswerTypes.bool as b
+import QL.AST.Statements.AnswerTypes.text as t
+import QL.AST.Statements.AnswerTypes.number as n
+import QL.Factory.expressions as efactory
 
 
 class TestBasicGrammar(unittest.TestCase):
-    def test_id(self):
+    def test_grammar_id(self):
         # Test simple _id
-        result = BasicTypes.characters.parseString("this_is_an_id").asList()
+        result = basic_types.BasicTypes.characters.parseString("this_is_an_id").asList()
         self.assertEqual(result, ["this_is_an_id"])
 
         # ids cannot have spaces
-        result = BasicTypes.characters.parseString("this_is_an_id a").asList()
+        result = basic_types.BasicTypes.characters.parseString("this_is_an_id a").asList()
         self.assertIsNot(result, ["this_is_and_id a"])
 
-    def test_sentence(self):
+    def test_grammar_sentence(self):
         # Test simple sentence, btw between every word (or punctuation) comes a space
-        result = BasicTypes.sentence.parseString("this is a sentence.").asList()
+        result = basic_types.BasicTypes.sentence.parseString("this is a sentence.").asList()
         self.assertEqual(result, ["this is a sentence ."])
 
     @unittest.expectedFailure
-    def test_sentence_fail(self):
+    def test_grammar_sentence_fail(self):
         # This sentence has no end sign and therefore will fail
-        result = BasicTypes.sentence.parseString("this is a sentence").asList()
+        result = basic_types.BasicTypes.sentence.parseString("this is a sentence").asList()
         self.assertEqual(result, ["this is a sentence"])
 
-    def test_sentences(self):
+    def test_grammar_sentences(self):
         # Multiple sentences with escaped characters
-        result = BasicTypes.sentences.parseString("this is a sentence. Is this another one? Can we escape \? , \!  and \. ?").asList()
+        result = basic_types.BasicTypes.sentences.parseString("this is a sentence. Is this another one? Can we escape \? , \!  and \. ?").asList()
         self.assertEqual(result, ["this is a sentence .", "Is this another one ?", 'Can we escape ? , ! and . ?'])
 
-    def test_answer_format(self):
+    def test_grammar_answer_format(self):
         # Test the three different _type of _answer possibilities
-        result = FormFormat.answerR.parseString("bool").asList()
-        self.assertEqual(result, ["bool"])
+        result = forms.FormFormat.answerR.parseString("bool")
+        self.assertIsInstance(result[0], b.Bool)
 
-        result = FormFormat.answerR.parseString("text").asList()
-        self.assertEqual(result, ["text"])
+        result = forms.FormFormat.answerR.parseString("text")
+        self.assertIsInstance(result[0], t.Text)
 
-        result = FormFormat.answerR.parseString("number").asList()
-        self.assertEqual(result, ["number"])
+        result = forms.FormFormat.answerR.parseString("number")
+        self.assertIsInstance(result[0], n.Number)
 
     @unittest.expectedFailure
-    def test_answer_format_fail(self):
+    def test_grammar_answer_format_fail(self):
         # Not an _answer possibility
-        result = FormFormat.answerR.parseString("set").asList()
+        result = forms.FormFormat.answerR.parseString("set").asList()
         self.assertEqual(result, ["set"])
 
 
 class TestExpressionGrammar(unittest.TestCase):
 
-    def test_simple_expression(self):
-        result = Expressions.expr.parseString(" _id == True")
-        result = ExpressionFactory.make_sub_expression(result)
-        self.assertIsInstance(result, SimpleExpression)
+    def test_expression_simple(self):
+        result = expressions.Expressions.expr.parseString(" id == True")
+        result = efactory.ExpressionFactory.make_sub_expression(result)
+        self.assertIsInstance(result, simple_expression.SimpleExpression)
 
         s = result.as_list()
-        self.assertEqual(s, ["_id", "==", True])
+        self.assertEqual(s, ["id", "==", True])
 
-    def test_complex_expression(self):
-        result = Expressions.expr.parseString("4 / 2 + (3 - 1) * 4")
+    def test_expression_complex(self):
+        result = expressions.Expressions.expr.parseString("4 / 2 + (3 - 1) * 4")
         self.assertEquals(result[0].as_list(), [4, "/", 2, "+", [3, "-", 1], "*", 4])
 
     @unittest.expectedFailure
-    def test_complex_expression_fail1(self):
-        result = Expressions.expr.parseString("4 * - 1")
+    def test_expression_complex_fail1(self):
+        result = expressions.Expressions.expr.parseString("4 * - 1")
         self.assertEquals(result[0].as_list(), [4, "*", "-", 1])
 
     def test_expression_malformed(self):
         # not valid _expression, but it is valid syntax
-        result = Expressions.expr.parseString("4 + True")
+        result = expressions.Expressions.expr.parseString("4 + True")
         self.assertEquals(result[0].as_list(), [4, "+", True])
 
-    def test_print(self):
-        result = Expressions.expr.parseString("1 + 2 - (3 * 4) / 6")
+    def test_expression_print(self):
+        result = expressions.Expressions.expr.parseString("1 + 2 - (3 * 4) / 6")
         self.assertEqual(result[0].pretty_print(), "1 + 2 - (3 * 4) / 6")
 
     def test_expression_type(self):
-        result = Expressions.expr.parseString("1 + 2 - (3 * 4) / 6 == True")
+        result = expressions.Expressions.expr.parseString("1 + 2 - (3 * 4) / 6 == True")
         self.assertEqual(result[0].return_type({}),
-                         "number calc_operator number calc_operator "
-                         "( number calc_operator number ) "
-                         "calc_operator number comp_operator bool")
+                         "number + number - "
+                         "(number * number) "
+                         "/ number == bool")
 
     def test_expression_type_variable(self):
-        result = Expressions.expr.parseString("bas + 5 == True")
+        result = expressions.Expressions.expr.parseString("bas + 5 == True")
         self.assertEqual(result[0].return_type({"bas" : "number"}),
-                         "number calc_operator number comp_operator bool")
+                         "number + number == bool")
 
-    def test_dependencies(self):
-        result = Expressions.expr.parseString("cool + awesome && 1 == True")
+    def test_expression_dependencies(self):
+        result = expressions.Expressions.expr.parseString("cool + awesome && 1 == True")
         self.assertEqual(result[0].get_dependencies(), ["cool", "awesome"])
