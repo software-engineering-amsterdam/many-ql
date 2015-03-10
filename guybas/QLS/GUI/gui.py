19490977b684658a5658033036d5635fc0708705
Guy Rombaut <admin@gsrweb.net>
2015-03-10 23:08:59 +0100

    pages gui

--- guybas/QLS/GUI/gui.py
+++ guybas/QLS/GUI/gui.py
@@ -20,15 +20,17 @@ class GUI(ql_gui.GUI):
         container = tk.Frame(qGui)
         qGui.geometry('500x500')
 
+        page_i = 1
         for page in pages:
-            GUI.draw_page(container, page)
+            GUI.draw_page(container, page, page_i)
+            page_i += 1
 
         container.pack(side="top", fill="both", expand=True)
         qGui.mainloop()
 
     @staticmethod
-    def draw_page(container, page):
+    def draw_page(container, page, page_i):
         p = tk.Frame(container)
         p.place(in_=container, x=0, y=0)
         tk.Label(p, text="This is page " + str(page), font="Arial 26 bold").pack(side="left", fill="both", expand=True)
-        tk.Button(container, text="Page " + str(page), width=10, command=lambda: p.lift()).pack(side="left")
+        tk.Button(container, text="Page " + str(page_i), width=10, command=lambda: p.lift()).pack(side="left")
