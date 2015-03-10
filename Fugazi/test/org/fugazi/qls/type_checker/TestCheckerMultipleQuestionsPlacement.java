1e3ff73c12a77407f59f5952f60925e4c7e5f1b2
haren <lukasz.harezlak@gmail.com>
2015-03-11 00:45:12 +0100

    Widget type compatibility checker tests added.

--- Fugazi/test/org/fugazi/qls/type_checker/TestCheckerMultipleQuestionsPlacement.java
+++ Fugazi/test/org/fugazi/qls/type_checker/TestCheckerMultipleQuestionsPlacement.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestMultipleQuestionsPlacement extends QlsTypeCheckerBaseTest {
+public class TestCheckerMultipleQuestionsPlacement extends QlsTypeCheckerBaseTest {
 
     @Before
     public void setUp() {
---
+++ Fugazi/test/org/fugazi/qls/type_checker/TestCheckerWidgetCompatibility.java
@@ -0,0 +1,61 @@
+package org.fugazi.qls.type_checker;
+
+import org.fugazi.ql.type_checker.issue.ASTNodeIssue;
+import org.fugazi.ql.type_checker.issue.ASTNodeIssueType;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by lukaszharezlak on 11/03/15.
+ */
+public class TestCheckerWidgetCompatibility  extends QlsTypeCheckerBaseTest {
+
+    @Before
+    public void setUp() {
+        this.qlsFileName = "incompatibleWidgets.qls";
+        super.setUp();
+    }
+
+    @Test
+    public void testFormCorrect() throws Exception {
+        boolean formCorrect = qlsChecker.isFormCorrect();
+        assertFalse(formCorrect);
+    }
+
+    @Test
+    public void testErrorCount() throws Exception {
+        List<ASTNodeIssue> errors = qlsChecker.getErrors();
+
+        assertFalse(errors.isEmpty());
+        assertEquals(6, errors.size());
+    }
+
+    @Test
+    public void testErrorTypes() throws Exception {
+        List<ASTNodeIssue> errors = qlsChecker.getErrors();
+
+        ASTNodeIssueType expectedType = ASTNodeIssueType.QLS_ERROR.WRONG_WIDGET_TYPE;
+        List<ASTNodeIssueType> receivedTypes = new ArrayList<>();
+
+        for (ASTNodeIssue error: errors) {
+            receivedTypes.add(error.getErrorType());
+        }
+        // no custom arrayEquals method
+        for (ASTNodeIssueType received : receivedTypes) {
+            assertTrue(received.equals(expectedType));
+        }
+    }
+
+    @Test
+    public void testNoWarnings() throws Exception {
+        List<ASTNodeIssue> warnings = qlsChecker.getWarnings();
+        assertTrue(warnings.isEmpty());
+    }
+}
