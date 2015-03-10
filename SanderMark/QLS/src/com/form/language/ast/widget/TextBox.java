107559ff2f7be4fd0bd5140363037cb5cc856337
marksuurland <marksuurland1@gmail.com>
2015-03-10 17:18:14 +0100

    Init implemation of ast classes of qls

--- SanderMark/QLS/src/com/form/language/ast/widget/TextBox.java
+++ SanderMark/QLS/src/com/form/language/ast/widget/TextBox.java
@@ -2,8 +2,14 @@ package com.form.language.ast.widget;
 
 import javax.swing.JTextField;
 
-public class TextBox extends JTextField implements Widget {
-
+public class TextBox extends JTextField {
+	private static final long serialVersionUID = 1L;
+	
+	public TextBox()
+	{
+		
+	}
+	
 	public void getType() {
 		// TODO Auto-generated method stub
 
