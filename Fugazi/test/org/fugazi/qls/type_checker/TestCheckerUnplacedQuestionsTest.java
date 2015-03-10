953499acf157ff5b334a1c066c4cdbbca519b487
haren <lukasz.harezlak@gmail.com>
2015-03-11 00:36:27 +0100

    Undefined questions qls type checker tests added.

--- Fugazi/test/org/fugazi/qls/type_checker/TestCheckerUnplacedQuestionsTest.java
+++ Fugazi/test/org/fugazi/qls/type_checker/TestCheckerUnplacedQuestionsTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestCheckerAllQuestionsPlacedTest extends QlsTypeCheckerBaseTest {
+public class TestCheckerUnplacedQuestionsTest extends QlsTypeCheckerBaseTest {
 
     @Before
     public void setUp() {
