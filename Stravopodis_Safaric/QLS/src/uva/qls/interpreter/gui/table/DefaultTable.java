60eef11facd7f287006cc605a4082d933d71c155
pstrav <pstrav@hotmail.com>
2015-03-11 09:47:10 +0100

    QLS GUI Init

---
+++ Stravopodis_Safaric/QLS/src/uva/qls/interpreter/gui/table/DefaultTable.java
@@ -0,0 +1,64 @@
+package uva.qls.interpreter.gui.table;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+import uva.qls.ast.primitive.PrimitiveType;
+import uva.qls.interpreter.typecheck.table.Table;
+
+public class DefaultTable extends Table<String, Deque<DefaultTableValue>>{
+
+	private Map<String, Deque<DefaultTableValue>> stacks;
+	
+	public DefaultTable(){
+		this.initializeStacks();
+	}
+	
+	public Map<String, Deque<DefaultTableValue>> getStacks(){
+		return this.stacks;
+	}
+	
+	public void clearStacks(){
+		this.initializeStacks();
+	}
+	
+	private void initializeStacks(){
+		this.stacks = new HashMap<String, Deque<DefaultTableValue>>();
+		
+		for (String s : PrimitiveType.getAllTypes()){
+			this.stacks.put(s, new ArrayDeque<DefaultTableValue>());
+		}
+	}
+	
+	@Override
+	public void putValue(String identifier, Deque<DefaultTableValue> value) {
+		this.stacks.put(identifier, value);
+	}
+	
+	public void push(String identifier, DefaultTableValue value){
+		Deque<DefaultTableValue> stack = this.retrieveValue(identifier);
+		stack.push(value);
+		this.putValue(identifier, stack);
+	}
+	
+	public void pop(String identifier){
+		Deque<DefaultTableValue> stack = this.retrieveValue(identifier);
+		stack.pop();
+		this.putValue(identifier, stack);
+	}
+
+	@Override
+	public Deque<DefaultTableValue> retrieveValue(String identifier) {
+		return this.stacks.get(identifier);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ Stravopodis_Safaric/QLS/src/uva/qls/interpreter/gui/visitor/Renderer.java
@@ -0,0 +1,215 @@
+package uva.qls.interpreter.gui.visitor;
+
+import uva.qls.ast.ASTNode;
+import uva.qls.ast.Page;
+import uva.qls.ast.Prog;
+import uva.qls.ast.StyleSheet;
+import uva.qls.ast.component.Checkbox;
+import uva.qls.ast.component.Dropdown;
+import uva.qls.ast.component.Radio;
+import uva.qls.ast.component.Slider;
+import uva.qls.ast.component.Spinbox;
+import uva.qls.ast.component.Textbox;
+import uva.qls.ast.literal.BooleanLiteral;
+import uva.qls.ast.literal.Identifier;
+import uva.qls.ast.literal.IntLiteral;
+import uva.qls.ast.literal.Literal;
+import uva.qls.ast.literal.MoneyLiteral;
+import uva.qls.ast.literal.StringLiteral;
+import uva.qls.ast.primitive.Type;
+import uva.qls.ast.statements.DefaultValue;
+import uva.qls.ast.statements.Question;
+import uva.qls.ast.statements.Section;
+import uva.qls.ast.statements.Statement;
+import uva.qls.ast.statements.Subsection;
+import uva.qls.ast.statements.visitor.*;
+import uva.qls.ast.style.Color;
+import uva.qls.ast.style.Font;
+import uva.qls.ast.style.FontName;
+import uva.qls.ast.style.FontSize;
+import uva.qls.ast.style.Height;
+import uva.qls.ast.style.Style;
+import uva.qls.ast.style.Width;
+
+public class Renderer implements StatementVisitor<Object>{
+
+	@Override
+	public Object visitStyle(Style style) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitColor(Color color) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitFont(Font font) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitFontsize(FontSize fontSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitFontName(FontName fontName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitHeight(Height height) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitWidth(Width width) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitCheckBox(Checkbox checkBox) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitDropDown(Dropdown dropDown) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitRadio(Radio radio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitSlider(Slider slider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitSpinbox(Spinbox spinbox) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitTextbox(Textbox textbox) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitBooleanLiteral(BooleanLiteral literal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIntLiteral(IntLiteral literal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIdentifier(Identifier identifier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitLiteral(Literal literal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitMoneyLiteral(MoneyLiteral literal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitStringLiteral(StringLiteral literal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitType(Type type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitASTNode(ASTNode node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitProg(Prog prog) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitPage(Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitStyleSheet(StyleSheet styleSheet) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitStatement(Statement statement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitDefaultValueComponent(DefaultValue defaultValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitDefaultValueStatements(DefaultValue defaultValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitQuestion(Question question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitSection(Section section) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitSubsection(Subsection subsection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
