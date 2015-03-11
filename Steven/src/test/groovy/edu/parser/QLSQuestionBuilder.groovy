e26c060fa5742aeb4a775fafc1921f509a78ccd4
Steven Kok <stevenen@live.nl>
2015-03-11 13:27:40 +0100

    added QLSQuestionBuilder

---
+++ Steven/src/test/groovy/edu/parser/QLSQuestionBuilder.groovy
@@ -0,0 +1,31 @@
+package edu.parser
+
+import edu.nodes.styles.Style
+import edu.parser.QLS.nodes.Identifier
+import edu.parser.QLS.nodes.statement.Page
+import edu.parser.QLS.nodes.statement.QLSQuestion
+
+/**
+ * Created by Steven Kok on 11/03/2015.
+ */
+public class QLSQuestionBuilder {
+    private Identifier identifier
+    private List<Style> styles
+
+    QLSQuestionBuilder() {
+        this.styles = new ArrayList<>()
+        identifier = new Identifier("identifier")
+    }
+
+    public Page build() {
+        return new QLSQuestion(identifier, styles)
+    }
+
+    public QLSQuestionBuilder addStyle(Style style) {
+        styles.add(style)
+    }
+
+    public QLSQuestionBuilder identifier(String identifier) {
+        this.identifier = new Identifier(identifier);
+    }
+}
