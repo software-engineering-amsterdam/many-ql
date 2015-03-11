e6243574707438b5db2cee115fc9c9b0c18b49cd
Steven Kok <stevenen@live.nl>
2015-03-11 13:12:30 +0100

    Stylesheet has list of pages and default statements

--- Steven/src/main/java/edu/parser/QLS/nodes/Stylesheet.java
+++ Steven/src/main/java/edu/parser/QLS/nodes/Stylesheet.java
@@ -2,7 +2,7 @@ package edu.parser.QLS.nodes;
 
 import edu.parser.QLS.QLSVisitor;
 import edu.parser.QLS.nodes.statement.Default;
-import edu.parser.QLS.nodes.statement.QLSQuestion;
+import edu.parser.QLS.nodes.statement.Page;
 
 import java.util.List;
 
@@ -10,18 +10,18 @@ import java.util.List;
  * Created by Steven Kok on 28/02/2015.
  */
 public class Stylesheet implements AbstractNode {
-    private final List<QLSQuestion> questions;
+    private final List<Page> pages;
     private final Identifier title;
     private final List<Default> globalDefaultStatements;
 
-    public Stylesheet(Identifier title, List<QLSQuestion> statements, List<Default> globalDefaultStatements) {
+    public Stylesheet(Identifier title, List<Page> pages, List<Default> globalDefaultStatements) {
         this.title = title;
-        this.questions = statements;
+        this.pages = pages;
         this.globalDefaultStatements = globalDefaultStatements;
     }
 
-    public List<QLSQuestion> getQuestions() {
-        return questions;
+    public List<Page> getPages() {
+        return pages;
     }
 
     public List<Default> getGlobalDefaultStatements() {
