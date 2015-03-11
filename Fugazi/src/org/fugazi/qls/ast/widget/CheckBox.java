bf317746d0a8fe7aa8a05a1a9cd84ccbb6008e26
Theologos_Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-03-11 21:46:53 +0100

    Starting with the widgets, checkbox.

--- Fugazi/src/org/fugazi/qls/ast/widget/CheckBox.java
+++ Fugazi/src/org/fugazi/qls/ast/widget/CheckBox.java
@@ -6,30 +6,47 @@ import org.fugazi.ql.ast.type.Type;
 import org.fugazi.qls.ast.IQLSASTVisitor;
 import org.fugazi.qls.ast.style.Style;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CheckBox extends Widget {
 
+    private JCheckBox component;
+
     public CheckBox(int _lineNum) {
         super(_lineNum);
+        this.component = new JCheckBox();
     }
 
     public CheckBox() {
+        this.component = new JCheckBox();
     }
 
     public CheckBox(int _lineNum, String _label) {
         super(_lineNum);
         this.label = _label;
+        this.component = new JCheckBox(label);
     }
 
     public CheckBox(String _label) {
         this.label = _label;
+        this.component = new JCheckBox(label);
+    }
+
+    @Override
+    public void setLabel(String _label) {
+        this.label = _label;
+        this.component.setText(label);
     }
 
     @Override
     public void applyStyle(Style _style) {
         this.style = _style;
+
+        // inherit properties that are not set in the given style from default.
+        this.style.inheriteFromStyle(this.getDefaultStyle());
+
         // todo
     }
 
