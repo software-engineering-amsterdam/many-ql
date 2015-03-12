d49d03dadb27bf1ab63bc56fe35368f957b4d344
Milan Milanov <whizzer.me@gmail.com>
2015-03-12 21:42:04 +0100

    Implement initial version of gui <-> evaluator bindings

--- KrasiMilan/QL/src/main/java/nl/uva/softwcons/ql/ui/UiBuilder.java
+++ KrasiMilan/QL/src/main/java/nl/uva/softwcons/ql/ui/UiBuilder.java
@@ -1,5 +1,9 @@
 package nl.uva.softwcons.ql.ui;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -11,45 +15,77 @@ import nl.uva.softwcons.ql.ast.form.FormVisitor;
 import nl.uva.softwcons.ql.ast.statement.ComputedQuestion;
 import nl.uva.softwcons.ql.ast.statement.Conditional;
 import nl.uva.softwcons.ql.ast.statement.Question;
-import nl.uva.softwcons.ql.ast.statement.Statement;
 import nl.uva.softwcons.ql.ast.statement.StatementVisitor;
-import nl.uva.softwcons.ql.ui.layout.ComputedQuestionLayout;
+import nl.uva.softwcons.ql.eval.Evaluator;
 import nl.uva.softwcons.ql.ui.layout.FormLayout;
 import nl.uva.softwcons.ql.ui.layout.QuestionLayout;
 import nl.uva.softwcons.ql.ui.widget.Widget;
 
-public class UiBuilder extends Application implements StatementVisitor<Node>, FormVisitor<Node> {
+public class UiBuilder extends Application implements StatementVisitor<List<QuestionLayout>>, FormVisitor<FormLayout> {
+    private TypeRenderer renderer;
+    private Evaluator evaluator;
 
-    private TypeRenderer renderer = new TypeRenderer();
+    public UiBuilder() {
+    }
 
-    @Override
-    public Node visit(Form form) {
-        FormLayout layout = new FormLayout(form);
-        for (Statement statement : form.getStatements()) {
-            layout.add(statement.accept(this));
-        }
-        return layout.getNode();
+    public UiBuilder(final Form form) {
+        this.renderer = new TypeRenderer();
+        this.evaluator = new Evaluator(form);
+    }
+
+    public static FormLayout buildFrom(final Form form) {
+        return form.accept(new UiBuilder(form));
     }
 
     @Override
-    public Node visit(final ComputedQuestion question) {
-        ComputedQuestionLayout layout = new ComputedQuestionLayout(question);
-        Widget questionWidget = question.getType().accept(renderer);
+    public FormLayout visit(final Form form) {
+        final FormLayout formLayout = new FormLayout(form);
+
+        form.getStatements().forEach(statement -> {
+            statement.accept(this).forEach(layout -> formLayout.add(layout));
+        });
+
+        return formLayout;
+    }
+
+    @Override
+    public List<QuestionLayout> visit(final ComputedQuestion question) {
+        final QuestionLayout layout = new QuestionLayout(question);
+        final Widget questionWidget = question.getType().accept(renderer);
+        questionWidget.setEditable(false); // TODO this should be in the
+                                           // renderer
         layout.add(questionWidget.getWidget());
-        return layout.getNode();
+
+        evaluator.addListener(question, (newValue) -> questionWidget.setValue(newValue));
+
+        return Arrays.asList(layout);
     }
 
     @Override
-    public Node visit(final Question question) {
-        QuestionLayout layout = new QuestionLayout(question);
-        Widget questionWidget = question.getType().accept(renderer);
+    public List<QuestionLayout> visit(final Question question) {
+        final QuestionLayout layout = new QuestionLayout(question);
+        final Widget questionWidget = question.getType().accept(renderer);
         layout.add(questionWidget.getWidget());
-        return layout.getNode();
+
+        questionWidget.addListener((newValue) -> evaluator.updateValue(question.getId(), newValue));
+
+        return Arrays.asList(layout);
     }
 
     @Override
-    public Node visit(final Conditional conditional) {
-        return null;
+    public List<QuestionLayout> visit(final Conditional conditional) {
+        final List<QuestionLayout> layouts = conditional.getQuestions().stream().flatMap(q -> q.accept(this).stream())
+                .collect(Collectors.toList());
+
+        evaluator.addListener(conditional, (value) -> {
+            layouts.forEach(l -> l.setVisible(value.inConditionalContext()));
+        });
+
+        layouts.forEach(layout -> {
+            layout.setVisible(evaluator.getValue(conditional).inConditionalContext());
+        });
+
+        return layouts;
     }
 
     public static void main(String[] args) {
@@ -59,11 +95,11 @@ public class UiBuilder extends Application implements StatementVisitor<Node>, Fo
     @Override
     public void start(Stage primaryStage) throws Exception {
         Form f = Questionnaire.build(UiBuilder.class.getResourceAsStream("/form.ql"));
-        Node n = f.accept(new UiBuilder());
+        Node n = UiBuilder.buildFrom(f).getNode();
 
         StackPane root = new StackPane();
         root.getChildren().add(n);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
