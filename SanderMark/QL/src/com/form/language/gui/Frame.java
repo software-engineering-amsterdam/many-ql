7a2de6caf77bbe6845989374486f0955a24696d1
sanderbos89 <sanderbos89@gmail.com>
2015-03-12 11:09:52 +0100

    printStacktrace should never be called

--- SanderMark/QL/src/com/form/language/gui/Frame.java
+++ SanderMark/QL/src/com/form/language/gui/Frame.java
@@ -34,12 +34,8 @@ public class Frame {
     public static void main(String[] args) {
 	EventQueue.invokeLater(new Runnable() {
 	    public void run() {
-		try {
-		    Frame window = new Frame();
-		    window.frame.setVisible(true);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
+		Frame window = new Frame();
+		window.frame.setVisible(true);
 	    }
 	});
     }
