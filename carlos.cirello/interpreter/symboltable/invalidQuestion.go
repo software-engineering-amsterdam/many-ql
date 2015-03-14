559a2a62b7add83bbd3cc0e5dfdd7df3251ddc16
Carlos C <uldericofilho@gmail.com>
2015-03-14 17:43:40 +0100

    All illegal question types are shown during typecheck step

---
+++ carlos.cirello/interpreter/symboltable/invalidQuestion.go
@@ -0,0 +1,30 @@
+package symboltable
+
+// InvalidQuestion stores the answer of question which type is invalid or
+// unknown
+type InvalidQuestion struct {
+	primitive string
+}
+
+// InvalidQuestionType constant used for type comparison internally in interpreter
+// and frontend
+const InvalidQuestionType = "invalid"
+
+func NewInvalidQuestion(primitive string) *InvalidQuestion {
+	return &InvalidQuestion{primitive}
+}
+
+// From takes the input from Frontend and stores locally - Numeric
+func (s *InvalidQuestion) From(str string) error {
+	return nil
+}
+
+// String prints in human form the content of the question - Numeric
+func (s InvalidQuestion) String() string {
+	return ""
+}
+
+// Value returns the invalid primitive type
+func (s InvalidQuestion) Value() interface{} {
+	return s.primitive
+}
