28f76fc7a7b37f19de8eb06b147a051a31495c89
verhoofstad <verhoofstad@hotmail.com>
2015-03-14 16:13:46 +0100

    Added QLS checkers.

---
+++ CarlaErik/UvA.SoftCon.Questionnaire/Runtime/Validation/QLS/TypeCheckingVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UvA.SoftCon.Questionnaire.QLS;
+using UvA.SoftCon.Questionnaire.QLS.AST.Model;
+
+namespace UvA.SoftCon.Questionnaire.Runtime.Validation.QLS
+{
+    /// <summary>
+    /// Checks whether widget assignments are compatible with question types.
+    /// </summary>
+    internal class TypeCheckingVisitor : QLSVisitor
+    {
+
+
+        public override void VisitQuestionReference(QuestionReference questionRef)
+        {
+        }
+
+        public override void VisitDefaultStyle(DefaultStyle defaultStyle)
+        {
+            //defaultStyle.StyleAttributes.Where(attr => attr )
+        }
+    }
+}
