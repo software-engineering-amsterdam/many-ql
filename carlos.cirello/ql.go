e409982eee3f9bb2662bd5544b9f6728be3d8919
Carlos C <uldericofilho@gmail.com>
2015-03-14 18:15:06 +0100

    Add initial documentation for main package

--- carlos.cirello/ql.go
+++ carlos.cirello/ql.go
@@ -1,3 +1,10 @@
+// QL.
+//
+// Carlos Cirello - MSc. Software Engineering - Batch 2015/2016.
+// Questionaire Language is a toy project for UvA Software Construction
+// Course 2014/2015 Batch.
+//
+// It is a DSL meant to describe forms (QL).
 package main
 
 import (
