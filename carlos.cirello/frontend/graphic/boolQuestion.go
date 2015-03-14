d081b187c6cc5f26802b8018df5bb233213f3c12
Carlos C <uldericofilho@gmail.com>
2015-03-14 17:47:07 +0100

    Remove unnecessary style code from QML checkbox template

--- carlos.cirello/frontend/graphic/boolQuestion.go
+++ carlos.cirello/frontend/graphic/boolQuestion.go
@@ -9,7 +9,6 @@ const checkboxQML = `
 import QtQuick 2.2
 import QtQuick.Controls 1.1
 import QtQuick.Layouts 1.0
-import QtQuick.Controls.Styles 1.3
 
 GroupBox {
 	Layout.fillWidth: true
@@ -20,23 +19,6 @@ GroupBox {
 		CheckBox {
 			objectName: "{{ .ObjectName }}"
 			text: "{{ .QuestionName }}"
-			style: CheckBoxStyle {
-				indicator: Rectangle {
-					implicitWidth: 16
-					implicitHeight: 16
-					radius: 3
-					border.color: control.activeFocus ? "darkblue" : "gray"
-					border.width: 1
-					Rectangle {
-						visible: control.checked
-						color: "#555"
-						border.color: "#333"
-						radius: 1
-						anchors.margins: 4
-						anchors.fill: parent
-					}
-				}
-			}
 		}
 	}
 }
