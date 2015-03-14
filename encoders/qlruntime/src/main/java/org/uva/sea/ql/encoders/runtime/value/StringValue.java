32d62c914aced4c0df7ceb7e3ed1fd5eb7e5951d
softwaregast <weisflog@hotmail.com>
2015-03-14 20:52:39 +0100

    Support string concatenation QL

--- encoders/qlruntime/src/main/java/org/uva/sea/ql/encoders/runtime/value/StringValue.java
+++ encoders/qlruntime/src/main/java/org/uva/sea/ql/encoders/runtime/value/StringValue.java
@@ -16,4 +16,10 @@ public class StringValue extends Value {
 	public String getValue() {
 		return value;
 	}
+
+	@Override
+	public StringValue add(Value otherValue) {
+		String result = value + ((StringValue) otherValue).getValue();
+		return new StringValue(result);
+	}
 }
