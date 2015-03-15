67bc85eb32eee4c6a95e873ede4b98b1fe9436a8
pimtegelaar <pimtegelaar@gmail.com>
2015-03-15 12:23:08 +0100

    make utility class Messages final

--- encoders/ql/src/main/java/org/uva/sea/ql/encoders/message/Messages.java
+++ encoders/ql/src/main/java/org/uva/sea/ql/encoders/message/Messages.java
@@ -4,12 +4,10 @@ import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class Messages {
-	private static final String BUNDLE_NAME = Messages.class.getPackage()
-			.getName() + ".messages";
+public final class Messages {
+	private static final String BUNDLE_NAME = Messages.class.getPackage().getName() + ".messages";
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
 	private Messages() {
 	}
