d20bf0beaa855a517b5d83f705754f3cfd5f78f7
pimtegelaar <pimtegelaar@gmail.com>
2015-03-15 12:46:53 +0100

    Added test cases for TypeCheckerVisitor condition must be boolean

--- encoders/ql/src/test/java/org/uva/sea/ql/encoders/visitor/TypeCheckerVisitorTest.java
+++ encoders/ql/src/test/java/org/uva/sea/ql/encoders/visitor/TypeCheckerVisitorTest.java
@@ -10,16 +10,51 @@ import java.util.List;
 import org.junit.Test;
 import org.uva.sea.ql.encoders.ast.Question;
 import org.uva.sea.ql.encoders.ast.TextLocation;
+import org.uva.sea.ql.encoders.ast.expression.BinaryExpression;
+import org.uva.sea.ql.encoders.ast.expression.BooleanExpression;
+import org.uva.sea.ql.encoders.ast.expression.Expression;
+import org.uva.sea.ql.encoders.ast.expression.IntegerExpression;
 import org.uva.sea.ql.encoders.ast.type.StringType;
 import org.uva.sea.ql.encoders.validation.Validation;
-import org.uva.sea.ql.encoders.visitor.TypeCheckerVisitor;
 
 public class TypeCheckerVisitorTest {
 
 	private TypeCheckerVisitor visitor;
 
 	@Test
-	public void testCheckTypes_findsDuplicateLabels() {
+	public void testCheckTypes_conditionsWithBooleansAreAllowed() {
+		String questionLabel = "Depends on this and that";
+		Question questionA = new Question(new TextLocation(10, 10), "questionA", new StringType(), questionLabel);
+		Expression leftHand = new BooleanExpression(new TextLocation(10, 10), true);
+		Expression rightHand = new BooleanExpression(new TextLocation(10, 10), true);
+		Expression condition = new BinaryExpression(new TextLocation(8, 0), leftHand, rightHand, "&&");
+		questionA.setCondition(condition);
+		List<Question> questions = Arrays.asList(questionA);
+		visitor = new TypeCheckerVisitor(questions);
+
+		List<Validation> validations = visitor.checkTypes();
+		assertThat(validations.size(), is(0));
+	}
+
+	@Test
+	public void testCheckTypes_conditionsWithIntegersAreNotAllowed() {
+		String questionLabel = "Depends on this and that";
+		Question questionA = new Question(new TextLocation(10, 10), "questionA", new StringType(), questionLabel);
+		Expression leftHand = new IntegerExpression(new TextLocation(10, 10), 0);
+		Expression rightHand = new IntegerExpression(new TextLocation(10, 10), 1);
+		Expression condition = new BinaryExpression(new TextLocation(8, 0), leftHand, rightHand, "&&");
+		questionA.setCondition(condition);
+		List<Question> questions = Arrays.asList(questionA);
+		visitor = new TypeCheckerVisitor(questions);
+
+		List<Validation> validations = visitor.checkTypes();
+		Validation validation = validations.get(0);
+		assertThat(validation, is(notNullValue()));
+		assertThat(validation.getValidationMessage(), is("Condition has to be of type boolean. Type encountered is 'integer'"));
+	}
+
+	@Test
+	public void testCheckTypes_duplicateLabelsAreNotAllowed() {
 		String questionLabel = "What is the meaning of life?";
 		Question questionA = new Question(new TextLocation(10, 10), "questionA", new StringType(), questionLabel);
 		Question questionB = new Question(new TextLocation(12, 10), "questionB", new StringType(), questionLabel);
@@ -29,6 +64,19 @@ public class TypeCheckerVisitorTest {
 		List<Validation> validations = visitor.checkTypes();
 		Validation validation = validations.get(0);
 		assertThat(validation, is(notNullValue()));
-		assertThat(validation.getValidationMessage(), is("Duplicate label: " + questionLabel));
+		assertThat(validation.getValidationMessage(), is("Duplicate label 'What is the meaning of life?'"));
 	}
+
+	@Test
+	public void testCheckTypes_differentLabelsAreAllowed() {
+		String questionLabel = "What is the meaning of life?";
+		Question questionA = new Question(new TextLocation(10, 10), "questionA", new StringType(), questionLabel);
+		Question questionB = new Question(new TextLocation(12, 10), "questionB", new StringType(), questionLabel + "2");
+		List<Question> questions = Arrays.asList(questionA, questionB);
+		visitor = new TypeCheckerVisitor(questions);
+
+		List<Validation> validations = visitor.checkTypes();
+		assertThat(validations.size(), is(0));
+	}
+
 }
