4b005361090c15ba7ceca1c43486a8e10ea54601
Gurrt <gerritkrijnen@hotmail.com>
2015-03-15 13:58:07 +0100

    Removed obsolete Parent checking

--- RobertGer/src/main/java/nl/uva/bromance/ast/conditionals/ElseIfStatement.java
+++ RobertGer/src/main/java/nl/uva/bromance/ast/conditionals/ElseIfStatement.java
@@ -1,24 +1,15 @@
 package nl.uva.bromance.ast.conditionals;
 
-import nl.uva.bromance.ast.Calculation;
-import nl.uva.bromance.ast.Form;
-import nl.uva.bromance.ast.Label;
 import nl.uva.bromance.ast.Node;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by Gerrit Krijnen on 2/16/2015.
  */
 public class ElseIfStatement extends Node implements ContainsExpression {
-    private static final List<Class<? extends Node>> parentsAllowed = new ArrayList<Class<? extends Node>>(Arrays.asList(Form.class, Label.class, Calculation.class));
     private Expression expression;
 
     public ElseIfStatement(int lineNumber) {
         super(lineNumber, ElseIfStatement.class);
-        super.setAcceptedParents(parentsAllowed);
     }
 
     @Override
