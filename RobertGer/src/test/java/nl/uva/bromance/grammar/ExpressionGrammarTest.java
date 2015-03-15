7f01d9e0e5a1f1b785522c1b63746aa12f4b3de7
Robert <robertdiebels@gmail.com>
2015-03-15 16:06:37 +0100

    Merge branch 'master' of D:\Dev\Workspaces\intellij\UvA\many-ql with conflicts.

--- RobertGer/src/test/java/nl/uva/bromance/grammar/ExpressionGrammarTest.java
+++ RobertGer/src/test/java/nl/uva/bromance/grammar/ExpressionGrammarTest.java
@@ -1,5 +1,7 @@
 package nl.uva.bromance.grammar;
 
+import nl.uva.bromance.listeners.GrammarErrorListener;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,6 +11,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ExpressionGrammarTest extends GrammarTest {
 
     private String parExpression = "(id)";
+
+    @Before
+    public void setupStartEnd() {
+        START = "Name: \"Tax\" {\n" +
+                "    Form: \"default\" {\n" +
+                "         If: expression *";
+        END = "{Text: \"text\" }}}";
+    }
 
     @Test
     public void correctIdExpression() throws IOException {
@@ -120,4 +130,64 @@ public class ExpressionGrammarTest extends GrammarTest {
     }
 
 
+    //Test all grammar types as children (by testing all possible types for all possible children we are by extension testing all possible types for all possible parents)
+    @Test
+    public void CalculationAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        CalculationAsChildSetup();
+    }
+
+    @Test
+    public void ExpressionAsChild() throws IOException {
+        ExpressionAsChildSetup();
+        assertThat(listener.expressionCount).isEqualTo(3);
+    }
+
+    @Test
+    public void FormAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        FormAsChildSetup();
+    }
+
+    @Test
+    public void IfStatementAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        IfStatementAsChildSetup();
+    }
+
+    @Test
+    public void ElseIfStatementAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        ElseIfStatementAsChildSetup();
+    }
+
+    @Test
+    public void ElseStatementAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        ElseStatementAsChildSetup();
+    }
+
+    @Test
+    public void InputAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        InputAsChildSetup();
+    }
+
+    @Test
+    public void LabelAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        LabelAsChildSetup();
+    }
+
+    @Test
+    public void QuestionAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        QuestionAsChildSetup();
+    }
+
+    @Test
+    public void QuestionnaireAsChild() throws IOException {
+        expectedException.expect(GrammarErrorListener.SyntaxError.class);
+        QuestionnaireAsChildSetup();
+    }
 }
