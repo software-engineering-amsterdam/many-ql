e99922e47a134fdc7282636c8082a4861421cd41
softwaregast <weisflog@hotmail.com>
2015-03-16 15:59:52 +0100

    Added Questions/Subsections to Sections

--- encoders/qls/src/main/java/org/uva/sea/qls/encoders/ast/Section.java
+++ encoders/qls/src/main/java/org/uva/sea/qls/encoders/ast/Section.java
@@ -1,10 +1,15 @@
 package org.uva.sea.qls.encoders.ast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Section {
 
 	private final String name;
 
-	// private List<Question> questions = new ArrayList<Section>();
+	private List<String> questionNames = new ArrayList<>();
+
+	private List<Section> subSections = new ArrayList<>();
 
 	public Section(String name) {
 		this.name = name;
@@ -14,12 +19,20 @@ public class Section {
 		return name;
 	}
 
-	// public List<Question> getQuestions() {
-	// return questions;
-	// }
+	public List<String> getQuestionNames() {
+		return questionNames;
+	}
 
-	// public void addQuestion(Question question) {
-	// questions.add(question);
-	// }
+	public void addQuestion(String questionName) {
+		questionNames.add(questionName);
+	}
+
+	public List<Section> getSectionNames() {
+		return subSections;
+	}
+
+	public void addSubSection(Section subSection) {
+		subSections.add(subSection);
+	}
 
 }
