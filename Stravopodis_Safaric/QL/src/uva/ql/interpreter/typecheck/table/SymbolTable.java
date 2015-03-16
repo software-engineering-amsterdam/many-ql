1399d59689d6fde0f823a74e33e504b9434383e8
pstrav <pstrav@hotmail.com>
2015-03-16 12:49:13 +0100

    QL refactored

---
+++ Stravopodis_Safaric/QL/src/uva/ql/interpreter/typecheck/table/SymbolTable.java
@@ -0,0 +1,43 @@
+package uva.ql.interpreter.typecheck.table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import uva.ql.ast.type.*;
+import uva.ql.supporting.table.Table;
+
+public class SymbolTable extends Table <String, Type> {
+
+	private final Map<String, Type> symbolTable = new HashMap<String, Type>();
+	
+	@Override
+	public void putValue(String key, Type value) {
+		this.symbolTable.put(key, value);
+	}
+
+	@Override
+	public boolean keyExists(String key) {
+		return this.symbolTable.containsKey(key);
+	}
+
+	@Override
+	public boolean valueExists(String key, Type value) {
+		
+		if (this.keyExists(key)){
+			return this.symbolTable.get(key).equals(value);
+		}
+		
+		return false;
+	}
+
+	@Override
+	public Type retrieveValue(String key) {
+		return this.symbolTable.get(key);
+	}
+
+	@Override
+	public String toString() {
+		return this.symbolTable.toString();
+	}
+
+}
