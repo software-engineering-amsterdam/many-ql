c19b1377deddd50a129a7b401d745b48a1dfbbf4
Gurrt <gerritkrijnen@hotmail.com>
2015-03-16 11:04:02 +0100

    Fixed dynamic dispatch for Questions

--- RobertGer/src/main/java/nl/uva/bromance/ast/questiontypes/StringType.java
+++ RobertGer/src/main/java/nl/uva/bromance/ast/questiontypes/StringType.java
@@ -1,13 +1,18 @@
 package nl.uva.bromance.ast.questiontypes;
 
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import nl.uva.bromance.ast.conditionals.Result;
 import nl.uva.bromance.ast.conditionals.StringResult;
+
+import java.util.List;
 
 /**
  * Created by Robert on 9-3-2015.
  */
-public class StringType extends QuestionType {
+public class StringType implements QuestionType {
 
+    @Override
     public String getTypeString() {
         return "string";
     }
@@ -15,5 +20,10 @@ public class StringType extends QuestionType {
     @Override
     public Result getCorrespondingResultType() {
         return new StringResult("");
+    }
+
+    @Override
+    public void addQuestionToPane(Pane parent, List<StringResult> multipleChoice) {
+        parent.getChildren().add(new TextField());
     }
 }
