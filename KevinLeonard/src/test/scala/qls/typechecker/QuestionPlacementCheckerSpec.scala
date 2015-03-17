153f73991db1a25424d47008f9dccc8efe98e936
leonardpunt <leonard@q42.nl>
2015-03-17 16:51:15 +0100

    Add question placement checker.

--- KevinLeonard/src/test/scala/qls/typechecker/QuestionPlacementCheckerSpec.scala
+++ KevinLeonard/src/test/scala/qls/typechecker/QuestionPlacementCheckerSpec.scala
@@ -3,18 +3,23 @@ package qls.typechecker
 import org.specs2.mutable.Specification
 import ql.ast.{BooleanType, Variable}
 import ql.typechecker.Error
-import qls.ast.{Question, Section, Text}
+import qls.ast.{Page, Question, Section, StyleSheet, Text}
 
 class QuestionPlacementCheckerSpec extends Specification {
   val checker = new QuestionPlacementChecker()
   import checker._
 
-  val EmptySection = Section("section", List())
+  val AnyLabel = "label"
+  val AnyVariable = Variable("x")
+  val EmptyStyleSheet = StyleSheet(AnyLabel, List(Page(AnyLabel, List(Section(AnyLabel, List())))))
 
   "all question placed checker" should {
     "return None if all questions are placed" in {
-      val sectionWithQuestion = Section("section", List(Question(Variable("x"), Text(List()))))
-      val environmentWithQuestion = Map("x" -> BooleanType())
+      val sectionWithQuestion = StyleSheet(AnyLabel, List(Page(AnyLabel, List(
+        Section(AnyLabel, List(Question(Variable("x"), Text(List())))),
+        Section(AnyLabel, List(Section(AnyLabel, List(Question(Variable("y"), Text(List()))))))
+      ))))
+      val environmentWithQuestion = Map("x" -> BooleanType(), "y" -> BooleanType())
 
       check(sectionWithQuestion, environmentWithQuestion) should beNone
     }
@@ -23,14 +28,14 @@ class QuestionPlacementCheckerSpec extends Specification {
       val environmentWithQuestion = Map("x" -> BooleanType())
       val error = Error(s"The question(s) x are not placed")
 
-      check(EmptySection, environmentWithQuestion) should beSome(error)
+      check(EmptyStyleSheet, environmentWithQuestion) should beSome(error)
     }
 
     "return Some with one error, if multiple questions aren't placed" in {
       val environmentWithMultipleQuestions = Map("x" -> BooleanType(), "y" -> BooleanType())
       val error = Error(s"The question(s) x, y are not placed")
 
-      check(EmptySection, environmentWithMultipleQuestions) should beSome(error)
+      check(EmptyStyleSheet, environmentWithMultipleQuestions) should beSome(error)
     }
   }
 }
