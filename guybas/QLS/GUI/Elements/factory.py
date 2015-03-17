39e663498a85e17e07233543e2310a3254fcdbe7
Guy Rombaut <admin@gsrweb.net>
2015-03-17 15:55:14 +0100

    QLS

---
+++ guybas/QLS/GUI/Elements/factory.py
@@ -0,0 +1,18 @@
+import QL.Grammar.constants as c
+import QLS.GUI.Elements.radio_button as r
+import QLS.GUI.Elements.spin_box as s
+import QLS.GUI.Elements.text_entry as t
+
+
+class Factory:
+    def __init__(self, statement, gui):
+        q_type = statement.ast.get_type()
+        if q_type == c.BOOL:
+            self.gui_element = r.RadioButton(statement, gui)
+        elif q_type == c.NUMBER:
+            self.gui_element = s.SpinBox(statement, gui)
+        elif q_type == c.TEXT:
+            self.gui_element = t.TextEntry(statement, gui)
+
+    def get_gui_element(self):
+        return self.gui_element
