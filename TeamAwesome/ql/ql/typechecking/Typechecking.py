c86b5d7e6365cef4d7f7b11ac51ee0dff76225c2
guidouva <guidouva@outlook.com>
2015-03-18 00:32:16 +0100

    clarify why the type checker depends on the parser

--- TeamAwesome/ql/ql/typechecking/Typechecking.py
+++ TeamAwesome/ql/ql/typechecking/Typechecking.py
@@ -8,6 +8,10 @@ from typechecking import Result
 
 
 
+# The type checker depends on the parser because it uses the parser
+# to generate error messages using tokens from the QL language for
+# example for operators. So instead of spitting out a message with
+# 'QLAddition' the type checker can say '+' (in case of the ANTLR parser)
 def check(parser):
     modules = (
         CyclicQuestionDependencies,
