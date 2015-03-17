628cf2d983f622b8ff96769675c9db18b01b6041
basmeesters <meesters.bas@gmail.com>
2015-03-17 09:50:00 +0100

    fix bug type checker

--- guybas/QL/AST/Expressions/Operations/less.py
+++ guybas/QL/AST/Expressions/Operations/less.py
@@ -1,10 +1,10 @@
 import QL.AST.Expressions.Primitives.primitive as e
 import QL.Grammar.constants as constants
-import QL.AST.Expressions.Operations.binary_expression as b
+import QL.AST.Expressions.Operations.equal as e
 
 
-class Less(b.BinaryExpression):
+class Less(e.Equal):
 
     # get the return _type of the _expression
     def return_type_string(self, type_dict):
-        return constants.NUMBER
+        return constants.BOOL
