9c946523c26b3d65d84c0e6d2962ee8ba745f6c4
Pim Tegelaar <pimtegelaar@gmail.com>
2015-03-17 01:21:33 +0100

    simplify messages

--- encoders/ql/src/main/java/nl/uva/sc/encoders/ql/message/Messages.java
+++ encoders/ql/src/main/java/nl/uva/sc/encoders/ql/message/Messages.java
@@ -5,7 +5,8 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public final class Messages {
-	private static final String BUNDLE_NAME = Messages.class.getPackage().getName() + ".messages";
+
+	private static final String BUNDLE_NAME = "messages";
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
