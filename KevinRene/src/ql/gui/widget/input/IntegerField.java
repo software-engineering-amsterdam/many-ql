cde23df0edbd8edc4f10ffba2e215774eb6b3a21
Kevin A <kevin_son@live.nl>
2015-03-18 17:18:00 +0100

    Cleanup of Text and Integer fields

--- KevinRene/src/ql/gui/widget/input/IntegerField.java
+++ KevinRene/src/ql/gui/widget/input/IntegerField.java
@@ -1,60 +1,23 @@
 package ql.gui.widget.input;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 
-import net.miginfocom.swing.MigLayout;
-import ql.gui.widget.InputWidget;
 import ql.value.IntegerValue;
 
-public class IntegerField extends InputWidget<IntegerValue> implements CaretListener {	
+public class IntegerField extends Field<IntegerValue> implements CaretListener {	
 	protected JPanel container;
 	protected JTextField textField;
 	protected JLabel errorLabel;
 	
-	public IntegerField() {
-		textField = new JTextField(50);
-    	textField.setMaximumSize(new Dimension(textField.getPreferredSize().width, textField.getPreferredSize().height * 2));
-    	textField.setFont(new Font("Serif", Font.BOLD, 20));
-    	textField.addCaretListener(this);
-    	textField.setFocusable(true);
-    	
-    	errorLabel = new JLabel();
-    	errorLabel.setFont(new Font("Serif", Font.BOLD, 20));
-    	errorLabel.setVisible(true);
-    	
-    	container = new JPanel(new MigLayout());
-    	container.add(textField);
-    	container.add(errorLabel, "wrap");
+	public IntegerField () {
+		super();
 	}
-	
-	public IntegerField (IntegerValue stringValue) {
-		this();		
-    	textField.setText(stringValue.getValue().toString());	
-	}
-	
-	public void setError(String text) {
-		textField.setBorder(BorderFactory.createLineBorder(Color.RED));
-		errorLabel.setText(text);
-	}
-	
-	public void removeError() {
-		textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		errorLabel.setText("");
-	}
-	
-	@Override
-	public void disable() {
-		textField.setEnabled(false);
+	public IntegerField (IntegerValue value) {
+		super();		
+    	textField.setText(value.getValue().toString());	
 	}
 
 	@Override
@@ -65,26 +28,5 @@ public class IntegerField extends InputWidget<IntegerValue> implements CaretList
 	@Override
 	public void setValue(IntegerValue value) {
 		textField.setText(value.toString());		
-	}
-
-	@Override
-	public void updateComponent() {
-		textField.repaint();
-	}
-	
-	@Override
-	public JComponent getComponent() {
-		return this.container;
-	}
-
-	@Override
-	public void caretUpdate(CaretEvent e) {
-		try {
-			handleChange(getValue(), this);
-			removeError();
-		}
-		catch (NumberFormatException nfe) {
-			setError("Not a valid integer");
-		}
 	}
 }
--- KevinRene/src/ql/gui/widget/input/TextField.java
+++ KevinRene/src/ql/gui/widget/input/TextField.java
@@ -1,62 +1,26 @@
 package ql.gui.widget.input;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 
-import ql.gui.widget.InputWidget;
 import ql.value.StringValue;
-import net.miginfocom.swing.MigLayout;
 
-public class TextField extends InputWidget<StringValue> implements CaretListener {	
+public class TextField extends Field<StringValue> implements CaretListener {	
 	protected JPanel container;
 	protected JTextField textField;
 	protected JLabel errorLabel;
 	
 	public TextField() {
-		textField = new JTextField(50);
-    	textField.setMaximumSize(new Dimension(textField.getPreferredSize().width, textField.getPreferredSize().height * 2));
-    	textField.setFont(new Font("Serif", Font.BOLD, 20));
-    	textField.addCaretListener(this);
-    	textField.setFocusable(true);
-    	
-    	errorLabel = new JLabel();
-    	errorLabel.setFont(new Font("Serif", Font.BOLD, 20));
-    	errorLabel.setVisible(true);
-    	
-    	container = new JPanel(new MigLayout());
-    	container.add(textField);
-    	container.add(errorLabel, "wrap");
+		super();
 	}
 	
 	public TextField (StringValue stringValue) {
-		this();		
+		super();		
     	textField.setText(stringValue.getValue());	
 	}
 	
-	public void setError(String text) {
-		textField.setBorder(BorderFactory.createLineBorder(Color.RED));
-		errorLabel.setText(text);
-	}
-	
-	public void removeError() {
-		textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		errorLabel.setText("");
-	}
-	
-	@Override
-	public void disable() {
-		textField.setEnabled(false);
-	}
-
 	@Override
 	public StringValue getValue() {
 		return new StringValue(textField.getText());
@@ -65,20 +29,5 @@ public class TextField extends InputWidget<StringValue> implements CaretListener
 	@Override
 	public void setValue(StringValue value) {
 		textField.setText(value.toString());		
-	}
-
-	@Override
-	public void updateComponent() {
-		textField.repaint();
-	}
-	
-	@Override
-	public JComponent getComponent() {
-		return this.container;
-	}
-
-	@Override
-	public void caretUpdate(CaretEvent e) {
-		handleChange(getValue(), this);
 	}
 }
