2401bcd2de35bba5c0951a3d4310808f1709d68b
softwaregast <weisflog@hotmail.com>
2015-03-19 22:22:45 +0100

    Refer to Stylesheet instead of Page for TypeChecker

--- encoders/qls/src/main/java/nl/uva/sc/encoders/qls/validation/TypeChecker.java
+++ encoders/qls/src/main/java/nl/uva/sc/encoders/qls/validation/TypeChecker.java
@@ -1,30 +1,23 @@
 package nl.uva.sc.encoders.qls.validation;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import nl.uva.sc.encoders.qls.ast.Page;
+import nl.uva.sc.encoders.qls.ast.Stylesheet;
 
 public class TypeChecker {
 
-	// private static final String DUPLICATE_LABEL = "duplicateLabel";
-	// private static final String REFERENCE_BEFORE_STATED =
-	// "referenceBeforeStated";
-	// private static final String UNDEFINED_PAGE = "undefinedPage";
-	// private static final String UNDEFINED_SECTION = "undefinedSection";
-
-	private final Set<String> pageLabels = new HashSet<>();
-
-	private final Set<String> pageNames = new HashSet<String>();
-
 	private final List<TypeValidation> validations = new ArrayList<>();
 
-	private final Page page;
+	private final Stylesheet stylesheet;
 
-	public TypeChecker(Page page) {
-		this.page = page;
+	public TypeChecker(Stylesheet stylesheet) {
+		this.stylesheet = stylesheet;
+	}
+
+	public List<TypeValidation> checkTypes() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
