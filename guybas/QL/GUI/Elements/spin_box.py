78e05960721cc03ecdb9ea1734fab03081fab1c7
Guy Rombaut <admin@gsrweb.net>
2015-03-20 14:38:00 +0100

    remove the annoying "1" printing

--- guybas/QL/GUI/Elements/spin_box.py
+++ guybas/QL/GUI/Elements/spin_box.py
@@ -9,7 +9,7 @@ class SpinBox(i_element.Element):
         label = e_label.Label(self.statement.get_label(), self.frame)
         e_list += label.get_row()
         e = tk.Spinbox(self.frame, from_=0, to_=10000, command=lambda: self.gui.update(self.statement, None if e.get() is '' else int(e.get())))
-        print(e.winfo_height())
+        # print(e.winfo_height())
         e.bind("<KeyPress><KeyRelease>", lambda event: self.gui.update(self.statement, None if e.get() is '' else int(e.get())))
         # expression_factory.grid(row=statement.get_order(), column=1, columnspan=2, sticky=W)
         e_list.append(e)
