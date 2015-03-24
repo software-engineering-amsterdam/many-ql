c24d369642d3e5279405e8500a7a3b8daf27b876
Pim Tegelaar <pimtegelaar@gmail.com>
2015-03-24 13:42:02 +0100

    add literal interface

---
+++ encoders/ql/src/main/java/nl/uva/sc/encoders/ql/ast/expression/literal/Literal.java
@@ -0,0 +1,12 @@
+package nl.uva.sc.encoders.ql.ast.expression.literal;
+
+import nl.uva.sc.encoders.ql.ast.AstNode;
+import nl.uva.sc.encoders.ql.ast.type.DataType;
+import nl.uva.sc.encoders.ql.visitor.LiteralVisitor;
+
+public interface Literal extends AstNode {
+
+	DataType getType();
+
+	<T> T accept(LiteralVisitor<T> visitor);
+}
