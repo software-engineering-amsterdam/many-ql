0e093ad704e0e213348083f6db62b29d8d5aefa6
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-03-26 19:49:12 +0100

    comment out JUIQuestion.

--- Fugazi/src/org/fugazi/qls/gui/ui_segment/JUIQuestion.java
+++ Fugazi/src/org/fugazi/qls/gui/ui_segment/JUIQuestion.java
@@ -2,6 +2,6 @@ package org.fugazi.qls.gui.ui_segment;
 
 import org.fugazi.ql.gui.ui_elements.ui_questions.UIQuestion;
 
-public class JUIQuestion extends UIQuestion{
-
-}
+//public class JUIQuestion extends UIQuestion{
+//
+//}
