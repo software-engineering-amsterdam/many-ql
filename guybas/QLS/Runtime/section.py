0014ff8eb5d4527d2c3e676369b6cb578281de96
Guy Rombaut <admin@gsrweb.net>
2015-03-28 19:12:42 +0100

    colours, fonts, sections names etc.

--- guybas/QLS/Runtime/section.py
+++ guybas/QLS/Runtime/section.py
@@ -1,10 +1,14 @@
 class Section:
-    def __init__(self, order, style=None):
+    def __init__(self, order, title=None, style=None):
         self.__order = order
         self.__style = style
+        self.__title = title
 
     def get_order(self):
         return self.__order
 
     def get_style(self):
         return self.__style
+
+    def get_title(self):
+        return self.__title
