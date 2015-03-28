c0aa09c9eff4fdec80b4d607a1c95305ad0ef664
Theol.Zacharopoulos <theol.zacharopoulos@gmail.com>
2015-03-28 15:44:00 +0100

    Make an abstractWidget to set implementation up.

---
+++ Fugazi/src/org/fugazi/ql/gui/widgets/IWidget.java
@@ -0,0 +1,14 @@
+package org.fugazi.ql.gui.widgets;
+
+import org.fugazi.ql.evaluator.expression_value.ExpressionValue;
+import org.fugazi.ql.gui.ui_elements.UIForm;
+
+public interface IWidget {
+    public void render(UIForm _canvas);
+    public void suppress(UIForm _canvas);
+
+    public ExpressionValue getWidgetValue();
+    public void setWidgetValue(ExpressionValue _value);
+    public void setReadOnly(boolean _isReadonly);
+    public void addEventListener(WidgetsEventListener _listener);
+}
