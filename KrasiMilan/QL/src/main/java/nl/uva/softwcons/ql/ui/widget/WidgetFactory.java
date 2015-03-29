7440b52db1e2f281768a1f120a2be31b21375ecc
Milan Milanov <whizzer.me@gmail.com>
2015-03-29 11:36:42 +0200

    Tweak the widget factory interface and add the qls implementation

--- KrasiMilan/QL/src/main/java/nl/uva/softwcons/ql/ui/widget/WidgetFactory.java
+++ KrasiMilan/QL/src/main/java/nl/uva/softwcons/ql/ui/widget/WidgetFactory.java
@@ -1,10 +1,7 @@
 package nl.uva.softwcons.ql.ui.widget;
 
-import nl.uva.softwcons.ql.ast.statement.ComputedQuestion;
 import nl.uva.softwcons.ql.ast.statement.Question;
 
 public interface WidgetFactory {
     Widget getWidget(Question question);
-
-    Widget getWidget(ComputedQuestion question);
 }
