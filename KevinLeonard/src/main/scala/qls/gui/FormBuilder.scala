f0b467c780e51ec712b7cfbaaf1eab94f1a13a9f
leonardpunt <leonard@q42.nl>
2015-03-29 14:37:13 +0200

    Refactoring in FormBuilder.

--- KevinLeonard/src/main/scala/qls/gui/FormBuilder.scala
+++ KevinLeonard/src/main/scala/qls/gui/FormBuilder.scala
@@ -1,13 +1,7 @@
 package qls.gui
 
-import ql.ast.{StringType, NumberType, BooleanType, Expression, Question => QLQuestion}
-import ql.gui.widgets.{
-  BooleanQuestionWidget => QLBooleanQuestionWidget,
-  NumberQuestionWidget => QLNumberQuestionWidget,
-  StringQuestionWidget => QLStringQuestionWidget
-}
+import ql.ast.{BooleanType, Expression, NumberType, Question => QLQuestion, StringType}
 import ql.gui.{FormBuilder => QLFormBuilder}
-
 import qls.ast._
 import qls.gui.widgets._
 import qls.typechecker.Questions
@@ -16,27 +10,21 @@ import types.EvalEnvironment
 import scalafx.collections.ObservableMap
 import scalafx.scene.layout.VBox
 
-class FormBuilder(stylesheet: StyleSheet, env: EvalEnvironment = ObservableMap.empty)
-  extends QLFormBuilder {
+class FormBuilder(stylesheet: StyleSheet, env: EvalEnvironment = ObservableMap.empty) extends QLFormBuilder(env) {
 
   val questionStyles: List[Question] = Questions.extract(stylesheet)
 
   override def buildQuestion(q: QLQuestion, visibilityExpressions: List[Expression]): VBox = {
-    questionStyles.find(qs => qs.variable == q.variable) match {
-      case None => {
-        q._type match {
-          case BooleanType() => new QLBooleanQuestionWidget(q, visibilityExpressions, env)
-          case NumberType() => new QLNumberQuestionWidget(q, visibilityExpressions, env)
-          case StringType() => new QLStringQuestionWidget(q, visibilityExpressions, env)
-        }
-      }
+    questionStyles.find(_.variable == q.variable) match {
+      case None => throw new AssertionError("All questions should be placed.")
       case Some(qs) => qs.widget match {
         // TODO: Spinner class not available: see https://codingonthestaircase.wordpress.com/2014/11/08/scalafx-8-0-40-snapshot-spinner/
-        //case SpinBox() => new SpinBoxQuestionWidget(q, visibilityExpressions, env)
+        case SpinBox(_) => throw new NotImplementedError("Spinner class not available.")
         case Slider(styleProperties) => new SliderQuestionWidget(q, visibilityExpressions, env)
         case Text(styleProperties) => q._type match {
           case NumberType() => new NumberQuestionWidget(q, visibilityExpressions, env)
           case StringType() => new StringQuestionWidget(q, visibilityExpressions, env)
+          case BooleanType() => throw new AssertionError("Text widget not allowed for boolean questions.")
         }
         case TextBlock(styleProperties) => new TextBlockQuestionWidget(q, visibilityExpressions, env)
         case Radio(styleProperties) => new RadioQuestionWidget(q, visibilityExpressions, env)
