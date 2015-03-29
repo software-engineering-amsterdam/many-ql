e97decfcab702314432cb0f51a97ff08cd6ef1aa
Ger <gerrit.krijnen@hotmail.com>
2015-03-29 18:32:50 +0200

    Added labels with Calculations, Removed obsolete Input class

--- RobertGer/src/main/java/nl/uva/bromance/ast/Calculation.java
+++ RobertGer/src/main/java/nl/uva/bromance/ast/Calculation.java
@@ -1,14 +1,22 @@
 package nl.uva.bromance.ast;
 
+import nl.uva.bromance.ast.conditionals.*;
 import nl.uva.bromance.ast.visitors.QLNodeVisitor;
 
 
-public class Calculation extends QLNode{
-    private String identifier;
+public class Calculation extends QLNode implements ContainsExpression, HasIdentifier {
+    private Identifier identifier;
+    private IntResult result;
+    private Expression expression;
 
-    public Calculation(int lineNumber, String id) {
+    public Calculation(int lineNumber, Identifier id) {
         super(lineNumber);
         this.identifier = id;
+        this.identifier.setResult(new IntResult(0));
+    }
+
+    public IntResult getResult(){
+        return result;
     }
 
     //TODO: Find fix for childType
@@ -18,5 +26,21 @@ public class Calculation extends QLNode{
         for (QLNode child : this.getChildren()) {
             child.accept(visitor);
         }
+    }
+
+    @Override
+    public void setExpression(Expression expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public void handleExpressionResult() {
+        result = (IntResult) expression.getResult();
+        identifier.setResult(result);
+    }
+
+    @Override
+    public Identifier getIdentifier() {
+        return identifier;
     }
 }
