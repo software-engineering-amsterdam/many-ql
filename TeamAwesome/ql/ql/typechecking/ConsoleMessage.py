be776578fc6f093ce662d2c1ae27af7c39f1a1e6
guidouva <guidouva@outlook.com>
2015-03-29 16:03:52 +0200

    parameterize factories to remove duplication, order console messages by line number

--- TeamAwesome/ql/ql/typechecking/ConsoleMessage.py
+++ TeamAwesome/ql/ql/typechecking/ConsoleMessage.py
@@ -2,97 +2,26 @@ from . import Message
 
 
 
-class Factory(Message.Factory):
-    def __init__(self, parser):
-        self._parser = parser
-
-
-    def duplicateLabel(self, label, lineNumber):
-        return ConsoleMessage(
-            Warning(),
-            Local(lineNumber),
-            'duplicate question label `%s`' % (label)
-        )
-
-    def questionCycle(self, questionCycle, lineNumber):
-        text = 'There is a question dependency cycle: %s. \
-It means the calculation of the answer requires its own \
-result as input. This is not possible. Please double check \
-the definitions of the questions.' % (
-               ' <- '.join([str(q.identifier) for q in questionCycle])
-            )
-
-        return ConsoleMessage(
-            Error(),
-            Local(lineNumber),
-            text
-        )
-
-    def questionRedefinition(
-        self, question, actualType, expectedType, lineNumber
-    ):
-        text = 'duplicate definition of question `%s` with different \
-type `%s` (expected type `%s`)' % (
-                question.identifier,
-                self._parser.expressionTypeToken(actualType),
-                self._parser.expressionTypeToken(expectedType)
-            )
-
-        return ConsoleMessage(
-            Error(),
-            Local(lineNumber),
-            text
-        )
-            
-
-    def incompatibleExpressionType(
-        self, actualType, allowedTypes, lineNumber
-    ):
-        actualTypeText = self._parser.expressionTypeToken(actualType)
-        allowedTypesText = list(map(
-            lambda t: '`%s`' % (self._parser.expressionTypeToken(t)),
-            allowedTypes
-        ))
-        text = 'got an expression of type `%s` which is not \
-compatible with any of the following types which are \
-allowed here: %s' % (
-                actualTypeText, ', '.join(allowedTypesText)
-            )
-
-        return ConsoleMessage(
-            Error(),
-            Local(lineNumber),
-            text
-        )
-
-
-    def undeclaredIdentifier(self, identifier, lineNumber):
-        return ConsoleMessage(
-            Error(),
-            Local(lineNumber),
-            'undeclared identifier `%s`' % (identifier)
-        )
-
-
-    def invalidOperands(self, operator, operands, lineNumber):
-        text = 'invalid operand(s) to operator `%s`: %s' % (
-            self._parser.operatorToken(operator),
-            ', '.join([str(operand) for operand in operands])
-        )
-
-        return ConsoleMessage(
-            Error(),
-            Local(lineNumber),
-            text
-        )
-
-
-
 class ConsoleMessage:
     def __init__(self, level, scope, text):
         self._level = level
         self._scope = scope
         self._text = text
+
+
+    @property
+    def lineNumber(self):
+        return self._scope.lineNumber
+
+
+    # allows messages to be ordered
+    def __le__(self, other):
+        if self.lineNumber is None:
+            return False
+        elif other.lineNumber is None:
+            return True
+        else:
+            return self.lineNumber < other.lineNumber
 
 
     def __str__(self):
@@ -102,6 +31,97 @@ class ConsoleMessage:
         )
 
         return '[%s] %s' % (' '.join(prefixParts), self._text)
+
+
+
+# See ErrorsWarningsResult.py for the rationale of why factories are
+# created this way, in short: it's the Python way of doing generics.
+def factory(parser, cls = ConsoleMessage):
+    class Factory(Message.Factory):
+        def __init__(self, parser):
+            self._parser = parser
+
+
+        def duplicateLabel(self, label, lineNumber):
+            return cls(
+                Warning(),
+                Local(lineNumber),
+                'duplicate question label `%s`' % (label)
+            )
+
+        def questionCycle(self, questionCycle, lineNumber):
+            text = 'There is a question dependency cycle: %s. \
+    It means the calculation of the answer requires its own \
+    result as input. This is not possible. Please double check \
+    the definitions of the questions.' % (
+                   ' <- '.join([str(q.identifier) for q in questionCycle])
+                )
+
+            return cls(
+                Error(),
+                Local(lineNumber),
+                text
+            )
+
+        def questionRedefinition(
+            self, question, actualType, expectedType, lineNumber
+        ):
+            text = 'duplicate definition of question `%s` with different \
+    type `%s` (expected type `%s`)' % (
+                    question.identifier,
+                    self._parser.expressionTypeToken(actualType),
+                    self._parser.expressionTypeToken(expectedType)
+                )
+
+            return cls(
+                Error(),
+                Local(lineNumber),
+                text
+            )
+                
+
+        def incompatibleExpressionType(
+            self, actualType, allowedTypes, lineNumber
+        ):
+            actualTypeText = self._parser.expressionTypeToken(actualType)
+            allowedTypesText = list(map(
+                lambda t: '`%s`' % (self._parser.expressionTypeToken(t)),
+                allowedTypes
+            ))
+            text = 'got an expression of type `%s` which is not \
+    compatible with any of the following types which are \
+    allowed here: %s' % (
+                    actualTypeText, ', '.join(allowedTypesText)
+                )
+
+            return cls(
+                Error(),
+                Local(lineNumber),
+                text
+            )
+
+
+        def undeclaredIdentifier(self, identifier, lineNumber):
+            return cls(
+                Error(),
+                Local(lineNumber),
+                'undeclared identifier `%s`' % (identifier)
+            )
+
+
+        def invalidOperands(self, operator, operands, lineNumber):
+            text = 'invalid operand(s) to operator `%s`: %s' % (
+                self._parser.operatorToken(operator),
+                ', '.join([str(operand) for operand in operands])
+            )
+
+            return cls(
+                Error(),
+                Local(lineNumber),
+                text
+            )
+
+    return Factory(parser)
 
 
 
@@ -124,12 +144,22 @@ class Warning(Level):
 
 
 class Scope:
+    @property
+    def lineNumber(self):
+        raise NotImplementedError
+
+
     def __str__(self):
         raise NotImplementedError
 
 
 
 class Global(Scope):
+    @property
+    def lineNumber(self):
+        return None
+    
+
     def __str__(self):
         return ''
 
@@ -139,5 +169,11 @@ class Local(Scope):
     def __init__(self, lineNumber):
         self._lineNumber = lineNumber
 
+
+    @property
+    def lineNumber(self):
+        return self._lineNumber
+    
+
     def __str__(self):
-        return 'line '+str(self._lineNumber)
+        return 'line '+str(self.lineNumber)
