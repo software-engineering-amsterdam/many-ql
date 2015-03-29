1918a83cd38d2497b25496ec34c03f3d135b03dc
pimtegelaar <pimtegelaar@gmail.com>
2015-03-29 15:47:58 +0200

    Test for string concatenation

--- encoders/qlruntime/src/test/java/qlruntime/ExpressionEvaluatorTest.java
+++ encoders/qlruntime/src/test/java/qlruntime/ExpressionEvaluatorTest.java
@@ -10,13 +10,16 @@ import java.util.List;
 
 import nl.uva.sc.encoders.ql.ast.expression.BinaryExpression;
 import nl.uva.sc.encoders.ql.ast.expression.BracedExpression;
+import nl.uva.sc.encoders.ql.ast.expression.Expression;
 import nl.uva.sc.encoders.ql.ast.expression.LiteralExpression;
 import nl.uva.sc.encoders.ql.ast.literal.IntegerLiteral;
+import nl.uva.sc.encoders.ql.ast.literal.StringLiteral;
 import nl.uva.sc.encoders.ql.ast.operator.AddOperator;
 import nl.uva.sc.encoders.ql.ast.operator.MultiplyOperator;
 import nl.uva.sc.encoders.qlruntime.evaluator.ExpressionEvaluator;
 import nl.uva.sc.encoders.qlruntime.model.RuntimeQuestion;
 import nl.uva.sc.encoders.qlruntime.model.value.IntegerValue;
+import nl.uva.sc.encoders.qlruntime.model.value.StringValue;
 import nl.uva.sc.encoders.qlruntime.model.value.Value;
 
 import org.junit.Test;
@@ -45,6 +48,26 @@ public class ExpressionEvaluatorTest {
 
 		assertThat(result, is(instanceOf(IntegerValue.class)));
 		assertThat(((IntegerValue) result).getValue(), is(80));
+	}
+
+	/**
+	 * Testing "Hi" + " there!" = "Hi there!"
+	 */
+	@Test
+	public void testEvaluatesStringConcatenation() {
+		List<RuntimeQuestion> questions = new ArrayList<>();
+
+		ExpressionEvaluator evaluator = new ExpressionEvaluator(questions);
+
+		Expression hiExpression = new LiteralExpression(aTextLocation().build(), new StringLiteral("Hi"));
+		Expression thereExpression = new LiteralExpression(aTextLocation().build(), new StringLiteral(" there!"));
+		BinaryExpression multiplyExpression = new BinaryExpression(aTextLocation().build(), hiExpression, thereExpression,
+				new AddOperator("+"));
+
+		Value result = multiplyExpression.accept(evaluator);
+
+		assertThat(result, is(instanceOf(StringValue.class)));
+		assertThat(((StringValue) result).getValue(), is("Hi there!"));
 	}
 
 }
