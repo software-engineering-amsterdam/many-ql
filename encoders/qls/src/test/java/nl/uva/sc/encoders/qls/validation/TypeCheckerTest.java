0b5e882fdbc7c7517da6c64b9e381aefff02a013
softwaregast <weisflog@hotmail.com>
2015-03-29 18:20:59 +0200

    Start creating Tests for TypeChecker QLS

---
+++ encoders/qls/src/test/java/nl/uva/sc/encoders/qls/validation/TypeCheckerTest.java
@@ -0,0 +1,27 @@
+package nl.uva.sc.encoders.qls.validation;
+
+import org.junit.Test;
+
+public class TypeCheckerTest {
+
+	@Test
+	public void testCheckTypes_referencedQuestionDoesNotExistInQL() {
+
+	}
+
+	@Test
+	public void testCheckTypes_notAllQLQuestionsArePlaced() {
+
+	}
+
+	@Test
+	public void testCheckTypes_incompatibleWidgetToQuestionAssignment() {
+
+	}
+
+	@Test
+	public void testCheckTypes_singleQuestionPlacedMultipleTimesIsInvalid() {
+
+	}
+
+}
