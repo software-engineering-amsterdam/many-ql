4696bb9c0b757a6aa37bf0560b96ebacf62bde58
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-03-29 20:40:28 +0200

    Widgets for form input

--- fabien/src/typechecker/DuplicateQuestions.py
+++ fabien/src/typechecker/DuplicateQuestions.py
@@ -1,18 +1,17 @@
 
-from errors import DuplicationError
+from Errors import DuplicationError
 
 class DuplicateQuestions:
-    def __init__(self):
+    def __init__(self, questionIDs=None):
         self.errors = []
-        self.questionTypes = {}
+        self.questionIDs = questionIDs
 
     def Question(self, node):
-        if node.ID in self.questionTypes:
-            if node.type != self.questionTypes[node.ID].type:
-                error = DuplicationError(self.questionTypes[node.ID], node)
+        if node.ID in self.questionIDs:
+            if node.type != self.questionIDs[node.ID].type:
+                error = DuplicationError(self.questionIDs[node.ID], node)
                 self.errors.append(error)
             else:
                 # TODO Warning object, duplicate question but same type
+                #error = DuplicationError(self.questionIDs[node.ID], node)
                 pass
-        else:
-            self.questionTypes[node.ID] = node
