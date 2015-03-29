be7023df7c4dcd37ff6e8e6d1fd398da97f6546e
Pim Tegelaar <pimtegelaar@gmail.com>
2015-03-30 00:08:04 +0200

    remove unused constants

--- encoders/qls/src/main/java/nl/uva/sc/encoders/qls/validation/TypeChecker.java
+++ encoders/qls/src/main/java/nl/uva/sc/encoders/qls/validation/TypeChecker.java
@@ -15,9 +15,6 @@ import nl.uva.sc.encoders.qls.visitor.SectionVisitor;
 
 public class TypeChecker implements SectionVisitor<List<TypeValidation>> {
 
-	private static final String NON_EXISTING_QUESTION_REFERENCE = "nonexistingQuestionReference";
-	private static final String SINGLE_QUESTION_PLACED_MULTIPLE_TIMES = "singleQuestionPlacedMultipleTimes";
-
 	private final Stylesheet stylesheet;
 	private final Questionnaire questionnaire;
 
